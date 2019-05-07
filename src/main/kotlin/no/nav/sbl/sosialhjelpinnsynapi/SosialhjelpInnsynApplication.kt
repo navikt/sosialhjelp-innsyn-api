e223918abc3152b81bec05876eaa8de1ce7f787a
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-05-07 09:25:41 +0200

    switch til kotlin

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/SosialhjelpInnsynApplication.kt
@@ -0,0 +1,12 @@
+package no.nav.sbl.sosialhjelpinnsynapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SosialhjelpInnsynApplication
+
+    fun main(args: Array<String>) {
+        runApplication<SosialhjelpInnsynApplication>(*args)
+    }
+

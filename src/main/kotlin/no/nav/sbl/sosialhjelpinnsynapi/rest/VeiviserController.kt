a74df97ea34c61083cd9672e0e04ab1f9dd181e1
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-05-20 12:51:22 +0200

    Veiviser kommunenummer

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/VeiviserController.kt
@@ -0,0 +1,35 @@
+package no.nav.sbl.sosialhjelpinnsynapi.rest
+
+import io.micrometer.core.instrument.util.IOUtils
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import java.io.IOException
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+@RestController
+@RequestMapping("/api/veiviser/")
+class VeiviserController {
+
+    @GetMapping("kommunenummer", produces = [APPLICATION_JSON_UTF8_VALUE])
+    fun getInnsynForSoknad(): ResponseEntity<String> {
+        try {
+            val urlConnection = URL("https://register.geonorge.no/api/subregister/sosi-kodelister/kartverket/kommunenummer-alle.json").openConnection() as HttpsURLConnection
+            try {
+                val kommunenr = urlConnection.inputStream.use { inputStream -> IOUtils.toString(inputStream) }
+                return ResponseEntity.ok(kommunenr)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            return ResponseEntity.notFound().build()
+        } catch (e: Exception) {
+            throw ResponseStatusException(BAD_REQUEST)
+        }
+    }
+}

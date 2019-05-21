3dd859ae2e49b37f1369355f14c9e89c15960fab
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-05-21 15:33:34 +0200

    enabler oidc token suppert

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/Application.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/Application.kt
@@ -1,9 +1,11 @@
 package no.nav.sbl.sosialhjelpinnsynapi
 
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableOIDCTokenValidation
 class Application
 
     fun main(args: Array<String>) {

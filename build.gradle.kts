65c294a32e0bdf5fd767e921392adde7e68b44fd
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-06-04 13:54:54 +0200

    Oppdatert build.gradle for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,11 @@ val logbackVersion = "1.2.3"
 val logstashVersion = "5.3"
 val junitJupiterVersion = "5.4.2"
 val mockkVersion = "1.9.3"
+val wireMockVersion = "2.19.0"
 val filformatVersion = "1.2019.05.08-16.27-0a95b4468f3d"
 val micrometerRegistryVersion = "1.1.2"
 val tokenSupportVersion = "0.2.18"
+val jacksonVersion = "2.9.9"
 
 val mainClass = "no.nav.sbl.sosialhjelpinnsynapi.ApplicationKt"
 
@@ -22,7 +24,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.31"
     id("org.springframework.boot") version "2.1.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
-//    id("com.github.johnrengelman.shadow") version "5.0.0"
 }
 
 buildscript {
@@ -45,25 +46,29 @@ dependencies {
     compile(kotlin("reflect"))
 
     compile("org.springframework.boot:spring-boot-starter-web:$springBootVersion") {
-        exclude(module="spring-boot-starter-tomcat")
+        exclude(module = "spring-boot-starter-tomcat")
     }
     compile("org.springframework.boot:spring-boot-starter-jetty:$springBootVersion")
     compile("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
     compile("io.micrometer:micrometer-registry-prometheus:$micrometerRegistryVersion")
     compile("org.springframework.boot:spring-boot-starter-logging:$springBootVersion") {
-        exclude(module="commons-logging")
+        exclude(module = "commons-logging")
     }
     compile("ch.qos.logback:logback-classic:$logbackVersion")
     compile("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
 
     compile("no.nav.sbl.dialogarena:soknadsosialhjelp-filformat:$filformatVersion")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
-    compile("no.nav.security:oidc-spring-support:$tokenSupportVersion"){
-        exclude(module="spring-boot-starter-tomcat")
+    compile("no.nav.security:oidc-spring-support:$tokenSupportVersion") {
+        exclude(module = "spring-boot-starter-tomcat")
     }
 
     testCompile("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
+    testCompile("com.github.tomakehurst:wiremock:$wireMockVersion") {
+        exclude(group = "junit")
+    }
     testCompile("no.nav.security:oidc-test-support:$tokenSupportVersion")
 }
 
@@ -80,6 +85,7 @@ tasks {
             println(project.version)
         }
     }
+
     withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = "1.8"
@@ -88,7 +94,7 @@ tasks {
     }
 
     withType<Test> {
-        useJUnitPlatform{
+        useJUnitPlatform {
             includeEngines("junit-jupiter")
         }
         testLogging {

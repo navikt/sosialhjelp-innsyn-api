14368a04629b947f426771569013c757a8f64a07
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-06-19 13:35:20 +0200

    Endrer fiksDigisosId'er i test for å ikke gå i beina på stub

--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/responses/DigisosSakResponses.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/responses/DigisosSakResponses.kt
@@ -2,24 +2,24 @@ package no.nav.sbl.sosialhjelpinnsynapi.responses
 
 val ok_digisossak_response = """
 {
-  "fiksDigisosId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "fiksDigisosId": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
   "sokerFnr": "string",
-  "fiksOrgId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  "fiksOrgId": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
   "kommunenummer": "string",
   "sistEndret": 0,
   "orginalSoknadNAV": {
     "navEksternRefId": "string",
-    "metadata": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    "vedleggMetadata": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "metadata": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
+    "vedleggMetadata": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
     "soknadDokument": {
       "filnavn": "string",
-      "dokumentlagerDokumentId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "dokumentlagerDokumentId": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
       "storrelse": 0
     },
     "vedlegg": [
       {
         "filnavn": "string",
-        "dokumentlagerDokumentId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "dokumentlagerDokumentId": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
         "storrelse": 0
       }
     ],
@@ -29,11 +29,11 @@ val ok_digisossak_response = """
     "ettersendelser": [
       {
         "navEksternRefId": "string",
-        "vedleggMetadata": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "vedleggMetadata": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
         "vedlegg": [
           {
             "filnavn": "string",
-            "dokumentlagerDokumentId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "dokumentlagerDokumentId": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
             "storrelse": 0
           }
         ],
@@ -42,7 +42,7 @@ val ok_digisossak_response = """
     ]
   },
   "digisosSoker": {
-    "metadata": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    "metadata": "3fa85f64-5717-4562-b3fc-abcdabcdabcd",
     "dokumenter": [
       {
         "filnavn": "string",

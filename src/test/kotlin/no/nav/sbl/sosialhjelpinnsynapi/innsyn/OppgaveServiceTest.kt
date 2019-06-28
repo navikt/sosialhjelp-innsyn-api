8ac678c301442b833148a31762e77ee274a19e4e
Oskar Asplin <oskarasplin@gmail.com>
2019-06-28 11:51:27 +0200

    DIGISOS-1301 La til test

--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/innsyn/OppgaveServiceTest.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/innsyn/OppgaveServiceTest.kt
@@ -23,9 +23,18 @@ internal class OppgaveServiceTest {
 
     private val mockJsonDigisosSoker: JsonDigisosSoker = mockk()
 
-    private val type = "testType"
-    private val tillegg = "testTillegg"
-    private val frist = "2019-10-04T13:37:00.134Z"
+    private val type = "brukskonto"
+    private val tillegg = "fraarm"
+    private val type_2 = "sparekonto"
+    private val tillegg_2 = "sparegris"
+    private val type_3 = "bsu"
+    private val tillegg_3 = "bes svare umiddelbart"
+    private val type_4 = "pengebinge"
+    private val tillegg_4 = "Onkel Skrue penger"
+    private val frist = "2019-10-01T13:37:00.134Z"
+    private val frist_2 = "2019-10-02T13:37:00.134Z"
+    private val frist_3 = "2019-10-03T13:37:00.134Z"
+    private val frist_4 = "2019-10-04T13:37:00.134Z"
 
     @BeforeEach
     fun init() {
@@ -33,8 +42,8 @@ internal class OppgaveServiceTest {
     }
 
     @Test
-    fun `Should return Oppgaver`() {
-        every { innsynService.hentDigisosSak(any()) } returns jsonDigisosSoker_med_oppgaver
+    fun `Should return oppgave`() {
+        every { innsynService.hentDigisosSak(any()) } returns jsonDigisosSoker_med_oppgave
 
         val oppgaver = service.getOppgaverForSoknad("123")
 
@@ -44,7 +53,29 @@ internal class OppgaveServiceTest {
         assertTrue(oppgaver[0].innsendelsesfrist == frist)
     }
 
-    private val jsonDigisosSoker_med_oppgaver: JsonDigisosSoker = JsonDigisosSoker()
+    @Test
+    fun `Should return list of oppgaver from several JsonDokumentasjonEtterspurt and sorted by frist`() {
+        every { innsynService.hentDigisosSak(any()) } returns jsonDigisosSoker_med_oppgaver
+
+        val oppgaver = service.getOppgaverForSoknad("123")
+
+        assertNotNull(oppgaver)
+        assertTrue(oppgaver.size == 4)
+        assertTrue(oppgaver[0].dokumenttype == type)
+        assertTrue(oppgaver[0].tilleggsinformasjon == tillegg)
+        assertTrue(oppgaver[0].innsendelsesfrist == frist)
+        assertTrue(oppgaver[1].dokumenttype == type_2)
+        assertTrue(oppgaver[1].tilleggsinformasjon == tillegg_2)
+        assertTrue(oppgaver[1].innsendelsesfrist == frist_2)
+        assertTrue(oppgaver[2].dokumenttype == type_3)
+        assertTrue(oppgaver[2].tilleggsinformasjon == tillegg_3)
+        assertTrue(oppgaver[2].innsendelsesfrist == frist_3)
+        assertTrue(oppgaver[3].dokumenttype == type_4)
+        assertTrue(oppgaver[3].tilleggsinformasjon == tillegg_4)
+        assertTrue(oppgaver[3].innsendelsesfrist == frist_4)
+    }
+
+    private val jsonDigisosSoker_med_oppgave: JsonDigisosSoker = JsonDigisosSoker()
             .withAvsender(JsonAvsender().withSystemnavn("test"))
             .withVersion("1.2.3")
             .withHendelser(listOf(
@@ -55,4 +86,33 @@ internal class OppgaveServiceTest {
                                     .withDokumenttype(type)
                                     .withTilleggsinformasjon(tillegg)
                                     .withInnsendelsesfrist(frist)))))
+
+    private val jsonDigisosSoker_med_oppgaver: JsonDigisosSoker = JsonDigisosSoker()
+            .withAvsender(JsonAvsender().withSystemnavn("test"))
+            .withVersion("1.2.3")
+            .withHendelser(listOf(
+                    JsonDokumentasjonEtterspurt()
+                            .withType(JsonHendelse.Type.DOKUMENTASJON_ETTERSPURT)
+                            .withHendelsestidspunkt(LocalDateTime.now().minusHours(10).format(DateTimeFormatter.ISO_DATE_TIME))
+                            .withDokumenter(listOf(
+                                    JsonDokumenter()
+                                            .withDokumenttype(type)
+                                            .withTilleggsinformasjon(tillegg)
+                                            .withInnsendelsesfrist(frist),
+                                    JsonDokumenter()
+                                            .withDokumenttype(type_3)
+                                            .withTilleggsinformasjon(tillegg_3)
+                                            .withInnsendelsesfrist(frist_3))),
+                    JsonDokumentasjonEtterspurt()
+                            .withType(JsonHendelse.Type.DOKUMENTASJON_ETTERSPURT)
+                            .withHendelsestidspunkt(LocalDateTime.now().minusHours(10).format(DateTimeFormatter.ISO_DATE_TIME))
+                            .withDokumenter(listOf(
+                                    JsonDokumenter()
+                                            .withDokumenttype(type_4)
+                                            .withTilleggsinformasjon(tillegg_4)
+                                            .withInnsendelsesfrist(frist_4),
+                                    JsonDokumenter()
+                                            .withDokumenttype(type_2)
+                                            .withTilleggsinformasjon(tillegg_2)
+                                            .withInnsendelsesfrist(frist_2)))))
 }

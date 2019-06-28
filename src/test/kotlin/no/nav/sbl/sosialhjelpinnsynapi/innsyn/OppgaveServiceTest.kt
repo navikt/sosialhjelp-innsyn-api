36baf934306935d35380f94dacbb0800bca9b67e
Oskar Asplin <oskarasplin@gmail.com>
2019-06-28 14:45:15 +0200

    DIGISOS-1301 La til test

--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/innsyn/OppgaveServiceTest.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/innsyn/OppgaveServiceTest.kt
@@ -8,8 +8,7 @@ import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonDigisosSoker
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonHendelse
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonDokumentasjonEtterspurt
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonDokumenter
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
@@ -54,6 +53,18 @@ internal class OppgaveServiceTest {
     }
 
     @Test
+    fun `Should return oppgave without tilleggsinformasjon`() {
+        every { innsynService.hentDigisosSak(any()) } returns jsonDigisosSoker_med_oppgave_uten_tilleggsinfo
+
+        val oppgaver = service.getOppgaverForSoknad("123")
+
+        assertNotNull(oppgaver)
+        assertTrue(oppgaver[0].dokumenttype == type)
+        assertNull(oppgaver[0].tilleggsinformasjon)
+        assertTrue(oppgaver[0].innsendelsesfrist == frist)
+    }
+
+    @Test
     fun `Should return list of oppgaver from several JsonDokumentasjonEtterspurt and sorted by frist`() {
         every { innsynService.hentDigisosSak(any()) } returns jsonDigisosSoker_med_oppgaver
 
@@ -85,6 +96,17 @@ internal class OppgaveServiceTest {
                             .withDokumenter(listOf(JsonDokumenter()
                                     .withDokumenttype(type)
                                     .withTilleggsinformasjon(tillegg)
+                                    .withInnsendelsesfrist(frist)))))
+
+    private val jsonDigisosSoker_med_oppgave_uten_tilleggsinfo: JsonDigisosSoker = JsonDigisosSoker()
+            .withAvsender(JsonAvsender().withSystemnavn("test"))
+            .withVersion("1.2.3")
+            .withHendelser(listOf(
+                    JsonDokumentasjonEtterspurt()
+                            .withType(JsonHendelse.Type.DOKUMENTASJON_ETTERSPURT)
+                            .withHendelsestidspunkt(LocalDateTime.now().minusHours(10).format(DateTimeFormatter.ISO_DATE_TIME))
+                            .withDokumenter(listOf(JsonDokumenter()
+                                    .withDokumenttype(type)
                                     .withInnsendelsesfrist(frist)))))
 
     private val jsonDigisosSoker_med_oppgaver: JsonDigisosSoker = JsonDigisosSoker()

27e33e4359a230b9c957bb29a7d85e62a4b79c76
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-07-04 13:20:14 +0200

    Oppdatert Utils med konvertering fra string og unix timestamp til LocalDateTime

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/Utils.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/Utils.kt
@@ -4,6 +4,10 @@ import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonFilreferanse
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.filreferanse.JsonDokumentlagerFilreferanse
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.filreferanse.JsonSvarUtFilreferanse
 import no.nav.sbl.sosialhjelpinnsynapi.config.ClientProperties
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 fun hentUrlFraFilreferanse(clientProperties: ClientProperties, filreferanse: JsonFilreferanse): String {
     return when (filreferanse) {
@@ -11,4 +15,12 @@ fun hentUrlFraFilreferanse(clientProperties: ClientProperties, filreferanse: Jso
         is JsonSvarUtFilreferanse -> clientProperties.fiksSvarUtEndpointUrl + "/forsendelse/${filreferanse.id}/${filreferanse.nr}"
         else -> throw RuntimeException("Noe uventet feilet. JsonFilreferanse på annet format enn JsonDokumentlagerFilreferanse og JsonSvarUtFilreferanse")
     }
+}
+
+fun toLocalDateTime(hendelsetidspunkt: String): LocalDateTime {
+    return LocalDateTime.parse(hendelsetidspunkt, DateTimeFormatter.ISO_DATE_TIME)
+}
+
+fun unixToLocalDateTime(tidspunkt: Long): LocalDateTime {
+    return LocalDateTime.ofInstant(Instant.ofEpochMilli(tidspunkt), ZoneOffset.UTC)
 }

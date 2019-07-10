b0d769513ca05935a277bd17fa644730d7005fbe
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-07-10 10:56:09 +0200

    Endepunkt for opplasting av vedlegg - start

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/VedleggController.kt
@@ -0,0 +1,31 @@
+package no.nav.sbl.sosialhjelpinnsynapi.rest
+
+import no.nav.sbl.sosialhjelpinnsynapi.domain.VedleggOpplastingResponse
+import no.nav.sbl.sosialhjelpinnsynapi.vedlegg.VedleggService
+import no.nav.security.oidc.api.Unprotected
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+
+@Unprotected
+@RestController
+@RequestMapping("/api/v1/innsyn")
+class VedleggController(private val vedleggService: VedleggService) {
+
+    @PostMapping("/{fiksDigisosId}/vedlegg", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun lastOppVedlegg(@PathVariable fiksDigisosId: String, @RequestParam("file") files: Array<MultipartFile>): ResponseEntity<VedleggOpplastingResponse> {
+        // Sjekk om fileSize overskrider MAKS_FILSTORRELSE
+
+        files.forEach { println("file name: ${it.name}") }
+
+        val bytes = files[0].bytes
+        val inputStream = files[0].inputStream
+
+        // hva bør input være? inputStream / bytes / files ?
+        val response = vedleggService.handleVedlegg(fiksDigisosId, files)
+
+        return ResponseEntity.ok(response)
+    }
+
+}
---
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/vedlegg/VedleggService.kt
@@ -0,0 +1,15 @@
+package no.nav.sbl.sosialhjelpinnsynapi.vedlegg
+
+import no.nav.sbl.sosialhjelpinnsynapi.domain.VedleggOpplastingResponse
+import no.nav.sbl.sosialhjelpinnsynapi.fiks.FiksClient
+import org.springframework.stereotype.Component
+
+@Component
+class VedleggService(private val fiksClient: FiksClient) {
+
+    fun handleVedlegg(fiksDigisosId: String, file: Any): VedleggOpplastingResponse {
+
+        // Kall fiksClient.lastOppNyEttersendlse(...)
+        return VedleggOpplastingResponse("a", 1337)
+    }
+}

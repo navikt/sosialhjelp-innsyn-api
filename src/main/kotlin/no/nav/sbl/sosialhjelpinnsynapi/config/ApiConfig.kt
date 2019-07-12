10245282ad7d752578e7de562574561e1116cd61
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-07-12 13:40:48 +0200

    Endret fra å bruke `addCorsMappings(...)` til å lage `corsConfigurationSource`-bønne med likt innhold

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/ApiConfig.kt
@@ -1,46 +1,44 @@
 package no.nav.sbl.sosialhjelpinnsynapi.config
 
 import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
-//@EnableWebMvc
-@Configuration
-class ApiConfig(private val corsProperties: CorsProperties) : WebMvcConfigurer {
-
-    override fun addCorsMappings(registry: CorsRegistry) {
-
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOrigins(*corsProperties.allowedOrigins)
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(true)
-    }
-}
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 @EnableOIDCTokenValidation(ignore = ["org.springframework"])
-//@Import(ApiConfig::class)
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig(private val corsProperties: CorsProperties) : WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         http.cors()
 //        http.csrf().disable()
     }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf(*corsProperties.allowedOrigins)
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("Origin", "Content-Type", "Accept", "Authorization")
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
 }
 
 @Profile("mock")
 @Order(-1)
 @Configuration
-//@Import(ApiConfig::class)
 class WebSecurityMockConfig : WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)

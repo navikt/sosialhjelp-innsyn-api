0e336e0788cb795d87b5a4b3b535d8d870a41b8a
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-07-29 09:54:34 +0200

    mock holder nå på opplastede vedlegg i state

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/VedleggOpplastingServiceMock.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/VedleggOpplastingServiceMock.kt
@@ -6,6 +6,7 @@ import no.nav.sbl.sosialhjelpinnsynapi.vedleggopplasting.VedleggOpplastingServic
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
+import java.util.*
 
 @Profile("mock")
 @Component
@@ -14,7 +15,7 @@ class VedleggOpplastingServiceMock(private val fiksClient: FiksClient) : Vedlegg
     private val vedleggMap = mutableMapOf<String, List<ByteArray>>()
 
     override fun mellomlagreVedlegg(fiksDigisosId: String, files: List<Any>): List<VedleggOpplastingResponse> {
-        val arrayOfMultipartFiles = files as Array<MultipartFile>
+        val arrayOfMultipartFiles = files as LinkedList<MultipartFile>
 
         // knytte (1..n) vedlegg til fiksDigisosId
         for (file in arrayOfMultipartFiles) {
@@ -28,7 +29,7 @@ class VedleggOpplastingServiceMock(private val fiksClient: FiksClient) : Vedlegg
         return arrayOfMultipartFiles.map { VedleggOpplastingResponse(it.originalFilename, it.size) }
     }
 
-    override fun lastOppVedleggTilFiks(fiksDigisosId: String): String {
+    override fun sendVedleggTilFiks(fiksDigisosId: String): String {
         // Hent digisosSak
         val digisosSak = fiksClient.hentDigisosSak(fiksDigisosId, "token")
         val kommunenummer = digisosSak.kommunenummer
@@ -36,8 +37,8 @@ class VedleggOpplastingServiceMock(private val fiksClient: FiksClient) : Vedlegg
         // Hent ut vedlegg fra mellomlagring
         val files: List<ByteArray> = vedleggMap[fiksDigisosId]!!
 
-        files.forEach{ fiksClient.lastOppNyEttersendelse(it, kommunenummer, fiksDigisosId, "token") }
+        files.forEach { fiksClient.lastOppNyEttersendelse(it, kommunenummer, fiksDigisosId, "token") }
 
-        return "OK"
+        return "OK - sendt til Fiks (mock)"
     }
 }

6ad2fd2e03d4105ae438ba56f5ca0f25e74efd89
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-07-30 14:09:35 +0200

    Flere itester for vedleggoversikt

--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/itest/VedleggOversiktIT.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/itest/VedleggOversiktIT.kt
@@ -3,6 +3,7 @@ package no.nav.sbl.sosialhjelpinnsynapi.itest
 import com.github.tomakehurst.wiremock.client.WireMock
 import no.nav.sbl.sosialhjelpinnsynapi.domain.VedleggResponse
 import no.nav.sbl.sosialhjelpinnsynapi.fiks.typeRef
+import no.nav.sbl.sosialhjelpinnsynapi.unixToLocalDateTime
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpEntity
@@ -11,11 +12,35 @@ import org.springframework.http.HttpStatus
 
 class VedleggOversiktIT : AbstractIT() {
 
-
     @Test
     fun `GET vedlegg - happy path`() {
-        WireMock.stubFor(WireMock.get(WireMock.urlPathMatching("/dokumentlager/nedlasting/3fa85f64-5717-4562-b3fc-2c963f66afa1"))
-                .willReturn(WireMock.ok("/dokumentlager/digisossoker_ok_komplett.json".asResource())))
+        val id = "123"
+        val responseEntity = testRestTemplate.exchange("/api/v1/innsyn/$id/vedlegg", HttpMethod.GET, HttpEntity<Nothing>(getHeaders()), typeRef<List<VedleggResponse>>())
+
+        assertThat(responseEntity).isNotNull
+        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(responseEntity.body).isNotNull
+
+        val vedleggList = responseEntity.body as List<VedleggResponse>
+        assertThat(vedleggList).hasSize(1)
+    }
+
+    @Test
+    fun `GET vedlegg - no content`() {
+        WireMock.stubFor(WireMock.get(WireMock.urlPathMatching("/digisos/api/v1/soknader/(.*)"))
+                .willReturn(WireMock.ok("/digisossak/ok_ingen_ettersendelser.json".asResource())))
+
+        val id = "123"
+        val responseEntity = testRestTemplate.exchange("/api/v1/innsyn/$id/vedlegg", HttpMethod.GET, HttpEntity<Nothing>(getHeaders()), typeRef<List<VedleggResponse>>())
+
+        assertThat(responseEntity).isNotNull
+        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.NO_CONTENT)
+    }
+
+    @Test
+    fun `GET vedlegg - flere ettersendelser`() {
+        WireMock.stubFor(WireMock.get(WireMock.urlPathMatching("/digisos/api/v1/soknader/(.*)"))
+                .willReturn(WireMock.ok("/digisossak/ok_flere_ettersendelser.json".asResource())))
 
         val id = "123"
         val responseEntity = testRestTemplate.exchange("/api/v1/innsyn/$id/vedlegg", HttpMethod.GET, HttpEntity<Nothing>(getHeaders()), typeRef<List<VedleggResponse>>())
@@ -24,8 +49,19 @@ class VedleggOversiktIT : AbstractIT() {
         assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(responseEntity.body).isNotNull
 
-        val oppgaver = responseEntity.body as List<VedleggResponse>
-        assertThat(oppgaver).hasSize(1)
-    }
+        val vedleggList = responseEntity.body as List<VedleggResponse>
+        assertThat(vedleggList).hasSize(2)
 
+        assertThat(vedleggList[0].datoLagtTil).isEqualToIgnoringNanos(unixToLocalDateTime(1546473600000))
+        assertThat(vedleggList[0].filnavn).isEqualTo("fil 1")
+        assertThat(vedleggList[0].storrelse).isEqualTo(1337)
+        assertThat(vedleggList[0].url).contains("3fa85f64-5717-4562-b3fc-bbb111bbb222")
+        assertThat(vedleggList[0].beskrivelse).isEqualTo("beskrivelse") // endres
+
+        assertThat(vedleggList[1].datoLagtTil).isEqualToIgnoringNanos(unixToLocalDateTime(1546300800000))
+        assertThat(vedleggList[1].filnavn).isEqualTo("fil 2")
+        assertThat(vedleggList[1].storrelse).isEqualTo(42)
+        assertThat(vedleggList[1].url).contains("3fa85f64-5717-4562-b3fc-bbb111bbb333")
+        assertThat(vedleggList[1].beskrivelse).isEqualTo("beskrivelse") // endres
+    }
 }

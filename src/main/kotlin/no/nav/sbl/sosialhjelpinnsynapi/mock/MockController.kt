722e4d96e264473f6ddf28ebc86dbe37e929eaa3
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-07-31 10:28:49 +0200

    Flytter Innsyn-controller til mockController (da dette kallet kun bør eksistere i mock evt lokalt)

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/MockController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/MockController.kt
@@ -3,23 +3,30 @@ package no.nav.sbl.sosialhjelpinnsynapi.mock
 import com.fasterxml.jackson.databind.JsonNode
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonDigisosSoker
 import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpObjectMapper
+import no.nav.sbl.sosialhjelpinnsynapi.innsyn.InnsynService
 import no.nav.security.oidc.api.Unprotected
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @Profile("mock")
 @Unprotected
 @RestController
-@RequestMapping("/api/v1/mock")
-class MockController(val fiksClientMock: FiksClientMock, val dokumentlagerClientMock: DokumentlagerClientMock) {
+@RequestMapping("/api/v1/mock/innsyn")
+class MockController(private val fiksClientMock: FiksClientMock,
+                     private val dokumentlagerClientMock: DokumentlagerClientMock,
+                     private val innsynService: InnsynService) {
 
     private val log = LoggerFactory.getLogger(this.javaClass)
 
     private val mapper = JsonSosialhjelpObjectMapper.createObjectMapper()
 
-    @PostMapping("/innsyn/{soknadId}",
+    @PostMapping("/{soknadId}",
             consumes = [APPLICATION_JSON_UTF8_VALUE],
             produces = [APPLICATION_JSON_UTF8_VALUE])
     fun postJsonDigisosSoker(@PathVariable soknadId: String, @RequestBody jsonDigisosSoker: JsonDigisosSoker) {
@@ -28,5 +35,16 @@ class MockController(val fiksClientMock: FiksClientMock, val dokumentlagerClient
         val jsonNode = mapper.convertValue(jsonDigisosSoker, JsonNode::class.java)
         val tilbakeTilJsonDigisosSoker = mapper.convertValue(jsonNode, JsonDigisosSoker::class.java)
         digisosSak.digisosSoker?.metadata?.let { dokumentlagerClientMock.postDokument(it, tilbakeTilJsonDigisosSoker) }
+    }
+
+    @GetMapping("/{soknadId}",
+            produces = [APPLICATION_JSON_UTF8_VALUE])
+    fun getInnsynForSoknad(@PathVariable soknadId: String, @RequestHeader(value = HttpHeaders.AUTHORIZATION) token: String): ResponseEntity<JsonDigisosSoker> {
+        try {
+            val jsonDigisosSoker = innsynService.hentJsonDigisosSoker(soknadId, token)
+            return ResponseEntity.ok(jsonDigisosSoker!!)
+        } catch (e: Exception) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        }
     }
 }

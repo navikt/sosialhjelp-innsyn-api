716efbe962f204b5b931d229ba5c4e8d834a4a5e
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-08-05 12:49:54 +0200

    Lagt til summert utbetaling per måned. Venter fremdeles på endelig besluttning for utseende og datastruktur.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utbetalinger/UtbetalingerService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utbetalinger/UtbetalingerService.kt
@@ -1,9 +1,18 @@
 package no.nav.sbl.sosialhjelpinnsynapi.utbetalinger
 
+import no.nav.sbl.sosialhjelpinnsynapi.domain.Utbetaling
+import no.nav.sbl.sosialhjelpinnsynapi.domain.UtbetalingResponse
+import no.nav.sbl.sosialhjelpinnsynapi.domain.UtbetalingerManedResponse
 import no.nav.sbl.sosialhjelpinnsynapi.domain.UtbetalingerResponse
 import no.nav.sbl.sosialhjelpinnsynapi.event.EventService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.time.Month
+import java.time.format.TextStyle
+import java.util.*
+import java.util.stream.Collectors
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 private val log = LoggerFactory.getLogger(UtbetalingerService::class.java)
 
@@ -17,22 +26,23 @@ class UtbetalingerService(private val eventService: EventService) {
             log.info("Fant ingen saker for $fiksDigisosId")
             return UtbetalingerResponse(mutableListOf())
         }
-        return UtbetalingerResponse(mutableListOf())
-//
-//        val utbetalingerResponse = UtbetalingerResponse(mutableListOf())
-//
-//        val utbetalingerPerManed = model.saker.stream().flatMap { sak -> sak.utbetalinger.stream() }.collect(Collectors.groupingBy { t -> t.fom.month })
-//
-//        for (mutableEntry in utbetalingerPerManed) {
-//            val key = mutableEntry.key
-//            val tittel = key.getDisplayName(TextStyle.FULL, Locale.ENGLISH)
-//
-//
-//            val utbetalinger = mutableEntry.value.map { utbetaling -> UtbetalingResponse("", utbetaling.belop.toDouble(), utbetaling.utbetalingsDato) }
-//           utbetalingerResponse.maned.add( UtbetalingerManedResponse(tittel, utbetalinger.toMutableList(), mutableEntry.value.stream().map { t -> t.belop}.reduce { t, u ->  t.plus(u)}.get().toDouble()))
-//        }
-//
-//        return  utbetalingerResponse
+
+        val utbetalingerResponse = UtbetalingerResponse(mutableListOf())
+
+        val utbetalingerPerManed = HashMap<Month, MutableList<Utbetaling>>().withDefault { key -> ArrayList() }
+        model.saker.stream().flatMap { t -> t.utbetalinger.stream() }.collect(Collectors.toList()).forEach { utbetaling ->
+            utbetalingerPerManed[utbetaling.fom.month]!!.add(utbetaling)
+        }
+        for (utbetalinger in utbetalingerPerManed.entries) {
+            val key = utbetalinger.key
+            val tittel = key.getDisplayName(TextStyle.FULL, Locale.ENGLISH)
+
+
+            val utbetalinger = utbetalinger.value.map { utbetaling -> UtbetalingResponse("", utbetaling.belop.toDouble(), utbetaling.utbetalingsDato) }
+            utbetalingerResponse.maned.add(UtbetalingerManedResponse(tittel, utbetalinger.toMutableList(), utbetalinger.stream().map { t -> t.belop }.reduce { t, u -> t.plus(u) }.get()))
+        }
+
+        return utbetalingerResponse
     }
 
 }

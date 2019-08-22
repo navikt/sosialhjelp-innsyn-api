c63ad910232ba26c5657db45ba61c573fbb07206
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-08-22 15:40:52 +0200

    Optional queryparameter

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/DigisosApiController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/DigisosApiController.kt
@@ -5,15 +5,18 @@ import no.nav.sbl.sosialhjelpinnsynapi.digisosapi.DigisosApiService
 import no.nav.security.oidc.api.Unprotected
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Unprotected
 @RestController
 @RequestMapping("/api/v1/digisosapi/")
 class DigisosApiController(val digisosApiService: DigisosApiService) {
 
-    @PostMapping("/oppdaterDigisosSak/{fiksDigisosId}", consumes = [APPLICATION_JSON_UTF8_VALUE], produces = [APPLICATION_JSON_UTF8_VALUE])
-    fun oppdaterDigisosSak(@PathVariable fiksDigisosId:String?, @RequestBody jsonDigisosSoker: JsonDigisosSoker): ResponseEntity<String> {
+    @PostMapping("/oppdaterDigisosSak", consumes = [APPLICATION_JSON_UTF8_VALUE], produces = [APPLICATION_JSON_UTF8_VALUE])
+    fun oppdaterDigisosSak(fiksDigisosId:String?, @RequestBody jsonDigisosSoker: JsonDigisosSoker): ResponseEntity<String> {
         val id =  digisosApiService.oppdaterDigisosSak(fiksDigisosId, jsonDigisosSoker)
 
         return ResponseEntity.ok("{\"fiksDigisosId\":$id}")

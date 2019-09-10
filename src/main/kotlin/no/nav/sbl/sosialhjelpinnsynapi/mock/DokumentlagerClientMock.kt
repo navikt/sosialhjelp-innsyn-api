886210882117413b1dd1b54c89f397272f170493
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-09-10 15:52:28 +0200

    Bygger opp default-response for mock som objekt fremfor json

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/DokumentlagerClientMock.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/DokumentlagerClientMock.kt
@@ -5,6 +5,10 @@ import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpObjectMapper
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad
 import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonVedleggSpesifikasjon
 import no.nav.sbl.sosialhjelpinnsynapi.fiks.DokumentlagerClient
+import no.nav.sbl.sosialhjelpinnsynapi.mock.responses.digisosSoker
+import no.nav.sbl.sosialhjelpinnsynapi.mock.responses.jsonVedleggSpesifikasjon
+import no.nav.sbl.sosialhjelpinnsynapi.mock.responses.jsonVedleggSpesifikasjonEttersendelse
+import no.nav.sbl.sosialhjelpinnsynapi.mock.responses.jsonVedleggSpesifikasjonEttersendelse_2
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
@@ -14,31 +18,16 @@ class DokumentlagerClientMock : DokumentlagerClient {
 
     private val dokumentMap = mutableMapOf<String, Any>()
     private val mapper = JsonSosialhjelpObjectMapper.createObjectMapper()
-    private val jsonDigisosSoker: String = this.javaClass.classLoader
-            .getResourceAsStream("mock/json_digisos_soker.json")
-            .bufferedReader().use { it.readText() }
 
     private val jsonSoknad: String = this.javaClass.classLoader
-            .getResourceAsStream("mock/json_soknad.json")
-            .bufferedReader().use { it.readText() }
-
-    private val jsonVedleggSpesifikasjon: String = this.javaClass.classLoader
-            .getResourceAsStream("mock/json_vedlegg_spesifikasjon.json")
-            .bufferedReader().use { it.readText() }
-
-    private val jsonVedleggSpesifikasjonEttersendelse: String = this.javaClass.classLoader
-            .getResourceAsStream("mock/json_vedlegg_spesifikasjon_ettersendelse.json")
-            .bufferedReader().use { it.readText() }
-
-    private val jsonVedleggSpesifikasjonEttersendelse_2: String = this.javaClass.classLoader
-            .getResourceAsStream("mock/json_vedlegg_spesifikasjon_ettersendelse_2.json")
+            .getResourceAsStream("mock/json_soknad.json")!!
             .bufferedReader().use { it.readText() }
 
 
     override fun hentDokument(dokumentlagerId: String, requestedClass: Class<out Any>): Any {
         return when (requestedClass) {
             JsonDigisosSoker::class.java -> dokumentMap.getOrElse(dokumentlagerId, {
-                val default = getDefaultJsonDigisosSoker()
+                val default = digisosSoker
                 dokumentMap[dokumentlagerId] = default
                 default
             })
@@ -50,19 +39,19 @@ class DokumentlagerClientMock : DokumentlagerClient {
             JsonVedleggSpesifikasjon::class.java ->
                 if (dokumentlagerId == "mock-soknad-vedlegg-metadata") {
                     dokumentMap.getOrElse(dokumentlagerId, {
-                        val default = getDefaultJsonVedleggSpesifikasjon(jsonVedleggSpesifikasjon)
+                        val default = jsonVedleggSpesifikasjon
                         dokumentMap[dokumentlagerId] = default
                         default
                     })
                 } else if (dokumentlagerId == "mock-ettersendelse-vedlegg-metadata"){
                     dokumentMap.getOrElse(dokumentlagerId, {
-                        val default = getDefaultJsonVedleggSpesifikasjon(jsonVedleggSpesifikasjonEttersendelse)
+                        val default = jsonVedleggSpesifikasjonEttersendelse
                         dokumentMap[dokumentlagerId] = default
                         default
                     })
                 } else {
                     dokumentMap.getOrElse(dokumentlagerId, {
-                        val default = getDefaultJsonVedleggSpesifikasjon(jsonVedleggSpesifikasjonEttersendelse_2)
+                        val default = jsonVedleggSpesifikasjonEttersendelse_2
                         dokumentMap[dokumentlagerId] = default
                         default
                     })
@@ -75,15 +64,7 @@ class DokumentlagerClientMock : DokumentlagerClient {
         dokumentMap[dokumentlagerId] = jsonDigisosSoker
     }
 
-    private fun getDefaultJsonDigisosSoker(): JsonDigisosSoker {
-        return mapper.readValue(jsonDigisosSoker, JsonDigisosSoker::class.java)
-    }
-
     private fun getDefaultJsonSoknad(): JsonSoknad {
         return mapper.readValue(jsonSoknad, JsonSoknad::class.java)
-    }
-
-    private fun getDefaultJsonVedleggSpesifikasjon(vedleggSpesifikasjon: String): JsonVedleggSpesifikasjon {
-        return mapper.readValue(vedleggSpesifikasjon, JsonVedleggSpesifikasjon::class.java)
     }
 }

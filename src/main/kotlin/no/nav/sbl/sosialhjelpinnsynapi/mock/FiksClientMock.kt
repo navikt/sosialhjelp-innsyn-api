1c7441769791b46d8081b6a0ca4d21a811f75383
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-09-13 10:34:31 +0200

    Default response for DigisosSak i mock

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/FiksClientMock.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/FiksClientMock.kt
@@ -1,27 +1,21 @@
 package no.nav.sbl.sosialhjelpinnsynapi.mock
 
-import com.fasterxml.jackson.databind.node.ObjectNode
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.sbl.sosialhjelpinnsynapi.domain.DigisosSak
 import no.nav.sbl.sosialhjelpinnsynapi.domain.KommuneInfo
 import no.nav.sbl.sosialhjelpinnsynapi.fiks.FiksClient
+import no.nav.sbl.sosialhjelpinnsynapi.mock.responses.defaultDigisosSak
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Profile("mock")
 @Component
 class FiksClientMock : FiksClient {
 
     private val innsynMap = mutableMapOf<String, DigisosSak>()
-    private val mapper = jacksonObjectMapper()
-    private val digisosSak: String = this.javaClass.classLoader
-            .getResourceAsStream("mock/digisos_sak.json")
-            .bufferedReader().use { it.readText() }
 
     override fun hentDigisosSak(digisosId: String, token: String): DigisosSak {
         return innsynMap.getOrElse(digisosId, {
-            val default = getDefaultDigisosSak()
+            val default = defaultDigisosSak
             innsynMap[digisosId] = default
             default
         })
@@ -41,13 +35,5 @@ class FiksClientMock : FiksClient {
 
     fun postDigisosSak(digisosSak: DigisosSak) {
         innsynMap[digisosSak.fiksDigisosId] = digisosSak
-    }
-
-    private fun getDefaultDigisosSak(): DigisosSak {
-        val tree = mapper.readTree(digisosSak)
-        val node = tree["digisosSoker"] as ObjectNode
-        // Generer ny dokumentlagerId for å ikke overskrive dokumenter på andre digisosId-er
-        node.put("metadata", UUID.randomUUID().toString())
-        return mapper.treeToValue(tree, DigisosSak::class.java)
     }
 }

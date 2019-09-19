bf07940687938a5b6a6b32ed428e6b8e5965b74b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-09-19 15:54:17 +0200

    Flytter logikk til DigisosApiService

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/DigisosApiController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/DigisosApiController.kt
@@ -3,17 +3,22 @@ package no.nav.sbl.sosialhjelpinnsynapi.rest
 import no.nav.sbl.sosialhjelpinnsynapi.digisosapi.DigisosApiService
 import no.nav.sbl.sosialhjelpinnsynapi.utils.DigisosApiWrapper
 import no.nav.security.oidc.api.Unprotected
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Unprotected
 @RestController
 @RequestMapping("/api/v1/digisosapi/")
-class DigisosApiController(val digisosApiService: DigisosApiService) {
+class DigisosApiController(private val digisosApiService: DigisosApiService) {
+
+    @GetMapping("/kommuneInfo/{kommunenummer}")
+    fun hentKommuneInfo(@PathVariable kommunenummer: String, @RequestHeader(value = HttpHeaders.AUTHORIZATION) token: String): ResponseEntity<String>{
+        val kommuneStatus = digisosApiService.hentKommuneStatus(kommunenummer, token)
+
+        return ResponseEntity.ok(kommuneStatus.toString())
+    }
 
     @PostMapping("/oppdaterDigisosSak", consumes = [APPLICATION_JSON_UTF8_VALUE], produces = [APPLICATION_JSON_UTF8_VALUE])
     fun oppdaterDigisosSak(fiksDigisosId: String?, @RequestBody digisosApiWrapper: DigisosApiWrapper): ResponseEntity<String> {

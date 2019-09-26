39925bda2228dcc257ffac87c2916e3eede0900b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-09-26 13:12:05 +0200

    rydder i EventService og InnsynService. Unngår unødvendige ekstrakall til Fiks

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/innsyn/InnsynService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/innsyn/InnsynService.kt
@@ -8,23 +8,17 @@ import org.springframework.stereotype.Component
 @Component
 class InnsynService(private val fiksClient: FiksClient) {
 
-    fun hentJsonDigisosSoker(soknadId: String, token: String): JsonDigisosSoker? {
-        val digisosSak = fiksClient.hentDigisosSak(soknadId, token)
-        return if (digisosSak.digisosSoker != null) {
-            fiksClient.hentDokument(soknadId, digisosSak.digisosSoker.metadata, JsonDigisosSoker::class.java, token) as JsonDigisosSoker
-        } else {
-            null
+    fun hentJsonDigisosSoker(digisosId: String, digisosSokerMetadata: String?, token: String): JsonDigisosSoker? {
+        return when {
+            digisosSokerMetadata != null -> fiksClient.hentDokument(digisosId, digisosSokerMetadata, JsonDigisosSoker::class.java, token) as JsonDigisosSoker
+            else -> null
         }
     }
 
-    fun hentOriginalSoknad(soknadId: String, token: String): JsonSoknad {
-        val digisosSak = fiksClient.hentDigisosSak(soknadId, token)
-        return fiksClient.hentDokument(soknadId, digisosSak.originalSoknadNAV!!.metadata, JsonSoknad::class.java, token) as JsonSoknad
-    }
-
-    // Returnerer UNIX tid med millisekunder
-    fun hentInnsendingstidspunktForOriginalSoknad(soknadId: String, token: String): Long {
-        val digisosSak = fiksClient.hentDigisosSak(soknadId, token)
-        return digisosSak.originalSoknadNAV!!.timestampSendt
+    fun hentOriginalSoknad(digisosId: String, originalSoknadNAVMetadata: String?, token: String): JsonSoknad? {
+        return when {
+            originalSoknadNAVMetadata != null -> fiksClient.hentDokument(digisosId, originalSoknadNAVMetadata, JsonSoknad::class.java, token) as JsonSoknad
+            else -> null
+        }
     }
 }

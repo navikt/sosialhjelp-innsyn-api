10a0b222256c7eb4d2be12185bd11483b7cea9ba
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-01 16:51:34 +0200

    prøver å fjerne consumerId

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/health/checks/NorgCheck.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/health/checks/NorgCheck.kt
@@ -29,7 +29,7 @@ class NorgCheck(private val restTemplate: RestTemplate,
             val norgApiKey = System.getProperty("NORG_PASSWORD")
             val headers = HttpHeaders()
             headers.set("Nav-Call-Id", generateCallId())
-            headers.set("Nav-Consumer-Id", "srvsosialhjelp-inn")
+//            headers.set("Nav-Consumer-Id", "srvsosialhjelp-inn")
             headers.set("x-nav-apiKey", norgApiKey)
 
             // samme kall som selftest i soknad-api utfører

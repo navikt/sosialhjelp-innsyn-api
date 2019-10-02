ca8d916b8f068788c1842d28e833d56707bc8456
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-02 16:06:43 +0200

    henter kommuneinfo ut fra fiksDigisosId

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
@@ -1,6 +1,9 @@
 package no.nav.sbl.sosialhjelpinnsynapi.kommune
 
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad
+import no.nav.sbl.sosialhjelpinnsynapi.domain.KommuneInfo
 import no.nav.sbl.sosialhjelpinnsynapi.fiks.FiksClient
+import no.nav.sbl.sosialhjelpinnsynapi.innsyn.InnsynService
 import no.nav.sbl.sosialhjelpinnsynapi.kommune.KommuneStatus.IKKE_INNSYN
 import no.nav.sbl.sosialhjelpinnsynapi.kommune.KommuneStatus.INNSYN
 import org.slf4j.Logger
@@ -8,16 +11,27 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
-class KommuneService(private val fiksClient: FiksClient) {
+class KommuneService(private val fiksClient: FiksClient,
+                     private val innsynService: InnsynService) {
 
     private val log: Logger = LoggerFactory.getLogger(KommuneService::class.java)
 
-    fun hentKommuneStatus(kommunenummer: String): KommuneStatus {
-        val kommuneInfo = fiksClient.hentKommuneInfo(kommunenummer)
+    fun hentKommuneStatus(fiksDigisosId: String, token: String): KommuneStatus {
+        val digisosSak = fiksClient.hentDigisosSak(fiksDigisosId, token)
+
+        val originalSoknad: JsonSoknad? = innsynService.hentOriginalSoknad(fiksDigisosId, digisosSak.originalSoknadNAV?.metadata, token)
+
+        val kommunenummer: String? = originalSoknad?.mottaker?.kommunenummer
+        if (kommunenummer == null) {
+            log.warn("Forsøkte å hente kommuneStatus, men JsonSoknad.mottaker.kommunenummer finnes ikke")
+            throw RuntimeException("KommuneStatus kan ikke hentes uten kommunenummer")
+        }
+
+        val kommuneInfo: KommuneInfo? = fiksClient.hentKommuneInfo(kommunenummer)
 
         return when {
-            !kommuneInfo.kanOppdatereStatus -> IKKE_INNSYN
-            kommuneInfo.kanOppdatereStatus -> INNSYN
+            kommuneInfo != null && !kommuneInfo.kanOppdatereStatus -> IKKE_INNSYN
+            kommuneInfo != null && kommuneInfo.kanOppdatereStatus -> INNSYN
             else -> {
                 log.warn("Forsøkte å hente kommunestatus, men scenariet er ikke dekket")
                 throw RuntimeException("KommuneStatus scenario er ikke dekket")
--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneServiceTest.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneServiceTest.kt
@@ -3,42 +3,63 @@ package no.nav.sbl.sosialhjelpinnsynapi.kommune
 import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
+import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad
+import no.nav.sbl.sosialhjelpinnsynapi.domain.DigisosSak
 import no.nav.sbl.sosialhjelpinnsynapi.domain.KommuneInfo
 import no.nav.sbl.sosialhjelpinnsynapi.fiks.FiksClient
+import no.nav.sbl.sosialhjelpinnsynapi.innsyn.InnsynService
 import no.nav.sbl.sosialhjelpinnsynapi.kommune.KommuneStatus.IKKE_INNSYN
 import no.nav.sbl.sosialhjelpinnsynapi.kommune.KommuneStatus.INNSYN
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class KommuneServiceTest {
 
     private val fiksClient: FiksClient = mockk()
+    private val innsynService: InnsynService = mockk()
 
-    private val service = KommuneService(fiksClient)
+    private val service = KommuneService(fiksClient, innsynService)
 
+    private val mockDigisosSak: DigisosSak = mockk()
+    private val mockJsonSoknad: JsonSoknad = mockk()
     private val kommuneNr = "1234"
 
     @BeforeEach
     internal fun setUp() {
-        clearMocks(fiksClient)
+        clearMocks(fiksClient, innsynService, mockDigisosSak, mockJsonSoknad)
+
+        every { fiksClient.hentDigisosSak(any(), any()) } returns mockDigisosSak
+        every { mockDigisosSak.originalSoknadNAV?.metadata }  returns "some id"
+        every { innsynService.hentOriginalSoknad(any(), any(), any()) } returns mockJsonSoknad
+        every { mockJsonSoknad.mottaker.kommunenummer } returns kommuneNr
     }
 
     @Test
-    fun `Kommune på FIKS men ikke på INNSYN`() {
+    fun `Kommune kan ikke oppdatere status gir IKKE_INNSYN`() {
         every { fiksClient.hentKommuneInfo(any()) } returns KommuneInfo(kommuneNr, kanMottaSoknader = true, kanOppdatereStatus = false, kontaktPersoner = null)
 
-        val status = service.hentKommuneStatus(kommuneNr)
+        val status = service.hentKommuneStatus("123", "token")
 
         assertThat(status).isEqualTo(IKKE_INNSYN)
     }
 
     @Test
-    fun `Kommune på FIKS og INNSYN`() {
+    fun `Kommune kan oppdatere status gir INNSYN`() {
         every { fiksClient.hentKommuneInfo(any()) } returns KommuneInfo(kommuneNr, kanMottaSoknader = true, kanOppdatereStatus = true, kontaktPersoner = null)
 
-        val status = service.hentKommuneStatus(kommuneNr)
+        val status = service.hentKommuneStatus("123", "token")
 
         assertThat(status).isEqualTo(INNSYN)
+    }
+
+    @Test
+    fun `Ingen originalSoknad - skal kaste feil`() {
+        every { mockDigisosSak.originalSoknadNAV?.metadata }  returns null
+        every { innsynService.hentOriginalSoknad(any(), any(), any()) } returns null
+
+        assertThatExceptionOfType(RuntimeException::class.java).isThrownBy { service.hentKommuneStatus("123", "token") }
+                .withMessage("KommuneStatus kan ikke hentes uten kommunenummer")
     }
 }

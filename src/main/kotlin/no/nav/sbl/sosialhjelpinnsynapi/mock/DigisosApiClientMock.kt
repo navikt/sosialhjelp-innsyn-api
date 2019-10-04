bdfd4c20089034b8caa30217669ddb3c93db636a
Oskar Asplin <34914449+OskarAsplin@users.noreply.github.com>
2019-10-04 16:42:35 +0200

    Fikser deserialisering av digisosSoker (#49)

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/DigisosApiClientMock.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/DigisosApiClientMock.kt
@@ -3,7 +3,6 @@ package no.nav.sbl.sosialhjelpinnsynapi.mock
 import no.nav.sbl.sosialhjelpinnsynapi.digisosapi.DigisosApiClient
 import no.nav.sbl.sosialhjelpinnsynapi.domain.*
 import no.nav.sbl.sosialhjelpinnsynapi.utils.DigisosApiWrapper
-import no.nav.sbl.sosialhjelpinnsynapi.utils.objectMapper
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import java.util.*
@@ -21,7 +20,7 @@ class DigisosApiClientMock(private val fiksClientMock: FiksClientMock) : Digisos
 
         fiksClientMock.postDigisosSak(DigisosSak(id, "01234567890", "11415cd1-e26d-499a-8421-751457dfcbd5", "1", System.currentTimeMillis(),
                 OriginalSoknadNAV("110000000", "", "mock-soknad-vedlegg-metadata", DokumentInfo("", "", 0L), Collections.emptyList(), System.currentTimeMillis()),
-                EttersendtInfoNAV(Collections.emptyList()), DigisosSoker(objectMapper.writeValueAsString(digisosApiWrapper.sak), Collections.emptyList(), System.currentTimeMillis())))
+                EttersendtInfoNAV(Collections.emptyList()), DigisosSoker(dokumentlagerId, Collections.emptyList(), System.currentTimeMillis())))
         return id
     }
 }

c4d08b3084ed502f323159592570b3fbff0cedb0
Oskar Asplin <oskarasplin@gmail.com>
2019-10-04 16:38:54 +0200

    Fikser deserialisering av digisosSoker

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/RestConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/RestConfig.kt
@@ -1,9 +1,23 @@
 package no.nav.sbl.sosialhjelpinnsynapi.config
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonFilreferanse
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonHendelse
+import no.nav.sbl.soknadsosialhjelp.json.AdresseMixIn
+import no.nav.sbl.soknadsosialhjelp.json.FilreferanseMixIn
+import no.nav.sbl.soknadsosialhjelp.json.HendelseMixIn
+import no.nav.sbl.soknadsosialhjelp.soknad.adresse.JsonAdresse
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
 
 @Configuration
 class RestConfig {
@@ -11,5 +25,33 @@ class RestConfig {
     @Bean
     fun restTemplate(builder: RestTemplateBuilder): RestTemplate =
             builder.build()
+
+    @Bean
+    fun objectMapperCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
+        return Jackson2ObjectMapperBuilderCustomizer { jacksonObjectMapperBuilder ->
+            jacksonObjectMapperBuilder
+                    .mixIn(JsonHendelse::class.java, HendelseMixIn::class.java)
+                    .mixIn(JsonFilreferanse::class.java, FilreferanseMixIn::class.java)
+                    .mixIn(JsonAdresse::class.java, AdresseMixIn::class.java)}
+    }
+
+    @Bean
+    fun myMessageConverter(reqAdapter: RequestMappingHandlerAdapter,
+                           jacksonObjectMapperBuilder: Jackson2ObjectMapperBuilder): MappingJackson2HttpMessageConverter {
+        val mapper = jacksonObjectMapperBuilder
+                .featuresToEnable(SerializationFeature.INDENT_OUTPUT)
+                .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .modulesToInstall(JavaTimeModule())
+                .build<ObjectMapper>()
+
+        // **replace previous MappingJackson converter**
+        val converters = reqAdapter.messageConverters
+        converters.removeIf { httpMessageConverter -> httpMessageConverter.javaClass == MappingJackson2HttpMessageConverter::class.java }
+
+        val jackson = MappingJackson2HttpMessageConverter(mapper)
+        converters.add(jackson)
+        reqAdapter.messageConverters = converters
+        return jackson
+    }
 
 }

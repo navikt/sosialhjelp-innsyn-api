5076c92db3048023c0964ae098231116c989c248
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-07 12:29:50 +0200

    gjenbruker ObjectMapper fra filformat

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/Objects.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/Objects.kt
@@ -9,11 +9,18 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
+import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpObjectMapper
 import org.apache.http.HttpHost
 import org.slf4j.LoggerFactory
 
 private val log = LoggerFactory.getLogger(HttpClient::class.java)
+
 val objectMapper: ObjectMapper = jacksonObjectMapper()
+        .registerModule(JavaTimeModule())
+        .configure(SerializationFeature.INDENT_OUTPUT, true)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+val filformatObjectMapper = JsonSosialhjelpObjectMapper.createObjectMapper()
         .registerModule(JavaTimeModule())
         .configure(SerializationFeature.INDENT_OUTPUT, true)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)

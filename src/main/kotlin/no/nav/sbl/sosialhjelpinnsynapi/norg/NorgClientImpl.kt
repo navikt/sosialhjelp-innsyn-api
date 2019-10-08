cb6856ad1eb20d4e9aff9626680645f8c0f57db1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-08 11:06:55 +0200

    mer logging NorgClient

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/norg/NorgClientImpl.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/norg/NorgClientImpl.kt
@@ -27,6 +27,9 @@ class NorgClientImpl(clientProperties: ClientProperties,
         val headers = HttpHeaders()
         headers.set("Nav-Call-Id", generateCallId())
         headers.set("x-nav-apiKey", norgApiKey)
+
+        log.info("Forsøker å hente NAV-enhet $enhetsnr fra NORG2, url = $baseUrl/enhet/$enhetsnr")
+
         val response = restTemplate.exchange("$baseUrl/enhet/$enhetsnr", HttpMethod.GET, HttpEntity<Nothing>(headers), String::class.java)
         if (response.statusCode.is2xxSuccessful) {
             log.info("Hentet NAV-enhet fra NORG")

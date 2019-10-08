9bb68ca105026d7fd22f8a6d96fa919f741dfebb
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-08 13:55:50 +0200

    override val message

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/error/exceptions/FiksException.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/error/exceptions/FiksException.kt
@@ -2,6 +2,6 @@ package no.nav.sbl.sosialhjelpinnsynapi.error.exceptions
 
 import org.springframework.http.HttpStatus
 
-class FiksException(status: HttpStatus?, message: String?, override val cause: Throwable?): RuntimeException(message, cause) {
+class FiksException(status: HttpStatus?, override val message: String?, override val cause: Throwable?): RuntimeException(message, cause) {
 
 }

16c66441d59a9789123ecdade65b43b6f4a3ead0
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-10-11 09:25:47 +0200

    korrigerer feilmelding

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/common/InnsynExceptionHandler.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/common/InnsynExceptionHandler.kt
@@ -42,8 +42,8 @@ class InnsynExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(OpplastingException::class)
     fun handleOpplastingError(e: OpplastingException): ResponseEntity<ErrorMessage> {
-        log.error("Noe feilet ved opplasting til Fiks", e)
-        val error = ErrorMessage(fiksError, "Noe uventet feilet")
+        log.error("Noe feilet ved opplasting av vedlegg", e)
+        val error = ErrorMessage(unexpectedError, "Noe uventet feilet")
         return ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 

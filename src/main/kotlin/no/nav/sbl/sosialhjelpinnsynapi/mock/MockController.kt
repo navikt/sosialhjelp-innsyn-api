7e4612dee2dbb5e3e9994ce1a6d3c8b6c9daaf66
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-10-25 10:40:39 +0200

    Utvidet mockcontroller med innsyn i sakene

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/MockController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/MockController.kt
@@ -1,6 +1,7 @@
 package no.nav.sbl.sosialhjelpinnsynapi.mock
 
 import no.nav.sbl.sosialhjelpinnsynapi.domain.NavEnhet
+import no.nav.sbl.sosialhjelpinnsynapi.innsyn.InnsynService
 import no.nav.sbl.sosialhjelpinnsynapi.utils.objectMapper
 import no.nav.security.oidc.api.Unprotected
 import org.springframework.context.annotation.Profile
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.*
 @Unprotected
 @RestController
 @RequestMapping("/api/v1/mock")
-class MockController(private val norgClient: NorgClientMock, private val fiksClientMock: FiksClientMock) {
+class MockController(private val norgClient: NorgClientMock, private val fiksClientMock: FiksClientMock, private val innsynService: InnsynService) {
 
     @PostMapping("/nyNavEnhet", consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     fun oppdaterNavEnhetMock(@RequestBody nyeNavEnheter: List<NyNavEnhet>): ResponseEntity<String> {
@@ -33,6 +34,11 @@ class MockController(private val norgClient: NorgClientMock, private val fiksCli
     @GetMapping("/soknad/{id}", produces = [APPLICATION_JSON_VALUE])
     fun listSoknad(@PathVariable id: String): ResponseEntity<String> {
         return ResponseEntity.ok(objectMapper.writeValueAsString(fiksClientMock.hentAlleDigisosSaker("token").filter { it.fiksDigisosId == id }))
+    }
+
+    @GetMapping("/dokument/{fiksdigisosId}/{id}", produces = [APPLICATION_JSON_VALUE])
+    fun listSoknads(@PathVariable fiksdigisosId: String, @PathVariable id: String): ResponseEntity<String> {
+        return ResponseEntity.ok(objectMapper.writeValueAsString(innsynService.hentJsonDigisosSoker(fiksdigisosId, id, "token")))
     }
 }
 

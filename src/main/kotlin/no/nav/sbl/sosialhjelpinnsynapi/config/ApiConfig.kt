d0633cd8029498e7b0cf14a89aadc7b8162dda6e
siljee <silje.hansen3@nav.no>
2019-10-29 11:55:50 +0100

    Må legge på webSecurity igjen hvis ikke vil for en eller annen grunn ikke filteret bli aktivert.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/ApiConfig.kt
@@ -3,14 +3,25 @@ package no.nav.sbl.sosialhjelpinnsynapi.config
 import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @Configuration
+@EnableWebSecurity
 @EnableWebMvc
 @EnableOIDCTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
-class ApiConfig {
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.cors()
+        http.csrf().disable()
+    }
+
     @Bean
-    fun corsFilter(): CORSFilter {
+    fun navCorsFilter(): CORSFilter {
         return CORSFilter()
     }
 }

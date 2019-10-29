877339c73db56b5fc073bf01041846bc47cb0085
siljee <silje.hansen3@nav.no>
2019-10-29 15:45:39 +0100

    Rulle tilbake spring sikkerhet pga tull når man ikke går via login-api. Så akkurat nå er det to cors-sjekker. Forhåpentligvis får vi fjernet spring sin.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/ApiConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/config/ApiConfig.kt
@@ -3,9 +3,14 @@ package no.nav.sbl.sosialhjelpinnsynapi.config
 import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @Configuration
@@ -21,7 +26,36 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     }
 
     @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf(
+                "https://www.nav.no",
+                "https://www-q0.nav.no",
+                "https://www-q1.nav.no",
+                "http://localhost:3000",
+                "http://localhost:3001")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("Origin", "Content-Type", "Accept", "Authorization")
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
     fun navCorsFilter(): CORSFilter {
         return CORSFilter()
+    }
+}
+
+@Profile("mock")
+@Order(-1)
+@Configuration
+class WebSecurityMockConfig : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.csrf().ignoringAntMatchers("/api/v1/mock/**", "/api/v1/innsyn/**/vedlegg/lastOpp", "/api/v1/innsyn/**/vedlegg/send")
+        http.cors()
     }
 }

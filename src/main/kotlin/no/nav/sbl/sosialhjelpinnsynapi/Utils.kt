0e0d7a2811fecb9edeb7db94020bd00f3456f54e
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2019-11-05 16:45:33 +0100

    Post merge fixes.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/Utils.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/Utils.kt
@@ -14,6 +14,7 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.reflect.full.companionObject
 
 const val NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME"

1b8d722678daa1102440027ead64a73044b921b1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-11-18 13:26:58 +0100

    Idempotens for TildeltNavKontor - ingen duplikat-hendelser i historikk

---
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/ApplicationContextTest.kt
@@ -0,0 +1,20 @@
+package no.nav.sbl.sosialhjelpinnsynapi
+
+import com.ninjasquad.springmockk.MockkBean
+import no.nav.sbl.sosialhjelpinnsynapi.idporten.IdPortenService
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(classes = [TestApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class ApplicationContextTest {
+
+    @MockkBean
+    private lateinit var idPortenService: IdPortenService
+
+    @Test
+    fun `app skal starte`() {
+
+    }
+}

98e0d14db867ed4c1782033c6a96c32d06ceff27
Andre Wolden <wolden.andre@gmail.com>
2019-11-19 15:26:49 +0100

    endrer på kommune responsen

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
@@ -18,22 +18,7 @@ class KommuneService(private val fiksClient: FiksClient,
     }
 
     fun hentKommuneStatus(fiksDigisosId: String, token: String): KommuneStatus {
-        val digisosSak = fiksClient.hentDigisosSak(fiksDigisosId, token)
-
-        val originalSoknad: JsonSoknad? = innsynService.hentOriginalSoknad(fiksDigisosId, digisosSak.originalSoknadNAV?.metadata, token)
-
-        val kommunenummer: String? = originalSoknad?.mottaker?.kommunenummer
-        if (kommunenummer == null) {
-            log.warn("Forsøkte å hente kommuneStatus, men JsonSoknad.mottaker.kommunenummer finnes ikke")
-            throw RuntimeException("KommuneStatus kan ikke hentes uten kommunenummer")
-        }
-
-        val kommuneInfo: KommuneInfo
-        try {
-            kommuneInfo = fiksClient.hentKommuneInfo(kommunenummer)
-        } catch (e: FiksException) {
-            return MANGLER_KONFIGURASJON
-        }
+        val kommuneInfo = hentKommuneInfo(fiksDigisosId, token) ?: return MANGLER_KONFIGURASJON
 
         return when {
             !kommuneInfo.kanMottaSoknader && !kommuneInfo.kanOppdatereStatus && !kommuneInfo.harMidlertidigDeaktivertMottak && !kommuneInfo.harMidlertidigDeaktivertOppdateringer -> HAR_KONFIGURASJON_MEN_SKAL_SENDE_VIA_SVARUT
@@ -50,34 +35,54 @@ class KommuneService(private val fiksClient: FiksClient,
         }
     }
 
+    fun hentKommuneInfo(fiksDigisosId: String, token: String): KommuneInfo? {
+        val digisosSak = fiksClient.hentDigisosSak(fiksDigisosId, token)
+
+        val originalSoknad: JsonSoknad? = innsynService.hentOriginalSoknad(fiksDigisosId, digisosSak.originalSoknadNAV?.metadata, token)
+
+        val kommunenummer: String? = originalSoknad?.mottaker?.kommunenummer
+        if (kommunenummer == null) {
+            log.warn("Forsøkte å hente kommuneStatus, men JsonSoknad.mottaker.kommunenummer finnes ikke")
+            throw RuntimeException("KommuneStatus kan ikke hentes uten kommunenummer")
+        }
+
+        return try {
+            fiksClient.hentKommuneInfo(kommunenummer)
+        } catch (e: FiksException) {
+            null
+        }
+    }
+
+
+
     fun hentAlleKommunerMedStatusStatus(): List<KommuneStatusDetaljer> {
         val alleKommunerMedStatus = fiksClient.hentKommuneInfoForAlle()
-        return alleKommunerMedStatus.map {info -> KommuneStatusDetaljer(info)}
+        return alleKommunerMedStatus.map { info -> KommuneStatusDetaljer(info) }
     }
 }
 
-class KommuneStatusDetaljer(kommuneInfo: KommuneInfo) {
-    var kommunenummer: String
-    var kanMottaSoknader: Boolean
-    var kanOppdatereStatus: Boolean
-    var harMidlertidigDeaktivertMottak: Boolean
-    var harMidlertidigDeaktivertOppdateringer: Boolean
+    class KommuneStatusDetaljer(kommuneInfo: KommuneInfo) {
+        var kommunenummer: String
+        var kanMottaSoknader: Boolean
+        var kanOppdatereStatus: Boolean
+        var harMidlertidigDeaktivertMottak: Boolean
+        var harMidlertidigDeaktivertOppdateringer: Boolean
 
-    init {
-        this.kommunenummer = kommuneInfo.kommunenummer
-        this.kanMottaSoknader = kommuneInfo.kanMottaSoknader
-        this.kanOppdatereStatus = kommuneInfo.kanOppdatereStatus
-        this.harMidlertidigDeaktivertMottak = kommuneInfo.harMidlertidigDeaktivertMottak
-        this.harMidlertidigDeaktivertOppdateringer = kommuneInfo.harMidlertidigDeaktivertOppdateringer
+        init {
+            this.kommunenummer = kommuneInfo.kommunenummer
+            this.kanMottaSoknader = kommuneInfo.kanMottaSoknader
+            this.kanOppdatereStatus = kommuneInfo.kanOppdatereStatus
+            this.harMidlertidigDeaktivertMottak = kommuneInfo.harMidlertidigDeaktivertMottak
+            this.harMidlertidigDeaktivertOppdateringer = kommuneInfo.harMidlertidigDeaktivertOppdateringer
+        }
     }
-}
 
-enum class KommuneStatus {
-    HAR_KONFIGURASJON_MEN_SKAL_SENDE_VIA_SVARUT,
-    MANGLER_KONFIGURASJON,
-    SKAL_SENDE_SOKNADER_OG_ETTERSENDELSER_VIA_FDA,
-    SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER_INNSYN_SOM_VANLIG,
-    SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER_INNSYN_IKKE_MULIG,
-    SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER_INNSYN_SKAL_VISE_FEILSIDE,
-    IKKE_STOTTET_CASE
-}
+    enum class KommuneStatus {
+        HAR_KONFIGURASJON_MEN_SKAL_SENDE_VIA_SVARUT,
+        MANGLER_KONFIGURASJON,
+        SKAL_SENDE_SOKNADER_OG_ETTERSENDELSER_VIA_FDA,
+        SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER_INNSYN_SOM_VANLIG,
+        SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER_INNSYN_IKKE_MULIG,
+        SKAL_VISE_MIDLERTIDIG_FEILSIDE_FOR_SOKNAD_OG_ETTERSENDELSER_INNSYN_SKAL_VISE_FEILSIDE,
+        IKKE_STOTTET_CASE
+    }

0dea7884b09cad16a516680a3f5020fca7458297
siljee <silje.hansen3@nav.no>
2019-11-27 13:01:24 +0100

    Hente kommunenummer fra digisos-saken og ikke fra originalsøknad. Fordi papirsøknader ikke har originalsøknader.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
@@ -37,10 +37,8 @@ class KommuneService(private val fiksClient: FiksClient,
 
     fun hentKommuneInfo(fiksDigisosId: String, token: String): KommuneInfo? {
         val digisosSak = fiksClient.hentDigisosSak(fiksDigisosId, token, true)
+        val kommunenummer: String? = digisosSak.kommunenummer
 
-        val originalSoknad: JsonSoknad? = innsynService.hentOriginalSoknad(fiksDigisosId, digisosSak.originalSoknadNAV?.metadata, token)
-
-        val kommunenummer: String? = originalSoknad?.mottaker?.kommunenummer
         if (kommunenummer == null) {
             log.warn("Forsøkte å hente kommuneStatus, men JsonSoknad.mottaker.kommunenummer finnes ikke")
             throw RuntimeException("KommuneStatus kan ikke hentes uten kommunenummer")

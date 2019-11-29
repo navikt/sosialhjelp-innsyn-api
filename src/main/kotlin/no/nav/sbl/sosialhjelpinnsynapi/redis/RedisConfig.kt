db9ca23a79f4c139a508720424ac01a47de25d04
martintveter <41987225+martintveter@users.noreply.github.com>
2019-11-29 14:30:11 +0100

    Feture/redis styres av properties (#99)
    * Flytter config av in-memory-redis til properties -> profile = mock | local | test spinner opp en in-memory redis-instans. property name = `redis_mocked`
    
    * oppdaterer readme

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/redis/RedisConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.sbl.sosialhjelpinnsynapi.redis
 
 import io.lettuce.core.RedisClient
+import no.nav.sbl.sosialhjelpinnsynapi.redis.RedisMockUtil.startRedisMocked
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -8,10 +9,19 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties(RedisProperties::class)
-class RedisConfig {
+class RedisConfig(private val cacheProperties: CacheProperties) {
 
     @Bean
     fun redisClient(properties: RedisProperties): RedisClient {
+        startInMemoryRedisIfMocked()
+
         return RedisClient.create("redis://${properties.host}:${properties.port}")
     }
+
+    private fun startInMemoryRedisIfMocked() {
+        if (cacheProperties.redisMocked) {
+            startRedisMocked()
+        }
+    }
+
 }

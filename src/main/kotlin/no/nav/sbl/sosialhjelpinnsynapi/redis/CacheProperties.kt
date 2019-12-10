511413e4f8ec5df8022f97b1a0c2caba4fcc3b14
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-12-10 09:14:56 +0100

    Setter time-to-live for cachen til 180s (3 min)

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/redis/CacheProperties.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/redis/CacheProperties.kt
@@ -1,10 +1,11 @@
 package no.nav.sbl.sosialhjelpinnsynapi.redis
 
+import no.nav.sbl.sosialhjelpinnsynapi.redis.RedisMockUtil.startRedisMocked
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 import kotlin.properties.Delegates
 
-const val CACHE_TIME_TO_LIVE_SECONDS: Long = 2
+const val CACHE_TIME_TO_LIVE_SECONDS: Long = 180
 
 @Component
 @ConfigurationProperties(prefix = "innsyn.cache")
@@ -12,4 +13,9 @@ class CacheProperties {
 
     var redisMocked by Delegates.notNull<Boolean>()
 
+    fun startInMemoryRedisIfMocked() {
+        if (redisMocked) {
+            startRedisMocked()
+        }
+    }
 }

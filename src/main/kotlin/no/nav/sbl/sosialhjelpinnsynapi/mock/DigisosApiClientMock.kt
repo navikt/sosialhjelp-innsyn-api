2ae536fdf2de3e4dffcd134d9230d7afeac266bb
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-12-16 16:37:04 +0100

    refaktorering digisosapiclientmock

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/DigisosApiClientMock.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/DigisosApiClientMock.kt
@@ -15,6 +15,7 @@ import java.util.*
 @Profile("mock")
 @Component
 class DigisosApiClientMock(private val fiksClientMock: FiksClientMock) : DigisosApiClient {
+
     override fun oppdaterDigisosSak(fiksDigisosId: String?, digisosApiWrapper: DigisosApiWrapper): String? {
         val dokumentlagerId = UUID.randomUUID().toString()
         fiksClientMock.postDokument(dokumentlagerId, digisosApiWrapper.sak.soker)
@@ -24,9 +25,21 @@ class DigisosApiClientMock(private val fiksClientMock: FiksClientMock) : Digisos
         }
 
         if (!fiksClientMock.digisosSakFinnes(id)) {
-            fiksClientMock.postDigisosSak(DigisosSak(id, "01234567890", "11415cd1-e26d-499a-8421-751457dfcbd5", "1", System.currentTimeMillis(),
-                    OriginalSoknadNAV("110000000", "", "mock-soknad-vedlegg-metadata", DokumentInfo("", "", 0L), Collections.emptyList(),
-                            femMinutterForMottattSoknad(digisosApiWrapper)), EttersendtInfoNAV(Collections.emptyList()), null))
+            fiksClientMock.postDigisosSak(DigisosSak(
+                    fiksDigisosId = id,
+                    sokerFnr = "01234567890",
+                    fiksOrgId = "11415cd1-e26d-499a-8421-751457dfcbd5",
+                    kommunenummer = "1",
+                    sistEndret = System.currentTimeMillis(),
+                    originalSoknadNAV = OriginalSoknadNAV(
+                            navEksternRefId = "110000000",
+                            metadata = "",
+                            vedleggMetadata = "mock-soknad-vedlegg-metadata",
+                            soknadDokument = DokumentInfo("", "", 0L),
+                            vedlegg = Collections.emptyList(),
+                            timestampSendt = femMinutterForMottattSoknad(digisosApiWrapper)),
+                    ettersendtInfoNAV = EttersendtInfoNAV(Collections.emptyList()),
+                    digisosSoker = null))
         }
 
         val digisosSak = fiksClientMock.hentDigisosSak(id, "", true)
@@ -35,15 +48,17 @@ class DigisosApiClientMock(private val fiksClientMock: FiksClientMock) : Digisos
         return id
     }
 
-    override fun lastOppNyeFilerTilFiks(files: List<FilForOpplasting>, soknadId: String): List<String> {return emptyList()}
+    override fun lastOppNyeFilerTilFiks(files: List<FilForOpplasting>, soknadId: String): List<String> {
+        return emptyList()
+    }
 
     private fun femMinutterForMottattSoknad(digisosApiWrapper: DigisosApiWrapper): Long {
         val mottattTidspunkt = digisosApiWrapper.sak.soker.hendelser.minBy { it.hendelsestidspunkt }!!.hendelsestidspunkt
-        try {
+        return try {
             val toLocalDateTime = toLocalDateTime(mottattTidspunkt)
-            return toLocalDateTime.minusMinutes(5).atZone(ZoneId.of("Europe/Oslo")).toInstant().toEpochMilli()
+            toLocalDateTime.minusMinutes(5).atZone(ZoneId.of("Europe/Oslo")).toInstant().toEpochMilli()
         } catch (e: DateTimeParseException) {
-            return LocalDateTime.now().minusMinutes(5).atZone(ZoneId.of("Europe/Oslo")).toInstant().toEpochMilli()
+            LocalDateTime.now().minusMinutes(5).atZone(ZoneId.of("Europe/Oslo")).toInstant().toEpochMilli()
         }
     }
 

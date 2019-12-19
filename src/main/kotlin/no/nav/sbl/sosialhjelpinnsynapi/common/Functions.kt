324fe9143f79eb744cfa617a42db3c17f132da49
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-12-19 18:10:25 +0100

    gjør det mulig å sette factor for retry-coroutine. defaulter til 2.0 som før

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/common/Functions.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/common/Functions.kt
@@ -8,6 +8,7 @@ internal suspend fun <T> retry(
         attempts: Int = 10,
         initialDelay: Long = 100L,
         maxDelay: Long = 1000L,
+        factor: Double = 2.0,
         vararg illegalExceptions: KClass<out Throwable> = arrayOf(),
         block: suspend () -> T
 ): T {
@@ -22,7 +23,7 @@ internal suspend fun <T> retry(
             }
         }
         delay(currentDelay)
-        currentDelay = (currentDelay * 2.0).toLong().coerceAtMost(maxDelay)
+        currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelay)
     }
     return try {
         timed { block() }
@@ -39,4 +40,3 @@ private fun countAndRethrowError(e: Throwable, block: () -> Any?): Nothing {
 
 internal suspend inline fun <T> timed(crossinline block: suspend () -> T) =
         block()
-

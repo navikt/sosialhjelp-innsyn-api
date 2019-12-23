78cde62437de161773e6f37b975ce5c7b71c395d
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2019-12-23 12:25:21 +0100

    SaksStatus-hendelser: * Ny hendelse i historikk ved opprettelse av nye saker. * Ny hendelse i historikk dersom status settes til IKKE_INNSYN eller BEHANDLES_IKKE

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/event/SaksStatus.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/event/SaksStatus.kt
@@ -1,9 +1,11 @@
 package no.nav.sbl.sosialhjelpinnsynapi.event
 
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonSaksStatus
+import no.nav.sbl.sosialhjelpinnsynapi.domain.Hendelse
 import no.nav.sbl.sosialhjelpinnsynapi.domain.InternalDigisosSoker
 import no.nav.sbl.sosialhjelpinnsynapi.domain.Sak
 import no.nav.sbl.sosialhjelpinnsynapi.domain.SaksStatus
+import no.nav.sbl.sosialhjelpinnsynapi.toLocalDateTime
 
 fun InternalDigisosSoker.apply(hendelse: JsonSaksStatus) {
 
@@ -13,21 +15,39 @@ fun InternalDigisosSoker.apply(hendelse: JsonSaksStatus) {
         // Oppdater felter
 
         if (hendelse.status != null) {
+            val prevStatus = sakForReferanse.saksStatus
+
             sakForReferanse.saksStatus = SaksStatus.valueOf(hendelse.status?.name
                     ?: JsonSaksStatus.Status.UNDER_BEHANDLING.name)
+
+            if (prevStatus != sakForReferanse.saksStatus
+                    && (sakForReferanse.saksStatus == SaksStatus.IKKE_INNSYN || sakForReferanse.saksStatus == SaksStatus.BEHANDLES_IKKE)) {
+                historikk.add(Hendelse("Vi kan ikke vise behandlingsstatus for ${hendelse.tittel} digitalt.", toLocalDateTime(hendelse.hendelsestidspunkt)))
+            }
         }
 
         sakForReferanse.tittel = hendelse.tittel
+
     } else {
         // Opprett ny Sak
+        val status = SaksStatus.valueOf(hendelse.status?.name ?: JsonSaksStatus.Status.UNDER_BEHANDLING.name)
         saker.add(Sak(
                 hendelse.referanse,
-                SaksStatus.valueOf(hendelse.status?.name ?: JsonSaksStatus.Status.UNDER_BEHANDLING.name),
+                status,
                 hendelse.tittel,
                 mutableListOf(),
                 mutableListOf(),
                 mutableListOf(),
                 mutableListOf()
         ))
+        val tittel = hendelse.tittel ?: "default tittel"
+        val beskrivelse: String? = when (status) {
+            SaksStatus.UNDER_BEHANDLING -> "$tittel er under behandling"
+            SaksStatus.BEHANDLES_IKKE, SaksStatus.IKKE_INNSYN -> "Vi kan ikke vise behandlingsstatus for $tittel digitalt."
+            else -> null
+        }
+        if (beskrivelse != null) {
+            historikk.add(Hendelse(beskrivelse, toLocalDateTime(hendelse.hendelsestidspunkt)))
+        }
     }
 }
--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/event/EventServiceTest.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/event/EventServiceTest.kt
@@ -125,7 +125,7 @@ internal class EventServiceTest {
             assertThat(model).isNotNull
             assertThat(model.status).isEqualTo(SoknadsStatus.UNDER_BEHANDLING)
             assertThat(model.saker).hasSize(1)
-            assertThat(model.historikk).hasSize(3)
+            assertThat(model.historikk).hasSize(4)
 
             val sak = model.saker.last()
             assertThat(sak.saksStatus).isEqualTo(SaksStatus.UNDER_BEHANDLING)
@@ -135,8 +135,8 @@ internal class EventServiceTest {
             assertThat(sak.utbetalinger).isEmpty()
 
             val hendelse = model.historikk.last()
-            assertThat(hendelse.tidspunkt).isEqualTo(toLocalDateTime(tidspunkt_2))
-            assertThat(hendelse.tittel).contains("Søknaden er under behandling")
+            assertThat(hendelse.tidspunkt).isEqualTo(toLocalDateTime(tidspunkt_3))
+            assertThat(hendelse.tittel).contains("$tittel_1 er under behandling")
         }
 
         @Test
@@ -157,7 +157,7 @@ internal class EventServiceTest {
             assertThat(model).isNotNull
             assertThat(model.status).isEqualTo(SoknadsStatus.UNDER_BEHANDLING)
             assertThat(model.saker).hasSize(1)
-            assertThat(model.historikk).hasSize(3)
+            assertThat(model.historikk).hasSize(4)
 
             val sak = model.saker.last()
             assertThat(sak.saksStatus).isEqualTo(SaksStatus.UNDER_BEHANDLING)
@@ -167,8 +167,9 @@ internal class EventServiceTest {
             assertThat(sak.utbetalinger).isEmpty()
 
             val hendelse = model.historikk.last()
-            assertThat(hendelse.tidspunkt).isEqualTo(toLocalDateTime(tidspunkt_2))
-            assertThat(hendelse.tittel).contains("Søknaden er under behandling")
+            assertThat(hendelse.tidspunkt).isEqualTo(toLocalDateTime(tidspunkt_3))
+            assertThat(hendelse.tittel).contains("er under behandling")
+            assertThat(hendelse.tittel).contains("default tittel")
         }
 
         @Test
@@ -190,7 +191,7 @@ internal class EventServiceTest {
             assertThat(model).isNotNull
             assertThat(model.status).isEqualTo(SoknadsStatus.UNDER_BEHANDLING)
             assertThat(model.saker).hasSize(1)
-            assertThat(model.historikk).hasSize(4)
+            assertThat(model.historikk).hasSize(5)
 
             val sak = model.saker.last()
             assertThat(sak.saksStatus).isEqualTo(SaksStatus.UNDER_BEHANDLING)
@@ -305,7 +306,7 @@ internal class EventServiceTest {
             assertThat(model).isNotNull
             assertThat(model.status).isEqualTo(SoknadsStatus.UNDER_BEHANDLING)
             assertThat(model.saker).hasSize(1)
-            assertThat(model.historikk).hasSize(5)
+            assertThat(model.historikk).hasSize(6)
 
             val sak = model.saker.last()
             assertThat(sak.saksStatus).isEqualTo(SaksStatus.UNDER_BEHANDLING)
@@ -341,7 +342,7 @@ internal class EventServiceTest {
             assertThat(model).isNotNull
             assertThat(model.status).isEqualTo(SoknadsStatus.UNDER_BEHANDLING)
             assertThat(model.saker).hasSize(1)
-            assertThat(model.historikk).hasSize(4)
+            assertThat(model.historikk).hasSize(5)
 
             val sak = model.saker.last()
             assertThat(sak.saksStatus).isEqualTo(SaksStatus.UNDER_BEHANDLING)
@@ -357,6 +358,67 @@ internal class EventServiceTest {
             assertThat(hendelse.tidspunkt).isEqualTo(toLocalDateTime(tidspunkt_4))
             assertThat(hendelse.tittel).contains("$DEFAULT_TITTEL er ferdig behandlet")
             assertThat(hendelse.url?.link).contains("/dokumentlager/nedlasting/$dokumentlagerId_1")
+        }
+
+        @Test
+        internal fun `saksStatus ikke_innsyn`() {
+            every { innsynService.hentJsonDigisosSoker(any(), any(), any()) } returns
+                    JsonDigisosSoker()
+                            .withAvsender(avsender)
+                            .withVersion("123")
+                            .withHendelser(listOf(
+                                    SOKNADS_STATUS_MOTTATT.withHendelsestidspunkt(tidspunkt_1),
+                                    SOKNADS_STATUS_UNDERBEHANDLING.withHendelsestidspunkt(tidspunkt_2),
+                                    SAK1_SAKS_STATUS_IKKEINNSYN.withHendelsestidspunkt(tidspunkt_3)
+                            ))
+            every { vedleggService.hentSoknadVedleggMedStatus(VEDLEGG_KREVES_STATUS, any(), any(), any()) } returns emptyList()
+
+            val model = service.createModel(mockDigisosSak, "token")
+
+            assertThat(model).isNotNull
+            assertThat(model.status).isEqualTo(SoknadsStatus.UNDER_BEHANDLING)
+            assertThat(model.saker).hasSize(1)
+            assertThat(model.historikk).hasSize(4)
+
+            val sak = model.saker.last()
+            assertThat(sak.saksStatus).isEqualTo(SaksStatus.IKKE_INNSYN)
+            assertThat(sak.referanse).isEqualTo(referanse_1)
+            assertThat(sak.tittel).isEqualTo(tittel_1)
+
+            val hendelse = model.historikk.last()
+            assertThat(hendelse.tidspunkt).isEqualTo(toLocalDateTime(tidspunkt_3))
+            assertThat(hendelse.tittel).contains("Vi kan ikke vise behandlingsstatus for $tittel_1 digitalt.")
+        }
+
+        @Test
+        internal fun `saksStatus endres fra under_behandling til ikke_innsyn`() {
+            every { innsynService.hentJsonDigisosSoker(any(), any(), any()) } returns
+                    JsonDigisosSoker()
+                            .withAvsender(avsender)
+                            .withVersion("123")
+                            .withHendelser(listOf(
+                                    SOKNADS_STATUS_MOTTATT.withHendelsestidspunkt(tidspunkt_1),
+                                    SOKNADS_STATUS_UNDERBEHANDLING.withHendelsestidspunkt(tidspunkt_2),
+                                    SAK1_SAKS_STATUS_UNDERBEHANDLING.withHendelsestidspunkt(tidspunkt_3),
+                                    SAK1_SAKS_STATUS_IKKEINNSYN.withHendelsestidspunkt(tidspunkt_4)
+                            ))
+            every { vedleggService.hentSoknadVedleggMedStatus(VEDLEGG_KREVES_STATUS, any(), any(), any()) } returns emptyList()
+
+            val model = service.createModel(mockDigisosSak, "token")
+
+            assertThat(model).isNotNull
+            assertThat(model.status).isEqualTo(SoknadsStatus.UNDER_BEHANDLING)
+            assertThat(model.saker).hasSize(1)
+            assertThat(model.historikk).hasSize(5)
+
+            val sak = model.saker.last()
+            assertThat(sak.saksStatus).isEqualTo(SaksStatus.IKKE_INNSYN)
+            assertThat(sak.referanse).isEqualTo(referanse_1)
+            assertThat(sak.tittel).isEqualTo(tittel_1)
+
+            val hendelse = model.historikk.last()
+            assertThat(hendelse.tidspunkt).isEqualTo(toLocalDateTime(tidspunkt_4))
+            assertThat(hendelse.tittel).contains("Vi kan ikke vise behandlingsstatus for $tittel_1 digitalt.")
         }
     }
 

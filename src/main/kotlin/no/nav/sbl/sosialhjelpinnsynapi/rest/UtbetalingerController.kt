55481f9780983b3e0a73e193f6835542827aa26f
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2020-01-30 11:30:34 +0100

    DIGISOS-1626 Optimalisere utbetalinger Returnerer 401 når Fiks returnerer 403.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/UtbetalingerController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/UtbetalingerController.kt
@@ -1,10 +1,12 @@
 package no.nav.sbl.sosialhjelpinnsynapi.rest
 
 
+import no.nav.sbl.sosialhjelpinnsynapi.common.FiksClientException
 import no.nav.sbl.sosialhjelpinnsynapi.domain.UtbetalingerResponse
 import no.nav.sbl.sosialhjelpinnsynapi.utbetalinger.UtbetalingerService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -16,7 +18,14 @@ class UtbetalingerController(private val utbetalingerService: UtbetalingerServic
     @GetMapping("utbetalinger")
     fun hentUtbetalinger(@RequestHeader(value = AUTHORIZATION) token: String, @RequestParam(defaultValue = "3") month: Int): ResponseEntity<List<UtbetalingerResponse>> {
         // Gitt innlogget bruker
-        return ResponseEntity.ok().body(utbetalingerService.hentUtbetalinger(token, month))
+        try {
+            return ResponseEntity.ok().body(utbetalingerService.hentUtbetalinger(token, month))
+        } catch (e: FiksClientException) {
+            if(e.status == HttpStatus.FORBIDDEN) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+            }
+            throw e
+        }
     }
 
 }

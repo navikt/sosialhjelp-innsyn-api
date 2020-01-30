fc99f01be0ed95b036a5607834ad77599ea3f962
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2020-01-30 12:48:45 +0100

    DIGISOS-1626 Optimalisere utbetalinger La til log.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/UtbetalingerController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/UtbetalingerController.kt
@@ -3,6 +3,7 @@ package no.nav.sbl.sosialhjelpinnsynapi.rest
 
 import no.nav.sbl.sosialhjelpinnsynapi.common.FiksClientException
 import no.nav.sbl.sosialhjelpinnsynapi.domain.UtbetalingerResponse
+import no.nav.sbl.sosialhjelpinnsynapi.logger
 import no.nav.sbl.sosialhjelpinnsynapi.utbetalinger.UtbetalingerService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpHeaders.AUTHORIZATION
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/v1/innsyn/")
 class UtbetalingerController(private val utbetalingerService: UtbetalingerService) {
 
+    companion object {
+        val log by logger()
+    }
+
     @GetMapping("utbetalinger")
     fun hentUtbetalinger(@RequestHeader(value = AUTHORIZATION) token: String, @RequestParam(defaultValue = "3") month: Int): ResponseEntity<List<UtbetalingerResponse>> {
         // Gitt innlogget bruker
@@ -22,6 +27,7 @@ class UtbetalingerController(private val utbetalingerService: UtbetalingerServic
             return ResponseEntity.ok().body(utbetalingerService.hentUtbetalinger(token, month))
         } catch (e: FiksClientException) {
             if(e.status == HttpStatus.FORBIDDEN) {
+                log.error("FiksClientException i UtbetalingerController status: ${e.status.value()} message: ${e.message}", e)
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
             }
             throw e

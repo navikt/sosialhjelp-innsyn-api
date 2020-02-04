aeaad1a191e987479308a9af22e7e8f3766a3e9a
martintveter <41987225+martintveter@users.noreply.github.com>
2020-02-04 15:20:21 +0100

    Fix/lukking av pdf 2 (#133)
    bruker kotlins `use`-funksjon istedet for try-catch-finally

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/vedlegg/VedleggOpplastingService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/vedlegg/VedleggOpplastingService.kt
@@ -17,6 +17,7 @@ import no.nav.sbl.sosialhjelpinnsynapi.utils.isPdf
 import no.nav.sbl.sosialhjelpinnsynapi.utils.objectMapper
 import no.nav.sbl.sosialhjelpinnsynapi.virusscan.VirusScanner
 import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.io.IOException
@@ -128,22 +129,24 @@ class VedleggOpplastingService(private val fiksClient: FiksClient,
     }
 
     private fun checkIfPdfIsValid(data: InputStream): String {
-        var document = PDDocument()
         try {
-            document = PDDocument.load(data)
-            if (document.signatureDictionaries.isNotEmpty()) {
-                log.warn(MESSAGE_PDF_IS_SIGNED)
-                return MESSAGE_PDF_IS_SIGNED
-            } else if (document.isEncrypted) {
-                log.warn(MESSAGE_PDF_IS_ENCRYPTED)
-                return MESSAGE_PDF_IS_ENCRYPTED
-            }
-            return "OK"
+            PDDocument.load(data)
+                    .use { document ->
+                        if (document.signatureDictionaries.isNotEmpty()) {
+                            log.warn(MESSAGE_PDF_IS_SIGNED)
+                            return MESSAGE_PDF_IS_SIGNED
+                        } else if (document.isEncrypted) {
+                            log.warn(MESSAGE_PDF_IS_ENCRYPTED)
+                            return MESSAGE_PDF_IS_ENCRYPTED
+                        }
+                        return "OK"
+                    }
+        } catch (e: InvalidPasswordException) {
+            log.warn(MESSAGE_PDF_IS_ENCRYPTED, e)
+            return MESSAGE_PDF_IS_ENCRYPTED
         } catch (e: IOException) {
-            log.warn(MESSAGE_COULD_NOT_LOAD_DOCUMENT + e.stackTrace)
+            log.warn(MESSAGE_COULD_NOT_LOAD_DOCUMENT, e)
             return MESSAGE_COULD_NOT_LOAD_DOCUMENT
-        } finally {
-            document.close()
         }
     }
 

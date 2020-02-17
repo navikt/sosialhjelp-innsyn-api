64d6f54f04127994b4f8d1767223c8f90d764e0e
siljee <silje.hansen3@nav.no>
2020-02-17 13:17:09 +0100

    Fjerner logging av forsendelseId til den opprettede ettersendelsen, fordi vi ikke mottar den av fiks

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/kommune/KommuneService.kt
@@ -39,8 +39,8 @@ class KommuneService(private val fiksClient: FiksClient) {
         val kommunenummer: String? = digisosSak.kommunenummer
 
         if (kommunenummer == null) {
-            log.warn("Forsøkte å hente kommuneStatus, men JsonSoknad.mottaker.kommunenummer finnes ikke for $fiksDigisosId")
-            throw RuntimeException("KommuneStatus kan ikke hentes, da kommunenummer mangler i $fiksDigisosId")
+            log.warn("Forsøkte å hente kommuneStatus, men JsonSoknad.mottaker.kommunenummer finnes ikke i soknadJson for $fiksDigisosId")
+            throw RuntimeException("KommuneStatus kan ikke hentes, da kommunenummer mangler for $fiksDigisosId")
         }
 
         return try {

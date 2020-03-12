24d4a957d320ba1163afbc9decd080b556521333
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-03-12 13:22:47 +0100

    legger på logging for debug

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/event/Vilkar.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/event/Vilkar.kt
@@ -5,6 +5,7 @@ import no.nav.sbl.sosialhjelpinnsynapi.domain.InternalDigisosSoker
 import no.nav.sbl.sosialhjelpinnsynapi.domain.Sak
 import no.nav.sbl.sosialhjelpinnsynapi.domain.Utbetaling
 import no.nav.sbl.sosialhjelpinnsynapi.domain.Vilkar
+import no.nav.sbl.sosialhjelpinnsynapi.hendelse.HendelseService.Companion.log
 import no.nav.sbl.sosialhjelpinnsynapi.toLocalDateTime
 
 fun InternalDigisosSoker.apply(hendelse: JsonVilkar) {
@@ -37,9 +38,13 @@ fun InternalDigisosSoker.apply(hendelse: JsonVilkar) {
 private fun MutableList<Vilkar>.oppdaterEllerLeggTilVilkar(hendelse: JsonVilkar, vilkar: Vilkar) {
     if (any { it.referanse == hendelse.vilkarreferanse }) {
         filter { it.referanse == hendelse.vilkarreferanse }
-                .forEach { it.oppdaterFelter(hendelse) }
+                .forEach {
+                    it.oppdaterFelter(hendelse)
+                    log.warn("Vilkar endret - vilkarreferanse:${hendelse.vilkarreferanse}")
+                }
     } else {
-        add(vilkar)
+        this.add(vilkar)
+        log.warn("Nytt Vilkar lagt til - vilkarreferanse:${hendelse.vilkarreferanse}")
     }
 }
 

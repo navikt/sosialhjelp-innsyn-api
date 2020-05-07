01be758a1c6004f0d5b0540b7dab727503f4d6e7
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-07 14:07:05 +0200

    Revert "debug --> info for å sjekke at det funker"
    This reverts commit 714d26af

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/vedlegg/KrypteringServiceImpl.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/vedlegg/KrypteringServiceImpl.kt
@@ -54,7 +54,7 @@ class KrypteringServiceImpl(
                 try {
                     log.debug("Starter kryptering, digisosId=$digisosId")
                     kryptering.krypterData(pipedOutputStream, fileInputStream, certificate, Security.getProvider("BC"))
-                    log.info("Ferdig med kryptering, digisosId=$digisosId")
+                    log.debug("Ferdig med kryptering, digisosId=$digisosId")
                 } catch (e: Exception) {
                     log.error("Det skjedde en feil ved kryptering, exception blir lagt til kryptert InputStream, digisosId=$digisosId", e)
                     throw IllegalStateException("An error occurred during encryption", e)
@@ -62,7 +62,7 @@ class KrypteringServiceImpl(
                     try {
                         log.debug("Lukker kryptering OutputStream, digisosId=$digisosId")
                         pipedOutputStream.close()
-                        log.info("OutputStream for kryptering er lukket, digisosId=$digisosId")
+                        log.debug("OutputStream for kryptering er lukket, digisosId=$digisosId")
                     } catch (e: IOException) {
                         log.error("Lukking av Outputstream for kryptering feilet, digisosId=$digisosId", e)
                     }

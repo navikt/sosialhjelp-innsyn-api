2140f6cb2c20abcee7e2691097637ecc517975ac
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-13 11:38:51 +0200

    apikey for pdl

--- nais/scripts/90-set-secrets.sh
+++ nais/scripts/90-set-secrets.sh
@@ -3,3 +3,4 @@ export SRVSOSIALHJELP_INNSYN_API_USERNAME=$(cat /serviceuser/srvsosial-inn-fss/u
 export SRVSOSIALHJELP_INNSYN_API_PASSWORD=$(cat /serviceuser/srvsosial-inn-fss/password)
 
 export SOSIALHJELP_INNSYN_API_STSTOKEN_APIKEY_PASSWORD=$(cat /apigw/securitytokenservicetoken/x-nav-apiKey)
+export SOSIALHJELP_INNSYN_API_PDL_APIKEY_PASSWORD=$(cat /apigw/pdl-api/x-nav-apiKey)

e9dc7c9932892bc7b9cd6f57174fe92a3a452ede
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-13 12:57:08 +0200

    Service for adressebeskyttelse (sjekke om bruker er kode6 eller kode7)

---
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/service/adressebeskyttelse/AdressebeskyttelseService.kt
@@ -0,0 +1,19 @@
+package no.nav.sbl.sosialhjelpinnsynapi.service.adressebeskyttelse
+
+import no.nav.sbl.sosialhjelpinnsynapi.common.PdlException
+import no.nav.sbl.sosialhjelpinnsynapi.consumer.pdl.PdlClient
+import no.nav.sbl.sosialhjelpinnsynapi.consumer.pdl.isKode6Or7
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+
+@Component
+class AdressebeskyttelseService(
+        private val pdlClient: PdlClient
+) {
+
+    fun isKode6Or7(ident: String): Boolean {
+        return pdlClient.hentPerson(ident)?.isKode6Or7()
+                ?: throw PdlException(HttpStatus.INTERNAL_SERVER_ERROR, "Noe uventet feilet ved uthenting ")
+    }
+
+}
---
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/service/adressebeskyttelse/AdressebeskyttelseServiceTest.kt
@@ -0,0 +1,70 @@
+package no.nav.sbl.sosialhjelpinnsynapi.service.adressebeskyttelse
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.sbl.sosialhjelpinnsynapi.common.PdlException
+import no.nav.sbl.sosialhjelpinnsynapi.consumer.pdl.Adressebeskyttelse
+import no.nav.sbl.sosialhjelpinnsynapi.consumer.pdl.Gradering
+import no.nav.sbl.sosialhjelpinnsynapi.consumer.pdl.PdlClient
+import no.nav.sbl.sosialhjelpinnsynapi.consumer.pdl.PdlHentPerson
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Test
+
+internal class AdressebeskyttelseServiceTest {
+
+    private val pdlClientMock: PdlClient = mockk()
+    private val service = AdressebeskyttelseService(pdlClientMock)
+
+    private val ident = "123"
+
+    val clientResponse: PdlHentPerson = mockk()
+
+    @Test
+    internal fun `skal kaste feil hvis client returnerer null`() {
+        every { pdlClientMock.hentPerson(any()) } returns null
+
+        assertThatExceptionOfType(PdlException::class.java).isThrownBy { service.isKode6Or7(ident) }
+    }
+
+    @Test
+    internal fun `skal returnere false hvis person ikke har adressebeskyttelse`() {
+        every { clientResponse.hentPerson?.adressebeskyttelse } returns null
+        every { pdlClientMock.hentPerson(any()) } returns clientResponse
+
+        val result = service.isKode6Or7(ident)
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    internal fun `skal returnere false hvis adressebeskyttelse-liste er tom`() {
+        every { clientResponse.hentPerson?.adressebeskyttelse } returns emptyList()
+        every { pdlClientMock.hentPerson(any()) } returns clientResponse
+
+        val result = service.isKode6Or7(ident)
+
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    internal fun `skal returnere true hvis person er kode6`() {
+        every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.STRENGT_FORTROLIG))
+        every { pdlClientMock.hentPerson(any()) } returns clientResponse
+
+        val result = service.isKode6Or7(ident)
+
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    internal fun `skal returnere true hvis person er kode7`() {
+        every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.FORTROLIG))
+        every { pdlClientMock.hentPerson(any()) } returns clientResponse
+
+        val result = service.isKode6Or7(ident)
+
+        assertThat(result).isTrue()
+    }
+
+}

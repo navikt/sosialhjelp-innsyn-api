4e91216e75a81ab32d74fc3244100bcb8f15a102
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-19 12:39:35 +0200

    debug

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/mdc/MDCFilter.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/mdc/MDCFilter.kt
@@ -16,8 +16,10 @@ import javax.servlet.http.HttpServletResponse
 class MDCFilter : OncePerRequestFilter() {
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-        addCallId(request)
-        addDigisosId(request)
+        if (request.requestURI.startsWith(INNSYN_BASE_URL) || request.requestURI.startsWith(KLIENTLOGGER_BASE_URL) || request.requestURI.startsWith(VEIVISER_BASE_URL)) {
+            addCallId(request)
+            addDigisosId(request)
+        }
 
         try {
             filterChain.doFilter(request, response)
@@ -30,13 +32,13 @@ class MDCFilter : OncePerRequestFilter() {
         Optional.ofNullable(request.getHeader(HEADER_CALL_ID))
                 .ifPresentOrElse(
                         {
-                            log.info("Bruker call-id fra request: $it")
                             setCallId(it)
+                            log.info("Bruker call-id fra request: $it")
                         },
                         {
                             val callId = generateCallId()
-                            log.info("Genererer ny call-id: $callId")
                             setCallId(callId)
+                            log.info("Genererte ny call-id: $callId")
                         }
                 )
     }
@@ -44,17 +46,20 @@ class MDCFilter : OncePerRequestFilter() {
     private fun addDigisosId(request: HttpServletRequest) {
         if (request.requestURI.matches(Regex("^${INNSYN_BASE_URL}(.*)/(forelopigSvar|hendelser|kommune|oppgaver|oppgaver/(.*)|orginalJsonSoknad|orginalSoknadPdlLink|saksStatus|soknadsStatus|vedlegg)"))) {
             val digisosId = request.requestURI.substringAfter(INNSYN_BASE_URL).substringBefore("/")
-            log.info("Setter digisosId til mdc: $digisosId, requesturi: ${request.requestURI}")
             setDigisosId(digisosId)
+            log.info("Setter digisosId til mdc: $digisosId, requesturi: ${request.requestURI}")
         } else if (request.requestURI.matches(Regex("^${INNSYN_BASE_URL}saksDetaljer")) && request.parameterMap.containsKey("id")) {
             val digisosId = request.getParameter("id")
-            log.info("Setter digisosId til mdc (saksDetaljer): $digisosId, requesturi: ${request.requestURI}")
             setDigisosId(digisosId)
+            log.info("Setter digisosId til mdc (saksDetaljer): $digisosId, requesturi: ${request.requestURI}")
         }
     }
 
     companion object {
-        private const val INNSYN_BASE_URL = "/sosialhjelp/innsyn-api/api/v1/innsyn/"
+        private const val CONTEXT_PATH = "/sosialhjelp/innsyn-api"
+        private const val INNSYN_BASE_URL = "$CONTEXT_PATH/api/v1/innsyn/"
+        private const val KLIENTLOGGER_BASE_URL = "$CONTEXT_PATH/api/v1/info/logg"
+        private const val VEIVISER_BASE_URL = "$CONTEXT_PATH/api/veiviser/kommunenummer"
 
         private val log by logger()
     }

6d81216451dde0dda669ab0ceaca598a5050be76
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2020-05-20 09:41:18 +0200

    Mock alt støtt i labs (og lokalt.) Bedre debug logging!

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/fiks/FiksClientImpl.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/fiks/FiksClientImpl.kt
@@ -75,6 +75,8 @@ class FiksClientImpl(
                 log.debug("Hentet digisosSak fra cache, digisosId=$digisosId")
                 return obj
             } catch (e: IOException) {
+                log.debug("Error mapping cache to DigisosSak:", e)
+                log.debug("DEBUG innhold: $get") //TODO: fjern
                 log.warn("Fant key=$digisosId i cache, men value var ikke DigisosSak")
             }
         }

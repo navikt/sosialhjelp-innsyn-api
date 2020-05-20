d783f436aec10080ed65addbe24613232ef04edf
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-05-20 12:48:01 +0200

    fjerner testbruker fnr i test

--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/pdf/EttersendelsePdfGeneratorTest.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/pdf/EttersendelsePdfGeneratorTest.kt
@@ -9,6 +9,8 @@ import java.util.*
 
 class EttersendelsePdfGeneratorTest {
 
+    private val ident = "11111111111"
+
     private val ettersendelsePdfGenerator  = EttersendelsePdfGenerator()
 
     // TODO: skrive bedre test for generering av pdf, foreløpig kun brukt for å sjekke at pdf ser riktig ut.
@@ -17,7 +19,7 @@ class EttersendelsePdfGeneratorTest {
     fun `skal generere pdf`() {
         val metadata = Collections.emptyList<OpplastetVedleggMetadata>()
 
-        val bytes = ettersendelsePdfGenerator.generate(metadata, "26104500284")
+        val bytes = ettersendelsePdfGenerator.generate(metadata, ident)
 
         try {
             val out = FileOutputStream("./starcraft.pdf")

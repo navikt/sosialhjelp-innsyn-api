8ed24445b5e2dd5bc74434f9cd1fce8786edd7d9
siljee <silje.hansen3@nav.no>
2020-05-20 11:29:44 +0200

    Legger tilbake innsending med restTemplate slik at første prodsetting blir kun omstruktureringen.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/fiks/FiksEttersendelseClientImpl.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/fiks/FiksEttersendelseClientImpl.kt
@@ -6,6 +6,8 @@ import no.nav.sbl.sosialhjelpinnsynapi.common.*
 import no.nav.sbl.sosialhjelpinnsynapi.config.ClientProperties
 import no.nav.sbl.sosialhjelpinnsynapi.feilmeldingUtenFnr
 import no.nav.sbl.sosialhjelpinnsynapi.logger
+import no.nav.sbl.sosialhjelpinnsynapi.toFiksErrorResponse
+import no.nav.sbl.sosialhjelpinnsynapi.utils.IntegrationUtils
 import no.nav.sbl.sosialhjelpinnsynapi.utils.objectMapper
 import no.nav.sbl.sosialhjelpinnsynapi.vedlegg.FilForOpplasting
 import no.nav.sbl.sosialhjelpinnsynapi.vedlegg.KrypteringService
@@ -18,11 +20,14 @@ import org.apache.http.entity.mime.MultipartEntityBuilder
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.util.EntityUtils
 import org.springframework.context.annotation.Profile
+import org.springframework.core.io.InputStreamResource
 import org.springframework.http.*
 import org.springframework.lang.NonNull
 import org.springframework.stereotype.Component
-import java.io.IOException
-import java.net.URI
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.RestTemplate
 import java.nio.charset.StandardCharsets
 import java.util.*
 import java.util.concurrent.*
@@ -30,13 +35,11 @@ import java.util.concurrent.*
 
 @Profile("!mock")
 @Component
-class FiksEttersendelseClientImpl(clientProperties: ClientProperties,
-                     private val krypteringService: KrypteringService) : FiksEttersendelseClient {
-
-    companion object {
-        val log by logger()
-        private const val SENDING_TIL_FIKS_TIMEOUT = 5 * 60 * 1000 // 5 minutter
-    }
+class FiksEttersendelseClientImpl(
+        clientProperties: ClientProperties,
+        private val krypteringService: KrypteringService,
+        private val restTemplate: RestTemplate
+) : FiksEttersendelseClient {
 
     private val baseUrl = clientProperties.fiksDigisosEndpointUrl
     private val fiksIntegrasjonid = clientProperties.fiksIntegrasjonId
@@ -46,25 +49,74 @@ class FiksEttersendelseClientImpl(clientProperties: ClientProperties,
                                         digisosId: String, navEksternRefId: String, kommunenummer: String, token: String) {
         log.info("Starter sending av ettersendelse med ${files.size} filer til digisosId=$digisosId")
 
-        val entityBuilder = createMultipartEntityBuilder()
+
+        val uri = "$baseUrl/digisos/api/v1/soknader/$kommunenummer/$digisosId/$navEksternRefId"
+        val headers = setRestTemplateHeaders(token)
+
+        val body = LinkedMultiValueMap<String, Any>()
+        body.add("vedlegg.json", createHttpEntityOfString(serialiser(vedleggJson), "vedlegg.json"))
+
+        val requestEntity = HttpEntity(body, headers)
+
+        val krypteringFutureList =
+                Collections.synchronizedList(ArrayList<CompletableFuture<Void>>(files.size))
+
+
+            try {
+                appendAndEncryptFilesForRestTemplate(body, files, krypteringFutureList, token, digisosId)
+
+                val startTime = System.currentTimeMillis()
+                val responseEntity = restTemplate.exchange(
+                        uri,
+                        HttpMethod.POST,
+                        requestEntity,
+                        String::class.java)
+                val endTime = System.currentTimeMillis()
+
+                log.info("Sendte ettersendelse til kommune $kommunenummer i Fiks. Tok ${endTime - startTime} ms. Fikk navEksternRefId $navEksternRefId (statusCode: ${responseEntity.statusCodeValue}) digisosId=$digisosId")
+
+            } catch (e: HttpClientErrorException) {
+                val fiksErrorResponse = e.toFiksErrorResponse()?.feilmeldingUtenFnr
+                val errorMessage = e.message?.feilmeldingUtenFnr
+                log.warn("Opplasting av ettersendelse på $digisosId feilet - $errorMessage - $fiksErrorResponse", e)
+                throw FiksClientException(e.statusCode, errorMessage, e)
+            } catch (e: HttpServerErrorException) {
+                val fiksErrorResponse = e.toFiksErrorResponse()?.feilmeldingUtenFnr
+                val errorMessage = e.message?.feilmeldingUtenFnr
+                log.warn("Opplasting av ettersendelse på $digisosId feilet - $errorMessage - $fiksErrorResponse", e)
+                throw FiksServerException(e.statusCode, errorMessage, e)
+            } catch (e: Exception) {
+                log.warn("Opplasting av ettersendelse på $digisosId feilet", e)
+                throw FiksException(e.message?.feilmeldingUtenFnr, e)
+            } finally {
+                cancelFailedEncryptions(krypteringFutureList)
+            }
+    }
+
+    fun lastOppNyEttersendelseMedApache(files: List<FilForOpplasting>, vedleggJson: JsonVedleggSpesifikasjon,
+                                        digisosId: String, navEksternRefId: String, kommunenummer: String, token: String) {
+        log.info("Starter sending av ettersendelse med ${files.size} filer til digisosId=$digisosId")
+
+        val entityBuilder = createApacheClientMultipartEntityBuilder()
         entityBuilder.addTextBody("vedlegg.json", serialiser(vedleggJson), ContentType.APPLICATION_JSON)
 
-        val post = HttpPost("$baseUrl/digisos/api/v1/soknader/$kommunenummer/$digisosId/$navEksternRefId")
-        setHeaders(post, token)
+        val uri = "$baseUrl/digisos/api/v1/soknader/$kommunenummer/$digisosId/$navEksternRefId"
+        val post = HttpPost(uri)
+        setApacheClientHeaders(post, token)
 
         val krypteringFutureList =
                 Collections.synchronizedList(ArrayList<CompletableFuture<Void>>(files.size))
 
         try {
-            appendAndEncryptFiles(entityBuilder, files, krypteringFutureList, token, digisosId)
+            appendAndEncryptFilesForApacheClient(entityBuilder, files, krypteringFutureList, token, digisosId)
             post.entity = entityBuilder.build()
 
             val startTime = System.currentTimeMillis()
             val response = postEttersendlse(post, digisosId)
             val endTime = System.currentTimeMillis()
 
-            throwExceptionsIfUploadFailed(response, digisosId, endTime-startTime)
-            FiksClientImpl.log.info("Sendte ettersendelse til kommune $kommunenummer i Fiks. Tok ${endTime - startTime} ms. Fikk navEksternRefId $navEksternRefId (statusCode: ${response.statusLine.statusCode}) digisosId=$digisosId")
+            throwExceptionsIfApacheClientUploadFailed(response, digisosId, endTime - startTime)
+            log.info("Sendte ettersendelse til kommune $kommunenummer i Fiks. Tok ${endTime - startTime} ms. Fikk navEksternRefId $navEksternRefId (statusCode: ${response.statusLine.statusCode}) digisosId=$digisosId")
 
             waitForEncryption(krypteringFutureList)
         } finally {
@@ -72,7 +124,28 @@ class FiksEttersendelseClientImpl(clientProperties: ClientProperties,
         }
     }
 
-    private fun setHeaders(post: HttpPost, token: String) {
+    private fun setRestTemplateHeaders(token: String): HttpHeaders {
+        val headers = HttpHeaders()
+        headers.accept = Collections.singletonList(MediaType.APPLICATION_JSON)
+        headers.set(HttpHeaders.AUTHORIZATION, token)
+        headers.set(IntegrationUtils.HEADER_INTEGRASJON_ID, fiksIntegrasjonid)
+        headers.set(IntegrationUtils.HEADER_INTEGRASJON_PASSORD, fiksIntegrasjonpassord)
+        headers.contentType = MediaType.MULTIPART_FORM_DATA
+        return headers
+    }
+
+    private fun appendAndEncryptFilesForRestTemplate(body: LinkedMultiValueMap<String, Any>, files: List<FilForOpplasting>,
+                                                     krypteringFutureList: MutableList<CompletableFuture<Void>>, token: String, digisosId: String) {
+        files.forEachIndexed { fileId, file ->
+            val vedleggMetadata = VedleggMetadata(file.filnavn, file.mimetype, file.storrelse)
+            body.add("vedleggSpesifikasjon:$fileId", createHttpEntityOfString(serialiser(vedleggMetadata), "vedleggSpesifikasjon:$fileId"))
+
+            val inputStream = krypteringService.krypter(file.fil, krypteringFutureList, token, "$fileId - $digisosId")
+            body.add("dokument:$fileId", createHttpEntity(inputStream, "dokument:$fileId", file.filnavn,  "application/octet-stream"))
+        }
+    }
+
+    private fun setApacheClientHeaders(post: HttpPost, token: String) {
         post.setHeader("requestid", UUID.randomUUID().toString())
         post.setHeader("Authorization", token)
         post.setHeader("IntegrasjonId", fiksIntegrasjonid)
@@ -80,15 +153,15 @@ class FiksEttersendelseClientImpl(clientProperties: ClientProperties,
         post.setHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
     }
 
-    private fun createMultipartEntityBuilder(): MultipartEntityBuilder {
+    private fun createApacheClientMultipartEntityBuilder(): MultipartEntityBuilder {
         val entitybuilder: MultipartEntityBuilder = MultipartEntityBuilder.create()
         entitybuilder.setCharset(StandardCharsets.UTF_8)
         entitybuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
         return entitybuilder
     }
 
-    private fun appendAndEncryptFiles(entityBuilder: MultipartEntityBuilder, files: List<FilForOpplasting>,
-                                      krypteringFutureList: MutableList<CompletableFuture<Void>>, token: String, digisosId: String) {
+    private fun appendAndEncryptFilesForApacheClient(entityBuilder: MultipartEntityBuilder, files: List<FilForOpplasting>,
+                                                     krypteringFutureList: MutableList<CompletableFuture<Void>>, token: String, digisosId: String) {
         files.forEachIndexed { fileId, file ->
             val vedleggMetadata = VedleggMetadata(file.filnavn, file.mimetype, file.storrelse)
             entityBuilder.addTextBody("vedleggSpesifikasjon:$fileId", serialiser(vedleggMetadata))
@@ -119,7 +192,7 @@ class FiksEttersendelseClientImpl(clientProperties: ClientProperties,
         }
     }
 
-    private fun throwExceptionsIfUploadFailed(response: CloseableHttpResponse, digisosId: String, tidsbruk: Long) {
+    private fun throwExceptionsIfApacheClientUploadFailed(response: CloseableHttpResponse, digisosId: String, tidsbruk: Long) {
         val statusCode = response.statusLine.statusCode
         if (statusCode in 400..499) {
             val fiksErrorResponse = EntityUtils.toString(response.entity).feilmeldingUtenFnr
@@ -158,6 +231,26 @@ class FiksEttersendelseClientImpl(clientProperties: ClientProperties,
         }
     }
 
+    fun createHttpEntityOfString(body: String, name: String): HttpEntity<Any> {
+        return createHttpEntity(body, name, null, "text/plain;charset=UTF-8")
+    }
+
+    fun createHttpEntityOfFile(file: FilForOpplasting, name: String): HttpEntity<Any> {
+        return createHttpEntity(InputStreamResource(file.fil), name, file.filnavn, "application/octet-stream")
+    }
+
+    private fun createHttpEntity(body: Any, name: String, filename: String?, contentType: String): HttpEntity<Any> {
+        val headerMap = LinkedMultiValueMap<String, String>()
+        val builder: ContentDisposition.Builder = ContentDisposition
+                .builder("form-data")
+                .name(name)
+        val contentDisposition: ContentDisposition = if (filename == null) builder.build() else builder.filename(filename).build()
+
+        headerMap.add(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString())
+        headerMap.add(HttpHeaders.CONTENT_TYPE, contentType)
+        return HttpEntity(body, headerMap)
+    }
+
     fun serialiser(@NonNull metadata: Any): String {
         try {
             return objectMapper.writeValueAsString(metadata)
@@ -165,6 +258,12 @@ class FiksEttersendelseClientImpl(clientProperties: ClientProperties,
             throw RuntimeException("Feil under serialisering av metadata", e)
         }
     }
+
+    companion object {
+        val log by logger()
+        private const val SENDING_TIL_FIKS_TIMEOUT = 5 * 60 * 1000 // 5 minutter
+    }
+
 }
 
 data class VedleggMetadata(

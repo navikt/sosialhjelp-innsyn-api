c5bb6562e999d02b1bc7492fe04daf17da583615
martintveter <41987225+martintveter@users.noreply.github.com>
2020-05-25 08:43:45 +0200

    Legger til subjectHandler (#179)
    * Legger til subjectHandler, for å enkelt kunne hente ut brukerId fra token (trengs for tilgangskontroll-sjekk)
    
    * Fjerner testbruker fnr --> Henter ut testbruker natalies fnr fra vault

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/digisosapi/DigisosApiClientImpl.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/digisosapi/DigisosApiClientImpl.kt
@@ -28,7 +28,9 @@ import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestTemplate
 import java.util.*
 
-
+/**
+ * Brukes kun i dev-sbs eller ved lokal testing mot fiks-test
+ */
 @Profile("!(prod-sbs|mock)")
 @Component
 class DigisosApiClientImpl(
@@ -37,6 +39,8 @@ class DigisosApiClientImpl(
         private val idPortenService: IdPortenService,
         private val fiksClientImpl: FiksClientImpl
 ) : DigisosApiClient {
+
+    private val testbrukerNatalie = System.getenv("TESTBRUKER_NATALIE") ?: "11111111111"
 
     private val baseUrl = clientProperties.fiksDigisosEndpointUrl
     private val fiksIntegrasjonIdKommune = clientProperties.fiksIntegrasjonIdKommune
@@ -112,7 +116,7 @@ class DigisosApiClientImpl(
     fun opprettDigisosSak(): String? {
         val httpEntity = HttpEntity("", headers())
         try {
-            val response = restTemplate.exchange("$baseUrl/digisos/api/v1/11415cd1-e26d-499a-8421-751457dfcbd5/ny?sokerFnr=26104500284", HttpMethod.POST, httpEntity, String::class.java)
+            val response = restTemplate.exchange("$baseUrl/digisos/api/v1/11415cd1-e26d-499a-8421-751457dfcbd5/ny?sokerFnr=$testbrukerNatalie", HttpMethod.POST, httpEntity, String::class.java)
             log.info("Opprettet sak hos Fiks. Digisosid: ${response.body}")
             return response.body?.replace("\"", "")
         } catch (e: HttpClientErrorException) {

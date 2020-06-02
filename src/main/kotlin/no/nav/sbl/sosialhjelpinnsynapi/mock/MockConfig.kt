dddd137006601f61db26e5536c7f3376c1e25070
Per-Christian Nielsen <per.christian.nielsen@nav.no>
2020-06-02 13:32:07 +0200

    Mock alt støtt i labs (og lokalt.) Tar bort AzureAdSubjectHandler når vi er i mock-alt.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/MockConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/MockConfig.kt
@@ -6,7 +6,7 @@ import no.nav.sbl.sosialhjelpinnsynapi.common.subjecthandler.SubjectHandlerUtils
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
-@Profile("mock")
+@Profile("mock | mock-alt")
 @Configuration
 class MockConfig {
 

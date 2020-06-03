0a4104898c3d896bb2a600fa552efea14190202b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-06-03 11:24:46 +0200

    /kommuner brukes ikke, og burde ikke være tilgjengelig for omverden(!)

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/KommuneController.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/rest/KommuneController.kt
@@ -3,9 +3,7 @@ package no.nav.sbl.sosialhjelpinnsynapi.rest
 import no.nav.sbl.sosialhjelpinnsynapi.domain.KommuneInfo
 import no.nav.sbl.sosialhjelpinnsynapi.domain.KommuneResponse
 import no.nav.sbl.sosialhjelpinnsynapi.service.kommune.KommuneService
-import no.nav.sbl.sosialhjelpinnsynapi.service.kommune.KommuneStatusDetaljer
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -33,12 +31,5 @@ class KommuneController(
                         erInnsendingEttersendelseDeaktivert = kommuneInfo == null || !kommuneInfo.kanMottaSoknader,
                         erInnsendingEttersendelseMidlertidigDeaktivert = kommuneInfo == null || kommuneInfo.harMidlertidigDeaktivertMottak,
                         tidspunkt = Date()))
-    }
-
-    @Unprotected
-    @GetMapping("/kommuner")
-    fun hentAlleKommuneStatuser(): ResponseEntity<List<KommuneStatusDetaljer>> {
-        val alleKommunerMedStatus = kommuneService.hentAlleKommunerMedStatusStatus()
-        return ResponseEntity.ok(alleKommunerMedStatus)
     }
 }

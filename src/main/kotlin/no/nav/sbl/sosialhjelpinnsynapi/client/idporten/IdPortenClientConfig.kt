39c2eba965da83ebb965f2c4bc44deba70902d97
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-06-24 12:50:13 +0200

    fjerner idporten-properties fra ClientProperties.kt siden de kun brukes i IdPortenClientConfig.kt

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/idporten/IdPortenClientConfig.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/idporten/IdPortenClientConfig.kt
@@ -1,9 +1,9 @@
 package no.nav.sbl.sosialhjelpinnsynapi.client.idporten
 
-import no.nav.sbl.sosialhjelpinnsynapi.config.ClientProperties
 import no.nav.sbl.sosialhjelpinnsynapi.utils.getenv
 import no.nav.sosialhjelp.idporten.client.IdPortenClient
 import no.nav.sosialhjelp.idporten.client.IdPortenProperties
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -11,22 +11,28 @@ import org.springframework.web.client.RestTemplate
 
 @Profile("!mock")
 @Configuration
-class IdPortenClientConfig {
+class IdPortenClientConfig(
+        @Value("\${idporten.token_url}") private val tokenUrl: String,
+        @Value("\${idporten.client_id}") private val clientId: String,
+        @Value("\${idporten.scope}") private val scope: String,
+        @Value("\${idporten.config_url}") private val configUrl: String,
+        @Value("\${idporten.truststore_type}") private val truststoreType: String,
+        @Value("\${idporten.truststore_filepath}") private val truststoreFilepath: String
+) {
 
     @Bean
-    fun idPortenClient(restTemplate: RestTemplate, clientProperties: ClientProperties): IdPortenClient {
-        return IdPortenClient(restTemplate, toIdPortenProperties(clientProperties))
-    }
-
-    private fun toIdPortenProperties(clientProperties: ClientProperties): IdPortenProperties {
-        return IdPortenProperties(
-                clientProperties.idPortenTokenUrl,
-                clientProperties.idPortenClientId,
-                clientProperties.idPortenScope,
-                clientProperties.idPortenConfigUrl,
-                getenv("VIRKSERT_STI", "/var/run/secrets/nais.io/virksomhetssertifikat"),
-                clientProperties.idPortenTruststoreType,
-                clientProperties.idPortenTruststoreFilePath
+    fun idPortenClient(restTemplate: RestTemplate): IdPortenClient {
+        return IdPortenClient(
+                restTemplate = restTemplate,
+                properties = IdPortenProperties(
+                        tokenUrl,
+                        clientId,
+                        scope,
+                        configUrl,
+                        getenv("VIRKSERT_STI", "/var/run/secrets/nais.io/virksomhetssertifikat"),
+                        truststoreType,
+                        truststoreFilepath
+                )
         )
     }
 }

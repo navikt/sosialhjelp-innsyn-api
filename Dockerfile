445d0800fd4c6088a5378f6aa697092d5410fe6c
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-09-18 11:22:03 +0200

    asdf

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM navikt/java:11-appdynamics
 
+ENV APPD_ENABLED=true
+
 ENV LC_ALL="no_NB.UTF-8"
 ENV LANG="no_NB.UTF-8"
 ENV TZ="Europe/Oslo"
--- nais/scripts/09-appdynamics-env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export APPD_ENABLED=true
-export APPD_TIER="${NAIS_NAMESPACE}_${NAIS_APP_NAME}"
-export APP_NAME="sosialhjelp-innsyn-api"
-export APPDYNAMICS_CONTROLLER_HOST_NAME="appdynamics.adeo.no"
-export APPDYNAMICS_CONTROLLER_PORT="443"
-export APPDYNAMICS_CONTROLLER_SSL_ENABLED="true"

22689b2182ce4dede2ffedbef69022b78e116097
joeldlin <59644512+joeldlin@users.noreply.github.com>
2020-09-29 14:17:03 +0200

    Legger til kode for generering av PdfA. (#203)
    * Legger til kode for generering av PdfA.
    
    * Fjernet bokstav.
    
    * Font blir embeddet i dokumentet nå.
    
    * Med endring fra png til jpg skal nå logo fungere med pdf/a.
    
    * PDF/A Test har blitt oppdatert.
    
    * La til .pdf i generering av filen i test
    
    * Fjernet ubrukt kode.
    
    * Fjernet if-else og la til bare Assertions.assertTrue() og la til fail() i catch().

--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/service/pdf/EttersendelsePdfGeneratorTest.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/service/pdf/EttersendelsePdfGeneratorTest.kt
@@ -1,8 +1,16 @@
 package no.nav.sbl.sosialhjelpinnsynapi.service.pdf
 
 import no.nav.sbl.sosialhjelpinnsynapi.rest.OpplastetVedleggMetadata
+import org.apache.pdfbox.preflight.PreflightDocument
+import org.apache.pdfbox.preflight.ValidationResult
+import org.apache.pdfbox.preflight.exception.SyntaxValidationException
+import org.apache.pdfbox.preflight.parser.PreflightParser
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+import java.io.File
 import java.io.FileOutputStream
 import java.util.*
 
@@ -26,6 +34,33 @@ class EttersendelsePdfGeneratorTest {
             out.close()
         } catch (e:Exception) {
             e.printStackTrace()
+        }
+    }
+
+    @Test
+    fun `skal generere pdfA`() {
+        val metadata = Collections.emptyList<OpplastetVedleggMetadata>()
+
+        val bytes = ettersendelsePdfGenerator.generate(metadata, ident)
+        val file = File("pdfaTest.pdf")
+        file.writeBytes(bytes)
+
+        val parser = PreflightParser(file)
+        val result: ValidationResult
+
+        try {
+            parser.parse()
+            val document = parser.preflightDocument
+            document.validate()
+            result = document.result
+            Assertions.assertTrue(result.isValid)
+            document.close()
+        }
+        catch(e: SyntaxValidationException){
+            fail("Exception when checking validity of pdf/a. Exception message", e)
+        }
+        finally {
+            file.deleteOnExit()
         }
     }
 }

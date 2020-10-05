78bdfc7b4943952c1a1d7043c57369b40b7282c1
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-10-05 15:36:26 +0200

    Setter requestAttributes til RequestContextHolder ved opprettelse av coroutines.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/service/utbetalinger/UtbetalingerService.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/service/utbetalinger/UtbetalingerService.kt
@@ -1,15 +1,20 @@
 package no.nav.sbl.sosialhjelpinnsynapi.service.utbetalinger
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import no.nav.sbl.sosialhjelpinnsynapi.client.fiks.FiksClient
 import no.nav.sbl.sosialhjelpinnsynapi.domain.InternalDigisosSoker
 import no.nav.sbl.sosialhjelpinnsynapi.domain.ManedUtbetaling
 import no.nav.sbl.sosialhjelpinnsynapi.domain.UtbetalingerResponse
 import no.nav.sbl.sosialhjelpinnsynapi.domain.UtbetalingsStatus
 import no.nav.sbl.sosialhjelpinnsynapi.event.EventService
+import no.nav.sbl.sosialhjelpinnsynapi.utils.flatMapParallel
 import no.nav.sbl.sosialhjelpinnsynapi.utils.logger
+import no.nav.sbl.sosialhjelpinnsynapi.utils.setRequestAttributes
 import no.nav.sosialhjelp.api.fiks.DigisosSak
 import org.joda.time.DateTime
 import org.springframework.stereotype.Component
+import org.springframework.web.context.request.RequestContextHolder
 import java.text.DateFormatSymbols
 import java.time.LocalDate
 import java.time.YearMonth
@@ -32,10 +37,16 @@ class UtbetalingerService(
             return emptyList()
         }
 
-        val alleUtbetalinger: List<ManedUtbetaling> =
-                    digisosSaker
-                            .filter { isDigisosSakNewerThanMonths(it, months) }
-                            .flatMap { manedsutbetalinger(token, it) }
+        val requestAttributes = RequestContextHolder.getRequestAttributes()
+
+        val alleUtbetalinger = runBlocking(Dispatchers.IO) {
+            digisosSaker
+                    .filter { isDigisosSakNewerThanMonths(it, months) }
+                    .flatMapParallel {
+                        setRequestAttributes(requestAttributes)
+                        manedsutbetalinger(token, it)
+                    }
+        }
 
         return alleUtbetalinger
                 .sortedByDescending { it.utbetalingsdato }
@@ -50,7 +61,7 @@ class UtbetalingerService(
                 }
     }
 
-    private fun manedsutbetalinger(token: String, digisosSak: DigisosSak): List<ManedUtbetaling> {
+    private suspend fun manedsutbetalinger(token: String, digisosSak: DigisosSak): List<ManedUtbetaling> {
         val model = eventService.hentAlleUtbetalinger(token, digisosSak)
         return model.utbetalinger
                 .filter { it.utbetalingsDato != null && it.status == UtbetalingsStatus.UTBETALT }

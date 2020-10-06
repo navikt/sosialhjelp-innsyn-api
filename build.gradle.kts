f0fc77a49827ed35b3a9be8dbbead3826df78966
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-10-06 08:30:40 +0200

    Tester kotlinx-coroutines-slf4j med MDCContext for å ivareta MDC-context i ny coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
 
 //    Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${Versions.coroutines}")
 
 //    Spring
     implementation("org.springframework.boot:spring-boot-starter-web:${Versions.springBoot}")

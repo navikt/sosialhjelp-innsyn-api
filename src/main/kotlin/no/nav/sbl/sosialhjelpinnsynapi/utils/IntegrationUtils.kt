4c1454180e69dfc7f3b683f269ec50cf7bc9ef78
siljee <silje.hansen3@nav.no>
2020-10-09 14:58:55 +0200

    PDL-client og endepunkt for å sjekke /tilgang (#231)
    PDL-client og endepunkt for å sjekke /tilgang
    Lar tilgangsjekk på de andre endepunktene komme i en senere PR. Prodsetter i denne rtekkefølgen:
    * Først prodsette /tilgang-endepunktet (denne PR)
    * Deretter prodsette frontend for håntering av både /tilgang og 403-respons.
    * Til slutt prodsette tilgangssjekk på alle endepunkter. (som returnerer 403)
    
    Hva er gjort (utdrag fra gammel PR):
    * PdlClient - med selftestsjekk
    * Vi har kun fått apikey for q1, derfor går PDL mot q1 også i q0.
    * Nytt endepunkt /tilgang, for å sjekke om bruker har tilgang, inkluderer fornavn
    * Oppdaterer StsClient. POST-kall skal ha grant_type og scope som body.
    * Mock for pdlClient
    * Klargjør exceptions for tilgangskontrollsjekken, og håndterer disse.
    
    * Endrer til forwardheaders for å få med tracing

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/IntegrationUtils.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/IntegrationUtils.kt
@@ -12,10 +12,14 @@ object IntegrationUtils {
 
     const val KILDE_INNSYN_API = "innsyn-api"
 
+    const val BEARER = "Bearer "
+
     const val HEADER_CALL_ID = "Nav-Call-Id"
     const val HEADER_NAV_APIKEY = "x-nav-apiKey"
+    const val HEADER_CONSUMER_TOKEN = "Nav-Consumer-Token"
+    const val HEADER_TEMA = "Tema"
 
-    const val BEARER = "Bearer "
+    const val TEMA_KOM = "KOM"
 
     const val X_REQUEST_ID = "x-request-id"
     const val X_B3_TRACEID = "x-b3-traceid"

2bf12d03c20b8c2bc638649a77f116371b388187
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2020-10-09 10:44:08 +0200

    Skrur på istio tracing i gcp.

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/IntegrationUtils.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/utils/IntegrationUtils.kt
@@ -1,6 +1,7 @@
 package no.nav.sbl.sosialhjelpinnsynapi.utils
 
 import no.nav.sbl.sosialhjelpinnsynapi.config.ClientProperties
+import org.slf4j.MDC
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import java.util.*
@@ -16,12 +17,31 @@ object IntegrationUtils {
 
     const val BEARER = "Bearer "
 
+    const val X_REQUEST_ID = "x-request-id"
+    const val X_B3_TRACEID = "x-b3-traceid"
+    const val X_B3_SPANID = "x-b3-spanid"
+    const val X_B3_PARENTSPANID = "x-b3-parentspanid"
+    const val X_B3_SAMPLED = "x-b3-sampled"
+    const val X_B3_FLAGS = "x-b3-flags"
+    const val X_OT_SPAN_CONTEXT = "x-ot-span-context"
+
     fun fiksHeaders(clientProperties: ClientProperties, token: String): HttpHeaders {
-        val headers = HttpHeaders()
+        val headers = forwardHeaders()
         headers.accept = Collections.singletonList(MediaType.APPLICATION_JSON)
         headers.set(HttpHeaders.AUTHORIZATION, token)
         headers.set(HEADER_INTEGRASJON_ID, clientProperties.fiksIntegrasjonId)
         headers.set(HEADER_INTEGRASJON_PASSORD, clientProperties.fiksIntegrasjonpassord)
+        return headers
+    }
+
+    fun forwardHeaders(headers: HttpHeaders = HttpHeaders()): HttpHeaders {
+        MDC.get(X_REQUEST_ID)?.let { headers.set(X_REQUEST_ID, it) }
+        MDC.get(X_B3_TRACEID)?.let { headers.set(X_B3_TRACEID, it) }
+        MDC.get(X_B3_SPANID)?.let { headers.set(X_B3_SPANID, it) }
+        MDC.get(X_B3_PARENTSPANID)?.let { headers.set(X_B3_PARENTSPANID, it) }
+        MDC.get(X_B3_SAMPLED)?.let { headers.set(X_B3_SAMPLED, it) }
+        MDC.get(X_B3_FLAGS)?.let { headers.set(X_B3_FLAGS, it) }
+        MDC.get(X_OT_SPAN_CONTEXT)?.let { headers.set(X_OT_SPAN_CONTEXT, it) }
         return headers
     }
 }

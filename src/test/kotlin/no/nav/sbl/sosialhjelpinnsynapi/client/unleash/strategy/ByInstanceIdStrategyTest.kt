c551a8a1a71dfb775a189cf99c11d102e9bc6d01
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-12-23 12:58:35 +0100

    endrer litt på ByInstanceIdStrategy. Tar inn currentInstanceId som parameter fremfor å hente fra System.properties

--- src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/unleash/strategy/ByInstanceIdStrategyTest.kt
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/unleash/strategy/ByInstanceIdStrategyTest.kt
@@ -1,29 +1,21 @@
 package no.nav.sbl.sosialhjelpinnsynapi.client.unleash.strategy
 
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ByInstanceIdStrategyTest {
 
-    private val strategy = ByInstanceIdStrategy()
-
-    @AfterEach
-    internal fun tearDown() {
-        System.clearProperty("unleash_instance_id")
-    }
-
     @Test
     fun shouldReturnFalse_instanceIdNotInMap() {
-        System.setProperty("unleash_instance_id", "local")
+        val strategy = ByInstanceIdStrategy("local")
         val parameters = mutableMapOf("instance.id" to "dev-sbs,dev-sbs-intern")
         assertFalse(strategy.isEnabled(parameters))
     }
 
     @Test
     fun shoudReturnTrue_instanceIdInMap() {
-        System.setProperty("unleash_instance_id", "dev-sbs")
+        val strategy = ByInstanceIdStrategy("dev-sbs")
         val parameters = mutableMapOf("instance.id" to "dev-sbs,dev-sbs-intern")
         assertTrue(strategy.isEnabled(parameters))
     }

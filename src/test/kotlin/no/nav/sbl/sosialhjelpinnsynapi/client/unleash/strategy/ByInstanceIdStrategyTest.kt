e0c5f798935401d2dfa953209bd8b25c875213cb
martintveter <41987225+martintveter@users.noreply.github.com>
2021-01-05 12:00:11 +0100

    Feature/unleash client (#286)

---
+++ src/test/kotlin/no/nav/sbl/sosialhjelpinnsynapi/client/unleash/strategy/ByInstanceIdStrategyTest.kt
@@ -0,0 +1,22 @@
+package no.nav.sbl.sosialhjelpinnsynapi.client.unleash.strategy
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ByInstanceIdStrategyTest {
+
+    @Test
+    fun shouldReturnFalse_instanceIdNotInMap() {
+        val strategy = ByInstanceIdStrategy("local")
+        val parameters = mutableMapOf("instance.id" to "dev-sbs,dev-sbs-intern")
+        assertFalse(strategy.isEnabled(parameters))
+    }
+
+    @Test
+    fun shoudReturnTrue_instanceIdInMap() {
+        val strategy = ByInstanceIdStrategy("dev-sbs")
+        val parameters = mutableMapOf("instance.id" to "dev-sbs,dev-sbs-intern")
+        assertTrue(strategy.isEnabled(parameters))
+    }
+}

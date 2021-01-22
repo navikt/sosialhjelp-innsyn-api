b8ba6abdc97e4aca798888ba7b430fff7ba37900
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-01-22 10:10:14 +0100

    revert asd

--- src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/KommuneInfoClientMock.kt
+++ src/main/kotlin/no/nav/sbl/sosialhjelpinnsynapi/mock/KommuneInfoClientMock.kt
@@ -11,28 +11,28 @@ import java.util.*
 class KommuneInfoClientMock : KommuneInfoClient {
 
     override fun get(kommunenummer: String, token: String): KommuneInfo {
-        return KommuneInfo(kommunenummer, true, false, false, false, null, true, null)
+        return KommuneInfo(kommunenummer, true, true, false, false, null, true, null)
     }
 
     override fun getAll(token: String): List<KommuneInfo> {
         val returnValue = ArrayList<KommuneInfo>()
-        returnValue.add(KommuneInfo("0001", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("1123", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("0002", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("9863", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("9999", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("2352", true, false, false, false, null, true, null))
+        returnValue.add(KommuneInfo("0001", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("1123", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("0002", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("9863", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("9999", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("2352", true, true, false, false, null, true, null))
         returnValue.add(KommuneInfo("0000", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("8734", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("0909", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("0301", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("1222", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("9002", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("6663", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("1201", true, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("4455", true, false, false, true, null, true, null))
+        returnValue.add(KommuneInfo("8734", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("0909", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("0301", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("1222", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("9002", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("6663", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("1201", true, true, false, false, null, true, null))
+        returnValue.add(KommuneInfo("4455", true, true, false, true, null, true, null))
         returnValue.add(KommuneInfo("1833", false, false, false, false, null, true, null))
-        returnValue.add(KommuneInfo("1430", true, false, true, true, null, true, null))
+        returnValue.add(KommuneInfo("1430", true, true, true, true, null, true, null))
         return returnValue
     }
 }

db88c68d4b7354cf51907b28893d5679701fd9fd
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-03-12 09:40:37 +0100

    bump sosialhjelp-common (forelopig snapshot-versjon)

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/client/fiks/KommuneInfoClientConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/client/fiks/KommuneInfoClientConfig.kt
@@ -7,16 +7,16 @@ import no.nav.sosialhjelp.innsyn.config.ClientProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 
 @Profile("!mock")
 @Configuration
 class KommuneInfoClientConfig {
 
     @Bean
-    fun kommuneInfoClient(restTemplate: RestTemplate, clientProperties: ClientProperties): KommuneInfoClient {
+    fun kommuneInfoClient(clientProperties: ClientProperties): KommuneInfoClient {
         return KommuneInfoClientImpl(
-                restTemplate,
+                WebClient.create(),
                 toFiksProperties(clientProperties)
         )
     }

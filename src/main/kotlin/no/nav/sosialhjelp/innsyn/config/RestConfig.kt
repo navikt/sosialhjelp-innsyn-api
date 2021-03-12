49049e813fb4347e6cdfd2cb4a3c190f1e8df826
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-03-12 11:45:37 +0100

    webclient config

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/config/RestConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/config/RestConfig.kt
@@ -7,26 +7,15 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
-import reactor.netty.http.client.HttpClient
 import java.nio.charset.StandardCharsets
 import java.time.Duration
 
 @Configuration
-class RestConfig(
-    private val webClientBuilder: WebClient.Builder
-) {
-
-    @Bean
-    fun webClient(): WebClient =
-        webClientBuilder
-            .clientConnector(ReactorClientHttpConnector(HttpClient.newConnection()))
-            .build()
+class RestConfig {
 
     @Bean
     fun restTemplate(builder: RestTemplateBuilder): RestTemplate =

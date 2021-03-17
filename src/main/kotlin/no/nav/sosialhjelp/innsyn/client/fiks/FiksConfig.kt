0eb69998b9f068e6330ffaafc58829c3369189b7
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-03-17 11:34:19 +0100

    fiksWebClient konfigurert med objectMapper

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/client/fiks/FiksConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/client/fiks/FiksConfig.kt
@@ -1,8 +1,11 @@
 package no.nav.sosialhjelp.innsyn.client.fiks
 
 import no.nav.sosialhjelp.innsyn.config.ClientProperties
+import no.nav.sosialhjelp.innsyn.utils.objectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -16,6 +19,10 @@ class FiksConfig(
         proxiedWebClient
             .mutate()
             .baseUrl(clientProperties.fiksDigisosEndpointUrl)
-            .codecs { it.defaultCodecs().maxInMemorySize(16 * 1024 * 1024) }
+            .codecs {
+                it.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)
+                it.customCodecs().register(Jackson2JsonDecoder(objectMapper))
+                it.customCodecs().register(Jackson2JsonEncoder(objectMapper))
+            }
             .build()
 }

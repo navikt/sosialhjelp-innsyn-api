ceef12a2fd8fec44886812ce22d6a8269feb6f54
martintveter <41987225+martintveter@users.noreply.github.com>
2021-03-19 08:39:57 +0100

    pdlClient: resttemplate -> webclient (#341)

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/config/RestConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/config/RestConfig.kt
@@ -1,18 +1,14 @@
 package no.nav.sosialhjelp.innsyn.config
 
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.HEADER_NAV_APIKEY
 import no.nav.sosialhjelp.innsyn.utils.objectMapper
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
-import java.nio.charset.StandardCharsets
-import java.time.Duration
 
 @Configuration
 class RestConfig {
@@ -20,16 +16,6 @@ class RestConfig {
     @Bean
     fun restTemplate(builder: RestTemplateBuilder): RestTemplate =
             builder.build()
-
-    @Bean
-    @Profile("!(mock | local)")
-    fun pdlRestTemplate(builder: RestTemplateBuilder): RestTemplate =
-            builder
-                    .basicAuthentication(System.getenv(SRVSOSIALHJELP_INNSYN_API_USERNAME), System.getenv(SRVSOSIALHJELP_INNSYN_API_PASSWORD), StandardCharsets.UTF_8)
-                    .setConnectTimeout(Duration.ofSeconds(15))
-                    .setReadTimeout(Duration.ofMinutes(1))
-                    .defaultHeader(HEADER_NAV_APIKEY, System.getenv(PDL_APIKEY))
-                    .build()
 
     @Bean
     fun objectMapperCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
@@ -49,13 +35,5 @@ class RestConfig {
         converters.add(jackson)
         reqAdapter.messageConverters = converters
         return jackson
-    }
-
-    companion object {
-        private const val SRVSOSIALHJELP_INNSYN_API_USERNAME: String = "SRVSOSIALHJELP_INNSYN_API_USERNAME"
-        private const val SRVSOSIALHJELP_INNSYN_API_PASSWORD: String = "SRVSOSIALHJELP_INNSYN_API_PASSWORD"
-
-        private const val STSTOKEN_APIKEY: String = "SOSIALHJELP_INNSYN_API_STSTOKEN_APIKEY_PASSWORD"
-        private const val PDL_APIKEY: String = "SOSIALHJELP_INNSYN_API_PDL_APIKEY_PASSWORD"
     }
 }

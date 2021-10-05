8f18a6cf48578c036f3217e85d0d3760de0d5d2b
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-10-05 15:13:26 +0200

    Slår sammen KrypteringService, KrypteringServiceImpl og KrypteringServiceMock

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/service/vedlegg/KrypteringService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/service/vedlegg/KrypteringService.kt
@@ -1,10 +1,72 @@
 package no.nav.sosialhjelp.innsyn.service.vedlegg
 
+import no.ks.kryptering.CMSKrypteringImpl
+import no.nav.sosialhjelp.innsyn.utils.logger
+import no.nav.sosialhjelp.innsyn.utils.runAsyncWithMDC
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+import java.io.IOException
 import java.io.InputStream
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
+import java.security.Security
 import java.security.cert.X509Certificate
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 interface KrypteringService {
 
     fun krypter(fileInputStream: InputStream, krypteringFutureList: MutableList<CompletableFuture<Void>>, certificate: X509Certificate): InputStream
+}
+
+@Profile("!mock-alt")
+@Component
+class KrypteringServiceImpl : KrypteringService {
+
+    private val executor = Executors.newFixedThreadPool(4)
+    private val kryptering = CMSKrypteringImpl()
+
+    override fun krypter(fileInputStream: InputStream, krypteringFutureList: MutableList<CompletableFuture<Void>>, certificate: X509Certificate): InputStream {
+        val pipedInputStream = PipedInputStream()
+        try {
+            val pipedOutputStream = PipedOutputStream(pipedInputStream)
+            val krypteringFuture = runAsyncWithMDC(
+                {
+                    try {
+                        log.debug("Starter kryptering")
+                        kryptering.krypterData(pipedOutputStream, fileInputStream, certificate, Security.getProvider("BC"))
+                        log.debug("Ferdig med kryptering")
+                    } catch (e: Exception) {
+                        log.error("Det skjedde en feil ved kryptering, exception blir lagt til kryptert InputStream", e)
+                        throw IllegalStateException("An error occurred during encryption", e)
+                    } finally {
+                        try {
+                            log.debug("Lukker kryptering OutputStream")
+                            pipedOutputStream.close()
+                            log.debug("OutputStream for kryptering er lukket")
+                        } catch (e: IOException) {
+                            log.error("Lukking av Outputstream for kryptering feilet", e)
+                        }
+                    }
+                },
+                executor
+            )
+            krypteringFutureList.add(krypteringFuture)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+        return pipedInputStream
+    }
+
+    companion object {
+        private val log by logger()
+    }
+}
+
+@Profile("mock-alt")
+@Component
+class KrypteringServiceMock : KrypteringService {
+    override fun krypter(fileInputStream: InputStream, krypteringFutureList: MutableList<CompletableFuture<Void>>, certificate: X509Certificate): InputStream {
+        return fileInputStream
+    }
 }

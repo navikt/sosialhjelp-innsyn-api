3dac8b26748629525a6b0326b2b771938d614b9f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2021-11-23 13:55:38 +0100

    registerKotlinModule

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/utils/Objects.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/utils/Objects.kt
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.nav.sbl.soknadsosialhjelp.json.JsonSosialhjelpObjectMapper
 
 val objectMapper: ObjectMapper = JsonSosialhjelpObjectMapper.createObjectMapper()
-    .registerModules(JavaTimeModule(), KotlinModule())
+    .registerKotlinModule()
+    .registerModule(JavaTimeModule())
     .configure(SerializationFeature.INDENT_OUTPUT, true)
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)

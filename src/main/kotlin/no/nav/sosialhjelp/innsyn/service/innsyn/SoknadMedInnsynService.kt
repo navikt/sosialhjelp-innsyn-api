e7f7680e7eef525b09f7294a560a3df01a250cbb
linemos <line.moseng@nav.no>
2022-02-11 11:07:15 +0100

    Endepunkt for å sjekke om bruker har søknader med innsyn siste 12 måneder (#469)
    Co-authored-by: jorel <jorgen.elden.lindgren@nav.no>

---
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/service/innsyn/SoknadMedInnsynService.kt
@@ -0,0 +1,21 @@
+package no.nav.sosialhjelp.innsyn.service.innsyn
+
+import no.nav.sosialhjelp.innsyn.client.fiks.FiksClient
+import no.nav.sosialhjelp.innsyn.dittnav.DittNavOppgaverService.Companion.isDigisosSakNewerThanMonths
+import no.nav.sosialhjelp.innsyn.service.kommune.KommuneService
+import org.springframework.stereotype.Component
+
+@Component
+class SoknadMedInnsynService(
+    private val fiksClient: FiksClient,
+    private val kommuneService: KommuneService
+) {
+
+    fun harSoknaderMedInnsyn(token: String): Boolean {
+        val saker = fiksClient.hentAlleDigisosSaker(token)
+
+        return saker
+            .filter { isDigisosSakNewerThanMonths(it, 12) }
+            .any { !kommuneService.erInnsynDeaktivertForKommune(it.fiksDigisosId, token) }
+    }
+}

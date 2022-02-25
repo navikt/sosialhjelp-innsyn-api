dd8371f81f4c750d01966699a1050958a7e1f9a7
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-02-25 11:50:34 +0100

    også fjern serviceuser, siden den ikke trengs lengre

--- nais/scripts/90-set-secrets.sh
+++
@@ -1,3 +0,0 @@
-echo "Export serviceuser credentials"
-export SRVSOSIALHJELP_INNSYN_API_USERNAME=$(cat /serviceuser/srvsosial-inn-fss/username)
-export SRVSOSIALHJELP_INNSYN_API_PASSWORD=$(cat /serviceuser/srvsosial-inn-fss/password)

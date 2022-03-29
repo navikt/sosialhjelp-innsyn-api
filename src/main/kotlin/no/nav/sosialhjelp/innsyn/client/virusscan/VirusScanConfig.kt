90ed6e26e0fa2b445c16c268c3cb276b604e398f
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-03-29 13:47:03 +0200

    Endrer virus-scan til å være støttet både fra on-prem og i gcp

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/client/virusscan/VirusScanConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/client/virusscan/VirusScanConfig.kt
@@ -1,21 +1,20 @@
 package no.nav.sosialhjelp.innsyn.client.virusscan
 
 import no.nav.sosialhjelp.innsyn.utils.HttpClientUtil.getUnproxiedReactorClientHttpConnector
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-class VirusScanConfig {
+class VirusScanConfig(
+    @Value("\${innsyn.vedlegg.virusscan.url}") private val clamAvUrl: String
+) {
 
     @Bean
     fun virusScanWebClient(webClientBuilder: WebClient.Builder) =
         webClientBuilder
-            .baseUrl(DEFAULT_CLAM_URI)
+            .baseUrl(clamAvUrl)
             .clientConnector(getUnproxiedReactorClientHttpConnector())
             .build()
-
-    companion object {
-        private const val DEFAULT_CLAM_URI = "http://clamav.nais.svc.nais.local/scan"
-    }
 }

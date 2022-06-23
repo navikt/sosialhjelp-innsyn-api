4642e084eb826f40617b0d5680eb4d188126f632
martintveter <41987225+martintveter@users.noreply.github.com>
2022-06-23 10:53:27 +0200

    bump sosialhjelp-common. (#529)
    Kan fjerne "allow-circular-references" fra application.yml

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/health/HealthConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/health/HealthConfig.kt
@@ -1,24 +1,25 @@
 package no.nav.sosialhjelp.innsyn.health
 
+import io.micrometer.core.instrument.MeterRegistry
 import no.nav.sosialhjelp.innsyn.utils.MiljoUtils
 import no.nav.sosialhjelp.selftest.DependencyCheck
-import no.nav.sosialhjelp.selftest.SelftestMeterBinder
 import no.nav.sosialhjelp.selftest.SelftestService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class HealthConfig(
-    private val dependencyChecks: List<DependencyCheck>
+    private val dependencyChecks: List<DependencyCheck>,
+    private val meterRegistry: MeterRegistry
 ) {
 
     @Bean
     fun selftestService(): SelftestService {
-        return SelftestService("sosialhjelp-innsyn-api", MiljoUtils.getAppImage(), dependencyChecks)
-    }
-
-    @Bean
-    fun selftestMeterBinder(selftestService: SelftestService): SelftestMeterBinder {
-        return SelftestMeterBinder(selftestService)
+        return SelftestService(
+            appName = "sosialhjelp-innsyn-api",
+            version = MiljoUtils.getAppImage(),
+            dependencyChecks = dependencyChecks,
+            meterRegistry = meterRegistry
+        )
     }
 }

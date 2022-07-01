063e9bf4369ea61c1425af458cc99054ea3e94a2
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-01 12:25:20 +0200

    fjerner noen ubruke metoder

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/utils/MiljoUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/utils/MiljoUtils.kt
@@ -3,7 +3,6 @@ package no.nav.sosialhjelp.innsyn.utils
 object MiljoUtils {
     private const val NAIS_APP_IMAGE = "NAIS_APP_IMAGE"
     private const val NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME"
-    private const val NAIS_APP_NAME = "NAIS_APP_NAME"
 
     fun getAppImage(): String {
         return getenv(NAIS_APP_IMAGE, "version")
@@ -12,21 +11,5 @@ object MiljoUtils {
     fun isRunningInProd(): Boolean {
         val clusterName = System.getenv(NAIS_CLUSTER_NAME)
         return clusterName != null && clusterName.contains("prod")
-    }
-
-    fun getDomain(): String {
-        return when (getenv(NAIS_CLUSTER_NAME, "prod-sbs")) {
-            "dev-sbs" -> "www-q0.dev.nav.no"
-            "dev-gcp" -> {
-                val env = if (getAppName().contains("-dev")) "" else ".ekstern"
-                "digisos$env.dev.nav.no"
-            }
-            "labs-gcp" -> "digisos.labs.nais.io"
-            else -> "www.nav.no"
-        }
-    }
-
-    private fun getAppName(): String {
-        return getenv(NAIS_APP_NAME, "sosialhjelp-innsyn-api")
     }
 }

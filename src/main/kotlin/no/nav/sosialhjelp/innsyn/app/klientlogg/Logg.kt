51b88ad69f785b1fdbb5e11da8f7a0297498c110
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-06 12:18:23 +0200

    klientlogg refactor

---
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/klientlogg/Logg.kt
@@ -0,0 +1,20 @@
+package no.nav.sosialhjelp.innsyn.app.klientlogg
+
+data class Logg(
+    val level: String,
+    val message: String,
+    val jsFileUrl: String,
+    val lineNumber: String,
+    val columnNumber: String,
+    val url: String,
+    val userAgent: String
+) {
+    fun melding(): String {
+        var useragentWithoutSpaceAndComma = ""
+        if (userAgent.isNotEmpty()) {
+            val useragentWithoutSpace = userAgent.replace(" ".toRegex(), "_")
+            useragentWithoutSpaceAndComma = useragentWithoutSpace.replace(",".toRegex(), "_")
+        }
+        return "jsmessagehash=${message.hashCode()}, fileUrl=$jsFileUrl:$lineNumber:$columnNumber, url=$url, userAgent=$useragentWithoutSpaceAndComma, melding: $message"
+    }
+}

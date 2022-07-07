541b0412ba6920c306e896dcb3ba5249ba0ec369
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-07 08:08:28 +0200

    `utbetalinger` pakke under `digisossak`

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/domain/EndepunktResponser.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/domain/EndepunktResponser.kt
@@ -49,33 +49,6 @@ data class DokumentasjonkravElement(
     val utbetalingsReferanse: List<String>?
 )
 
-data class UtbetalingerResponse(
-    val ar: Int,
-    val maned: String,
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    val foersteIManeden: LocalDate,
-    val utbetalinger: List<ManedUtbetaling>
-)
-
-data class ManedUtbetaling(
-    val tittel: String,
-    val belop: Double,
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    val utbetalingsdato: LocalDate?,
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    val forfallsdato: LocalDate?,
-    val status: String,
-    val fiksDigisosId: String,
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    val fom: LocalDate?,
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    val tom: LocalDate?,
-    val mottaker: String?,
-    val annenMottaker: Boolean,
-    val kontonummer: String?,
-    val utbetalingsmetode: String?,
-)
-
 data class SaksListeResponse(
     val fiksDigisosId: String,
     val soknadTittel: String,

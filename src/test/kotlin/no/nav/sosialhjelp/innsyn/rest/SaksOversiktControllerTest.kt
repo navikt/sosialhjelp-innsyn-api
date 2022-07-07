6f882870ae369e9ab2ef08f0fa21abfcb7e2c9ea
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-07 12:13:42 +0200

    ny package `dialogstatus`. Trekker ut relevante endepunkter fra SaksOversiktController til DialogStatusController

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/rest/SaksOversiktControllerTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/rest/SaksOversiktControllerTest.kt
@@ -7,14 +7,10 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
-import kotlinx.coroutines.runBlocking
 import no.finn.unleash.Unleash
 import no.nav.sosialhjelp.api.fiks.DigisosSak
-import no.nav.sosialhjelp.api.fiks.OriginalSoknadNAV
-import no.nav.sosialhjelp.innsyn.app.ClientProperties
 import no.nav.sosialhjelp.innsyn.app.subjecthandler.StaticSubjectHandlerImpl
 import no.nav.sosialhjelp.innsyn.app.subjecthandler.SubjectHandlerUtils
-import no.nav.sosialhjelp.innsyn.client.dialog.DialogClient
 import no.nav.sosialhjelp.innsyn.client.fiks.FiksClient
 import no.nav.sosialhjelp.innsyn.digisossak.oppgaver.DokumentasjonkravElement
 import no.nav.sosialhjelp.innsyn.digisossak.oppgaver.DokumentasjonkravResponse
@@ -42,11 +38,9 @@ internal class SaksOversiktControllerTest {
     private val eventService: EventService = mockk()
     private val oppgaveService: OppgaveService = mockk()
     private val tilgangskontroll: Tilgangskontroll = mockk()
-    private val dialogClient: DialogClient = mockk()
-    private val clientProperties: ClientProperties = mockk()
     private val unleashClient: Unleash = mockk()
 
-    private val controller = SaksOversiktController(fiksClient, eventService, oppgaveService, tilgangskontroll, dialogClient, clientProperties, unleashClient)
+    private val controller = SaksOversiktController(fiksClient, eventService, oppgaveService, tilgangskontroll, unleashClient)
 
     private val digisosSak1: DigisosSak = mockk()
     private val digisosSak2: DigisosSak = mockk()
@@ -125,74 +119,6 @@ internal class SaksOversiktControllerTest {
             val second = saker[1]
             assertThat(second.soknadTittel).isEqualTo("Søknad om økonomisk sosialhjelp")
             assertThat(second.kilde).isEqualTo(KILDE_INNSYN_API)
-        }
-    }
-
-    @Test
-    fun skalSjekkeOmSisteSoknadErSendtTilRettKommune_riktigKommune() {
-        every { fiksClient.hentAlleDigisosSaker(any()) } returns listOf(digisosSak1)
-        every { digisosSak1.kommunenummer } returns "0301"
-        every { clientProperties.meldingerKommunenummer } returns "0301"
-
-        runBlocking {
-            val response = controller.skalViseMeldingerLenke("token")
-            val resultat = response.body
-            assertThat(resultat).isEqualTo(true)
-        }
-    }
-
-    @Test
-    fun skalSjekkeOmSisteSoknadErSendtTilRettKommune_ingenSoknader() {
-        every { fiksClient.hentAlleDigisosSaker(any()) } returns emptyList()
-        every { clientProperties.meldingerKommunenummer } returns "0301"
-
-        runBlocking {
-            val response = controller.skalViseMeldingerLenke("token")
-            val resultat = response.body
-            assertThat(resultat).isEqualTo(false)
-        }
-    }
-
-    @Test
-    fun skalSjekkeOmSisteSoknadErSendtTilRettKommune_feilKommune() {
-        every { fiksClient.hentAlleDigisosSaker(any()) } returns listOf(digisosSak1)
-        every { digisosSak1.kommunenummer } returns "1234"
-        every { clientProperties.meldingerKommunenummer } returns "0301"
-
-        runBlocking {
-            val response = controller.skalViseMeldingerLenke("token")
-            val resultat = response.body
-            assertThat(resultat).isEqualTo(false)
-        }
-    }
-
-    @Test
-    fun skalSjekkeOmSisteSoknadErSendtTilRettKommune_riktigSortert() {
-        val digisosSakEldst: DigisosSak = mockk()
-        val orgSoknadNavEldst: OriginalSoknadNAV = mockk()
-        every { digisosSakEldst.originalSoknadNAV } returns orgSoknadNavEldst
-        every { orgSoknadNavEldst.timestampSendt } returns 1L
-        every { digisosSakEldst.kommunenummer } returns "1234"
-
-        val digisosSakNyeste: DigisosSak = mockk()
-        val orgSoknadNavNyeste: OriginalSoknadNAV = mockk()
-        every { digisosSakNyeste.originalSoknadNAV } returns orgSoknadNavNyeste
-        every { orgSoknadNavNyeste.timestampSendt } returns 3L
-        every { digisosSakNyeste.kommunenummer } returns "0301"
-
-        val digisosSakIMidten: DigisosSak = mockk()
-        val orgSoknadNavC: OriginalSoknadNAV = mockk()
-        every { digisosSakIMidten.originalSoknadNAV } returns orgSoknadNavC
-        every { orgSoknadNavC.timestampSendt } returns 2L
-        every { digisosSakIMidten.kommunenummer } returns "1234"
-
-        every { fiksClient.hentAlleDigisosSaker(any()) } returns listOf(digisosSakEldst, digisosSakNyeste, digisosSakIMidten)
-        every { clientProperties.meldingerKommunenummer } returns "0301"
-
-        runBlocking {
-            val response = controller.skalViseMeldingerLenke("token")
-            val resultat = response.body
-            assertThat(resultat).isEqualTo(true)
         }
     }
 

05c202d9088af096fd4502236e2f2ca35bd0d781
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-07-07 10:54:36 +0200

    trekker ut felles metode til DigisosSakUtils

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/service/innsyn/SoknadMedInnsynService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/service/innsyn/SoknadMedInnsynService.kt
@@ -1,11 +1,9 @@
 package no.nav.sosialhjelp.innsyn.service.innsyn
 
-import no.nav.sosialhjelp.api.fiks.DigisosSak
 import no.nav.sosialhjelp.innsyn.client.fiks.FiksClient
+import no.nav.sosialhjelp.innsyn.digisossak.isNewerThanMonths
 import no.nav.sosialhjelp.innsyn.kommuneinfo.KommuneService
 import org.springframework.stereotype.Component
-import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 @Component
 class SoknadMedInnsynService(
@@ -14,15 +12,8 @@ class SoknadMedInnsynService(
 ) {
 
     fun harSoknaderMedInnsyn(token: String): Boolean {
-        val saker = fiksClient.hentAlleDigisosSaker(token)
-
-        return saker
-            .filter { isDigisosSakNewerThanMonths(it, 15) }
+        return fiksClient.hentAlleDigisosSaker(token)
+            .filter { it.isNewerThanMonths(15) }
             .any { !kommuneService.erInnsynDeaktivertForKommune(it.fiksDigisosId, token) }
-    }
-
-    private fun isDigisosSakNewerThanMonths(digisosSak: DigisosSak, months: Int): Boolean {
-        val testDato = LocalDateTime.now().minusMonths(months.toLong()).toInstant(ZoneOffset.UTC).toEpochMilli()
-        return digisosSak.sistEndret >= testDato
     }
 }

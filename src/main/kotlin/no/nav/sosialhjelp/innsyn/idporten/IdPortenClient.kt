79038219573b6cde15c5dda9bcb4b77099fa9aeb
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-08-17 14:44:02 +0200

    wip idporten (2)

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenClient.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenClient.kt
@@ -5,6 +5,9 @@ import com.nimbusds.oauth2.sdk.ResponseType
 import com.nimbusds.oauth2.sdk.Scope
 import com.nimbusds.oauth2.sdk.id.ClientID
 import com.nimbusds.oauth2.sdk.id.State
+import com.nimbusds.oauth2.sdk.pkce.CodeChallengeMethod
+import com.nimbusds.oauth2.sdk.pkce.CodeVerifier
+import com.nimbusds.openid.connect.sdk.Nonce
 import no.nav.sosialhjelp.innsyn.app.tokendings.downloadWellKnown
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -20,12 +23,23 @@ class IdPortenClient(
     val wellknown = downloadWellKnown(idportenWellKnownUrl)
 
     fun getAuthorizeUrl(): URI {
+
+        // todo: lagre state til redis
+
+        val state = State()
+        val nonce = Nonce()
+
+        val codeVerifier = CodeVerifier()
+
         return AuthorizationRequest.Builder(
             ResponseType(ResponseType.Value.CODE),
             ClientID(idportenClientId)
         )
-            .scope(Scope("openid", "profile", "ks:fiks"))
-            .state(State())
+            .scope(Scope("openid", "profile")) // , "ks:fiks")) // KS må godkjenne vår clientId
+            .state(state)
+            .customParameter("nonce", nonce.value)
+            .customParameter("acr_values", "Level4")
+            .codeChallenge(codeVerifier, CodeChallengeMethod.S256)
             .redirectionURI(URI(idportenRedirectUri))
             .endpointURI(URI(wellknown.authorizationEndpoint))
             .build()
--- src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenController.kt
@@ -1,5 +1,11 @@
 package no.nav.sosialhjelp.innsyn.idporten
 
+import com.nimbusds.oauth2.sdk.AuthorizationResponse
+import com.nimbusds.oauth2.sdk.id.State
+import no.nav.sosialhjelp.innsyn.utils.logger
+import org.springframework.http.HttpRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,10 +16,41 @@ class IdPortenController {
     }
 
     @GetMapping("/oauth2/callback") // samme som 'redirectPath' i nais.yaml
-    fun handleCallback() {
+    fun handleCallback(request: HttpRequest): ResponseEntity<String> {
+        val response = AuthorizationResponse.parse(request.uri)
+
+        // TODO: hente state fra redis:
+        val state = State()
+
+        // Check the returned state parameter, must match the original
+        if (!state.equals(response.getState())) {
+            // Unexpected or tampered response, stop!!!
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
+        if (!response.indicatesSuccess()) {
+            // The request was denied or some error occurred
+            val errorResponse = response.toErrorResponse()
+            log.error("Error: ${errorResponse.errorObject}")
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
+        val successResponse = response.toSuccessResponse()
+
+        // Retrieve the authorisation code, to be used later to exchange the code for
+        // an access token at the token endpoint of the server
+        val code = successResponse.getAuthorizationCode()
+
+        // TODO: lagre code til session/redis
+
+        return ResponseEntity.ok().build()
     }
 
     @GetMapping("/oauth2/logout") // samme som 'frontchannelLogoutPath' i nais.yaml
     fun handleLogout() {
+    }
+
+    companion object {
+        private val log by logger()
     }
 }

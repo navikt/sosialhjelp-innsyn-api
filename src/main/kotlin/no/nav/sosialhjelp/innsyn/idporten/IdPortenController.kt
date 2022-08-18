a5bc783a99980b8d4e836d12e9bc2790309e0107
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-08-18 15:48:44 +0200

    Legger til IdPortenConfig og IdPortenProperties. Samt logging for debugging

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenController.kt
@@ -1,32 +1,44 @@
 package no.nav.sosialhjelp.innsyn.idporten
 
+import com.nimbusds.jose.jwk.KeyUse
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator
 import com.nimbusds.oauth2.sdk.AuthorizationResponse
 import com.nimbusds.oauth2.sdk.id.State
+import no.nav.security.token.support.core.api.Unprotected
+import no.nav.sosialhjelp.innsyn.app.MiljoUtils
+import no.nav.sosialhjelp.innsyn.app.tokendings.createSignedAssertion
 import no.nav.sosialhjelp.innsyn.utils.logger
-import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+import java.util.UUID
+import javax.servlet.http.HttpServletRequest
 
 @RestController
-class IdPortenController {
+class IdPortenController(
+    private val idPortenProperties: IdPortenProperties,
+) {
 
     fun getLoginUrl() {
     }
 
+    @Unprotected
     @GetMapping("/oauth2/callback") // samme som 'redirectPath' i nais.yaml
-    fun handleCallback(request: HttpRequest): ResponseEntity<String> {
-        val response = AuthorizationResponse.parse(request.uri)
+    fun handleCallback(request: HttpServletRequest): ResponseEntity<String> {
+        val redirectUri = request.requestURL.append('?').append(request.queryString).toString()
+        val response = AuthorizationResponse.parse(URI(redirectUri))
 
         // TODO: hente state fra redis:
         val state = State()
 
         // Check the returned state parameter, must match the original
-        if (!state.equals(response.getState())) {
+        /*if (!state.equals(response.getState())) {
             // Unexpected or tampered response, stop!!!
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
-        }
+        }*/
 
         if (!response.indicatesSuccess()) {
             // The request was denied or some error occurred
@@ -43,7 +55,24 @@ class IdPortenController {
 
         // TODO: lagre code til session/redis
 
+        // for å teste /token-endepunkt lokalt
+        log.info("code: ${code.value}")
+        log.info("client_assertion: $client_assertion")
+
         return ResponseEntity.ok().build()
+    }
+
+    private val client_assertion get() = createSignedAssertion(
+        clientId = idPortenProperties.clientId,
+        audience = idPortenProperties.wellKnown.issuer,
+        rsaKey = privateRsaKey
+    )
+
+    private val privateRsaKey: RSAKey = if (idPortenProperties.clientJwk == "generateRSA") {
+        if (MiljoUtils.isRunningInProd()) throw RuntimeException("Generation of RSA keys is not allowed in prod.")
+        RSAKeyGenerator(2048).keyUse(KeyUse.SIGNATURE).keyID(UUID.randomUUID().toString()).generate()
+    } else {
+        RSAKey.parse(idPortenProperties.clientJwk)
     }
 
     @GetMapping("/oauth2/logout") // samme som 'frontchannelLogoutPath' i nais.yaml

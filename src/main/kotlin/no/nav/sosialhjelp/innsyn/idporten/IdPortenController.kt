96ed1542bad80a7d04a193e9010552b3f1fb43cb
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2022-08-19 13:24:20 +0200

    Begynner på token request.

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenController.kt
@@ -8,37 +8,39 @@ import com.nimbusds.oauth2.sdk.id.State
 import no.nav.security.token.support.core.api.Unprotected
 import no.nav.sosialhjelp.innsyn.app.MiljoUtils
 import no.nav.sosialhjelp.innsyn.app.tokendings.createSignedAssertion
+import no.nav.sosialhjelp.innsyn.redis.RedisService
 import no.nav.sosialhjelp.innsyn.utils.logger
+import no.nav.sosialhjelp.innsyn.utils.objectMapper
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.RequestContextHolder
 import java.net.URI
 import java.util.UUID
 import javax.servlet.http.HttpServletRequest
 
 @RestController
 class IdPortenController(
+    private val idPortenClient: IdPortenClient,
     private val idPortenProperties: IdPortenProperties,
+    private val redisService: RedisService
 ) {
-
-    fun getLoginUrl() {
-    }
-
     @Unprotected
     @GetMapping("/oauth2/callback") // samme som 'redirectPath' i nais.yaml
     fun handleCallback(request: HttpServletRequest): ResponseEntity<String> {
         val redirectUri = request.requestURL.append('?').append(request.queryString).toString()
         val response = AuthorizationResponse.parse(URI(redirectUri))
 
-        // TODO: hente state fra redis:
-        val state = State()
+        val sessionId = RequestContextHolder.currentRequestAttributes().sessionId
+        val state = redisService.get("IDPORTEN_STATE_$sessionId", State::class.java)
+            ?: throw RuntimeException("No state found on sessionId")
 
         // Check the returned state parameter, must match the original
-        /*if (!state.equals(response.getState())) {
+        if (state != response.state) {
             // Unexpected or tampered response, stop!!!
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
-        }*/
+        }
 
         if (!response.indicatesSuccess()) {
             // The request was denied or some error occurred
@@ -53,16 +55,18 @@ class IdPortenController(
         // an access token at the token endpoint of the server
         val code = successResponse.getAuthorizationCode()
 
-        // TODO: lagre code til session/redis
+        redisService.put("IDPORTEN_CODE_$sessionId", objectMapper.writeValueAsBytes(code))
 
         // for å teste /token-endepunkt lokalt
         log.info("code: ${code.value}")
-        log.info("client_assertion: $client_assertion")
+        log.info("client_assertion: $clientAssertion")
+
+        idPortenClient.getToken(code, clientAssertion)
 
         return ResponseEntity.ok().build()
     }
 
-    private val client_assertion get() = createSignedAssertion(
+    private val clientAssertion get() = createSignedAssertion(
         clientId = idPortenProperties.clientId,
         audience = idPortenProperties.wellKnown.issuer,
         rsaKey = privateRsaKey

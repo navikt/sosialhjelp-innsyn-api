5b7d286122a7d7895b90083b71e5eabda6a7125e
Hilde Nøkland <93282568+hildenok@users.noreply.github.com>
2022-08-25 08:53:11 +0200

    Lage itest for saksoversikt controller (#598)
    * Lagt til integrasjonstest for SaksoversiktController.hentAlleSaker. Har også lagt til en verify i eksisterende SaksStatusITest.kt
    
    * Lagt til integrasjonstest for SaksoversiktController.hentSaksDetaljer. Samt noe refaktorering.
    
    * Lagt til integrasjonstest for SaksoversiktController.hentSaksDetaljer. Samt noe refaktorering.

---
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/testutils/IntegrasjonstestStubber.kt
@@ -0,0 +1,22 @@
+package no.nav.sosialhjelp.innsyn.testutils
+
+import no.nav.sosialhjelp.api.fiks.KommuneInfo
+
+class IntegrasjonstestStubber {
+
+    companion object {
+        fun lagKommuneInfoStub(): KommuneInfo {
+            val kommuneInfo = KommuneInfo(
+                "1001",
+                true,
+                true,
+                false,
+                false,
+                null,
+                true,
+                "Tester"
+            )
+            return kommuneInfo
+        }
+    }
+}

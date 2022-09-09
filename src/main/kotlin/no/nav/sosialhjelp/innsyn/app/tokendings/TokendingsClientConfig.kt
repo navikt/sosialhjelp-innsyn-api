b6800c01d6ee2cf35277151e044ffdfdc00c6150
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-09-09 15:27:00 +0200

    rydder litt i bruk av test spring profile

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/tokendings/TokendingsClientConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/tokendings/TokendingsClientConfig.kt
@@ -4,7 +4,6 @@ import no.nav.sosialhjelp.innsyn.app.ClientProperties
 import no.nav.sosialhjelp.kotlin.utils.logger
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.web.reactive.function.client.WebClient
 
 class TokendingsWebClient(
@@ -17,23 +16,12 @@ class TokendingsClientConfig(
     private val clientProperties: ClientProperties
 ) {
     @Bean
-    @Profile("!test")
     fun tokendingsWebClient(webClientBuilder: WebClient.Builder): TokendingsWebClient {
         val wellKnown = downloadWellKnown(clientProperties.tokendingsUrl)
         log.info("TokendingsClient: Lastet ned well known fra: ${clientProperties.tokendingsUrl} bruker token endpoint: ${wellKnown.tokenEndpoint}")
         return TokendingsWebClient(
             buildWebClient(webClientBuilder, wellKnown.tokenEndpoint, applicationFormUrlencodedHeaders()),
             wellKnown
-        )
-    }
-
-    @Bean
-    @Profile("test")
-    fun tokendingsWebClientTest(webClientBuilder: WebClient.Builder): TokendingsWebClient {
-        log.info("TokendingsClient: Setter opp test client som bruker token endpoint: ${clientProperties.tokendingsUrl}")
-        return TokendingsWebClient(
-            buildWebClient(webClientBuilder, clientProperties.tokendingsUrl),
-            WellKnown("iss-localhost", "authorizationEndpoint", "tokenEndpoint", clientProperties.tokendingsUrl)
         )
     }
 

76c699a41c5f341cd85860d1a81be925b20b49ad
Ida Onshus <25682301+idaons@users.noreply.github.com>
2022-09-23 08:28:06 +0200

    fix ktlintcheck

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/ToggleNames.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/ToggleNames.kt
@@ -7,5 +7,4 @@ const val DOKUMENTASJONKRAV = "sosialhjelp.innsyn.dokumentasjonkrav"
 const val UTVIDE_VEDLEGG_JSON = "sosialhjelp.innsyn.utvide-vedlegg-json"
 const val LOGGE_MISMATCH_FILNAVN = "sosialhjelp.innsyn.logge-mismatch-filnavn"
 
-
 const val FAGSYSTEM_MED_INNSYN_I_PAPIRSOKNADER = "sosialhjelp.innsyn.fagsystem-med-papirsoknader-loggmelding"

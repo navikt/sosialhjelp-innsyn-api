20807ba84f4210d3f900d1ab3bad5db4566aaab3
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-01-26 16:35:57 +0100

    Rydder i ubrukte feature-toggles

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/event/DokumentasjonkravTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/event/DokumentasjonkravTest.kt
@@ -8,7 +8,6 @@ import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonDigisosSoker
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad
 import no.nav.sosialhjelp.api.fiks.DigisosSak
 import no.nav.sosialhjelp.innsyn.app.ClientProperties
-import no.nav.sosialhjelp.innsyn.app.featuretoggle.DOKUMENTASJONKRAV
 import no.nav.sosialhjelp.innsyn.app.featuretoggle.DOKUMENTASJONKRAV_ENABLED
 import no.nav.sosialhjelp.innsyn.domain.Oppgavestatus
 import no.nav.sosialhjelp.innsyn.domain.SoknadsStatus
@@ -56,7 +55,6 @@ internal class DokumentasjonkravTest {
         every { mockDigisosSak.originalSoknadNAV?.soknadDokument?.dokumentlagerDokumentId } returns null
 
         every { unleashClient.isEnabled(DOKUMENTASJONKRAV_ENABLED, false) } returns true
-        every { unleashClient.isEnabled(DOKUMENTASJONKRAV, false) } returns true
 
         resetHendelser()
     }
--- src/test/kotlin/no/nav/sosialhjelp/innsyn/event/VilkarTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/event/VilkarTest.kt
@@ -8,7 +8,6 @@ import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonDigisosSoker
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad
 import no.nav.sosialhjelp.api.fiks.DigisosSak
 import no.nav.sosialhjelp.innsyn.app.ClientProperties
-import no.nav.sosialhjelp.innsyn.app.featuretoggle.VILKAR
 import no.nav.sosialhjelp.innsyn.domain.Oppgavestatus
 import no.nav.sosialhjelp.innsyn.domain.SoknadsStatus
 import no.nav.sosialhjelp.innsyn.navenhet.NavEnhet
@@ -51,7 +50,6 @@ internal class VilkarTest {
         every { innsynService.hentOriginalSoknad(any(), any()) } returns mockJsonSoknad
         every { norgClient.hentNavEnhet(enhetsnr) } returns mockNavEnhet
 
-        every { unleashClient.isEnabled(VILKAR, false) } returns true
         resetHendelser()
     }
 

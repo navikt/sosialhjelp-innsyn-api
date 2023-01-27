e5c1f31127bc7075338595be7df4c56408489bbe
martintveter <41987225+martintveter@users.noreply.github.com>
2023-01-27 09:18:10 +0100

    Rydder i ubrukte feature-toggles (#660)

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/ToggleNames.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/ToggleNames.kt
@@ -1,10 +1,7 @@
 package no.nav.sosialhjelp.innsyn.app.featuretoggle
 
 const val VILKAR_ENABLED = "sosialhjelp.innsyn.vilkar-enabled"
-const val VILKAR = "sosialhjelp.innsyn.vilkar"
 const val DOKUMENTASJONKRAV_ENABLED = "sosialhjelp.innsyn.dokumentasjonkrav-enabled"
-const val DOKUMENTASJONKRAV = "sosialhjelp.innsyn.dokumentasjonkrav"
-const val UTVIDE_VEDLEGG_JSON = "sosialhjelp.innsyn.utvide-vedlegg-json"
 const val LOGGE_MISMATCH_FILNAVN = "sosialhjelp.innsyn.logge-mismatch-filnavn"
 
 const val FAGSYSTEM_MED_INNSYN_I_PAPIRSOKNADER = "sosialhjelp.innsyn.fagsystem-med-papirsoknader-loggmelding"

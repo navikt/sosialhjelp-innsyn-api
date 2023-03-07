291770a08a792de6b4a4db84c29c04da1de95295
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2023-03-07 08:41:21 +0100

    asd

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/tilgang/Tilgangskontroll.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/tilgang/Tilgangskontroll.kt
@@ -28,9 +28,7 @@ class TilgangskontrollService(
 ) : Tilgangskontroll {
 
     override fun sjekkTilgang(token: String) {
-        if (System.getenv("SPRING_PROFILES_ACTIVE")?.split(",")?.contains("dev") == false) {
-            if (SubjectHandlerUtils.getClientId() != loginApiClientId) throw TilgangskontrollException("Feil clientId")
-        }
+        if (SubjectHandlerUtils.getClientId() != loginApiClientId) throw TilgangskontrollException("Feil clientId")
         sjekkTilgang(SubjectHandlerUtils.getUserIdFromToken(), token)
     }
 

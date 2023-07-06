fd3e9da36cd9369a30aec15dde50dc5354abba72
linemos <line.moseng@nav.no>
2023-07-06 13:16:14 +0200

    Bump jvm og spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,8 +55,8 @@ object Versions {
 }
 
 plugins {
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
     id("org.springframework.boot") version "3.1.1"
     id("com.github.ben-manes.versions") version "0.47.0" // ./gradlew dependencyUpdates
     id("org.jlleitschuh.gradle.ktlint") version "11.5.0"

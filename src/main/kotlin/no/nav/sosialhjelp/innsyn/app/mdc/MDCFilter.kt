d7bf5720c6e6dd3b28961ca2271c5e280043673e
hildenok <hilde.nokland@nav.no>
2023-08-29 09:58:04 +0200

    Fjernet setting av login_id da denne er blank.

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/mdc/MDCFilter.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/mdc/MDCFilter.kt
@@ -5,11 +5,9 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import no.nav.sosialhjelp.innsyn.app.mdc.MDCUtils.CALL_ID
 import no.nav.sosialhjelp.innsyn.app.mdc.MDCUtils.DIGISOS_ID
-import no.nav.sosialhjelp.innsyn.app.mdc.MDCUtils.LOGIN_ID
 import no.nav.sosialhjelp.innsyn.app.mdc.MDCUtils.clearMDC
 import no.nav.sosialhjelp.innsyn.app.mdc.MDCUtils.generateCallId
 import no.nav.sosialhjelp.innsyn.app.mdc.MDCUtils.put
-import no.nav.sosialhjelp.innsyn.idporten.IdPortenController
 import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.HEADER_CALL_ID
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
@@ -25,7 +23,6 @@ class MDCFilter : OncePerRequestFilter() {
     ) {
         addCallId(request)
         addDigisosId(request)
-        addLoginId(request)
         put(MDCUtils.PATH, request.requestURI)
         request.getHeader(HttpHeaders.USER_AGENT)?.let { put(MDCUtils.USER_AGENT, it) }
         request.getHeader(HttpHeaders.REFERER)?.let { put(MDCUtils.REFERER, it) }
@@ -35,10 +32,6 @@ class MDCFilter : OncePerRequestFilter() {
         } finally {
             clearMDC()
         }
-    }
-
-    private fun addLoginId(request: HttpServletRequest) {
-        request.cookies?.firstOrNull { it.name == IdPortenController.LOGIN_ID_COOKIE }?.value?.let { put(LOGIN_ID, it) }
     }
 
     private fun addCallId(request: HttpServletRequest) {

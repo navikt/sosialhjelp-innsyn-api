1607463712d2548a123a50f5c66844ffa4efd940
hildenok <hilde.nokland@nav.no>
2023-09-01 11:17:34 +0200

    Lagt til logging ved henting av token.

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenSessionHandler.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/idporten/IdPortenSessionHandler.kt
@@ -10,6 +10,7 @@ import no.nav.sosialhjelp.innsyn.idporten.CachePrefixes.SESSION_ID_CACHE_PREFIX
 import no.nav.sosialhjelp.innsyn.idporten.CachePrefixes.STATE_CACHE_PREFIX
 import no.nav.sosialhjelp.innsyn.idporten.IdPortenController.Companion.LOGIN_ID_COOKIE
 import no.nav.sosialhjelp.innsyn.redis.RedisService
+import no.nav.sosialhjelp.innsyn.utils.logger
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
@@ -43,14 +44,20 @@ class IdPortenSessionHandler(
     fun getToken(request: HttpServletRequest): String? {
         val loginId = request.cookies?.firstOrNull { it.name == LOGIN_ID_COOKIE }?.value
         if (loginId != null) {
+            log.info("Henter token for loginId er: $loginId")
             val accessToken = redisService.get("$ACCESS_TOKEN_CACHE_PREFIX$loginId", String::class.java)
             return accessToken ?: getAccessTokenFromRefreshToken(loginId)
         }
+        log.info("Ingen loginid i request. Det returneres derfor ingen token")
         return null
     }
 
     private fun getAccessTokenFromRefreshToken(loginId: String): String? {
         val refreshToken = redisService.get("$REFRESH_TOKEN_CACHE_PREFIX$loginId", String::class.java)
         return refreshToken?.let { idPortenClient.getAccessTokenFromRefreshToken(it, loginId) }
+    }
+
+    companion object {
+        private val log by logger()
     }
 }

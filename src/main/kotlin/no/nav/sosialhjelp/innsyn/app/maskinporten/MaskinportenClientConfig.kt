40384bffe2ce9fadcf75d9e3046dfc5706aefca6
Martin Almvik <martin.almvik@nav.no>
2023-09-08 14:02:01 +0200

    Fjerner logging av iss og endpoint url

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/maskinporten/MaskinportenClientConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/maskinporten/MaskinportenClientConfig.kt
@@ -46,10 +46,10 @@ class MaskinportenClientConfig(
 
     private val wellknown: WellKnown
         get() = maskinportenWebClient.get()
-            .uri(wellKnownUrl).also { log.info("Henter well known fra: $wellKnownUrl") }
+            .uri(wellKnownUrl)
             .retrieve()
             .bodyToMono<WellKnown>()
-            .doOnSuccess { log.info("Hentet WellKnown for Maskinporten. Fikk issuer: ${it.issuer} og endpoint: ${it.token_endpoint}") }
+            .doOnSuccess { log.info("Hentet WellKnown for Maskinporten.") }
             .doOnError { log.warn("Feil ved henting av WellKnown for Maskinporten", it) }
             .block() ?: throw RuntimeException("Feil ved henting av WellKnown for Maskinporten")
 

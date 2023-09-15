f3e8dfe93408d616189f5627e020e4e1074bb2c3
Martin Almvik <martin.almvik@nav.no>
2023-09-15 13:17:11 +0200

    Støtte trailing slash

---
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/config/WebConfiguration.kt
@@ -0,0 +1,13 @@
+package no.nav.sosialhjelp.innsyn.app.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+    override fun configurePathMatch(configurer: PathMatchConfigurer) {
+        configurer.setUseTrailingSlashMatch(true)
+    }
+}

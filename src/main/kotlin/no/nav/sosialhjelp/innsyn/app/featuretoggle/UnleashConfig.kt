b07ff7c14ca2aa1c151b57f46fed592d99d4224d
Line Moseng <line.moseng@nav.no>
2023-09-27 13:27:59 +0200

    Legger på profile

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/UnleashConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/UnleashConfig.kt
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
+@Profile("!local")
 @Configuration
 class UnleashConfig(
     private val clientProperties: ClientProperties

869fc944be84293957d53e532fa1138c31121f55
Martin Almvik <martin.almvik@nav.no>
2023-09-27 14:57:05 +0200

    Legg til endepunkter for å sende og motta klage

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/event/VedtakFattet.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/event/VedtakFattet.kt
@@ -1,8 +1,10 @@
 package no.nav.sosialhjelp.innsyn.event
 
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.filreferanse.JsonDokumentlagerFilreferanse
+import no.nav.sbl.soknadsosialhjelp.digisos.soker.filreferanse.JsonSvarUtFilreferanse
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.hendelse.JsonVedtakFattet
 import no.nav.sosialhjelp.innsyn.app.ClientProperties
-import no.nav.sosialhjelp.innsyn.digisossak.saksstatus.SaksStatusService.Companion.DEFAULT_SAK_TITTEL
+import no.nav.sosialhjelp.innsyn.digisossak.saksstatus.DEFAULT_SAK_TITTEL
 import no.nav.sosialhjelp.innsyn.domain.Hendelse
 import no.nav.sosialhjelp.innsyn.domain.HendelseTekstType
 import no.nav.sosialhjelp.innsyn.domain.InternalDigisosSoker
@@ -23,7 +25,14 @@ fun InternalDigisosSoker.apply(hendelse: JsonVedtakFattet, clientProperties: Cli
     val utfall = utfallString?.let { UtfallVedtak.valueOf(it) }
     val vedtaksfilUrl = hentUrlFraFilreferanse(clientProperties, hendelse.vedtaksfil.referanse)
 
-    val vedtakFattet = Vedtak(utfall, vedtaksfilUrl, hendelse.hendelsestidspunkt.toLocalDateTime().toLocalDate())
+    val id = when (val referanse = hendelse.vedtaksfil.referanse) {
+        is JsonDokumentlagerFilreferanse -> referanse.id
+        is JsonSvarUtFilreferanse -> referanse.id
+        else -> error("Ikke støttet referansetype ${referanse.type}")
+    }
+    val vedtakFattet = Vedtak(
+        id, utfall, vedtaksfilUrl, hendelse.hendelsestidspunkt.toLocalDateTime().toLocalDate()
+    )
 
     var sakForReferanse = saker.firstOrNull { it.referanse == hendelse.saksreferanse || it.referanse == "default" }
 
@@ -42,7 +51,14 @@ fun InternalDigisosSoker.apply(hendelse: JsonVedtakFattet, clientProperties: Cli
 
     log.info("Hendelse: Tidspunkt: ${hendelse.hendelsestidspunkt} Vedtak fattet. <skjult tittel> er ferdigbehandlet")
     if (sakForReferanse.tittel != null) {
-        historikk.add(Hendelse(HendelseTekstType.SAK_FERDIGBEHANDLET_MED_TITTEL, hendelse.hendelsestidspunkt.toLocalDateTime(), UrlResponse(HendelseTekstType.VIS_BREVET_LENKETEKST, vedtaksfilUrl), tekstArgument = sakForReferanse.tittel))
+        historikk.add(
+            Hendelse(
+                HendelseTekstType.SAK_FERDIGBEHANDLET_MED_TITTEL,
+                hendelse.hendelsestidspunkt.toLocalDateTime(),
+                UrlResponse(HendelseTekstType.VIS_BREVET_LENKETEKST, vedtaksfilUrl),
+                tekstArgument = sakForReferanse.tittel
+            )
+        )
     } else {
         historikk.add(Hendelse(HendelseTekstType.SAK_FERDIGBEHANDLET_UTEN_TITTEL, hendelse.hendelsestidspunkt.toLocalDateTime(), UrlResponse(HendelseTekstType.VIS_BREVET_LENKETEKST, vedtaksfilUrl)))
     }

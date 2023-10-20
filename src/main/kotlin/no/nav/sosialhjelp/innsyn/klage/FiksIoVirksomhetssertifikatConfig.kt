4a4da28ceb14879c9177e27b7c342c23b62f73d7
Martin Almvik <martin.almvik@nav.no>
2023-10-20 10:37:34 +0200

    Endrer til vault sertifikat

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/klage/FiksIoVirksomhetssertifikatConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/klage/FiksIoVirksomhetssertifikatConfig.kt
@@ -1,87 +1,42 @@
 package no.nav.sosialhjelp.innsyn.klage
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse
-import com.google.cloud.secretmanager.v1.SecretManagerServiceClient
-import com.google.cloud.secretmanager.v1.SecretVersionName
 import no.ks.fiks.io.client.konfigurasjon.VirksomhetssertifikatKonfigurasjon
-import no.nav.sosialhjelp.innsyn.utils.logger
 import no.nav.sosialhjelp.innsyn.utils.objectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
 import java.security.KeyStore
-import java.security.KeyStore.PasswordProtection
-import java.util.zip.CRC32C
-import java.util.zip.Checksum
+import java.util.Base64
 
 data class DigisosKeyStoreCredentials(
     val alias: String,
     val password: String,
-    val type: String
+    val type: String,
 )
 
 @Configuration
 @Profile("dev-fss|prod-fss")
 class FiksIoVirksomhetssertifikatConfig(
-    @Value("\${fiks-io.virksomhetssertifikat.passwordProjectId}")
-    private val passwordProjectId: String,
-    @Value("\${fiks-io.virksomhetssertifikat.passwordSecretId}")
-    private val passwordSecretId: String,
-    @Value("\${fiks-io.virksomhetssertifikat.passwordSecretVersion}")
-    private val passwordSecretVersion: String,
-    @Value("\${fiks-io.virksomhetssertifikat.projectId}")
-    private val projectId: String,
-    @Value("\${fiks-io.virksomhetssertifikat.secretId}")
-    private val secretId: String,
-    @Value("\${fiks-io.virksomhetssertifikat.secretVersion}")
-    private val versionId: String,
+    @Value("\$virksomhetssertifikatPath")
+    private val virksomhetssertifikatPath: String,
 ) {
-
-    private val log by logger()
 
     @Bean
     fun virksomhetssertifikatConfig(): VirksomhetssertifikatKonfigurasjon {
-        val (certificateResponse, passwordResponse) = SecretManagerServiceClient.create().use { client ->
-            val passwordResponse = client.accessSecretVersionResponse(passwordProjectId, passwordSecretId, passwordSecretVersion)
-            val certificateResponse = client.accessSecretVersionResponse(projectId, secretId, versionId)
-
-            Pair(certificateResponse.payload, passwordResponse.payload)
+        val sertifikat = Files.readString(Path.of("$virksomhetssertifikatPath/key.p12.b64")).let {
+            Base64.getDecoder().decode(it)
         }
-        val password = objectMapper.readValue<DigisosKeyStoreCredentials>(passwordResponse.data.toByteArray())
-        val passwordAsCharArray = password.password.toCharArray()
-        val passwordProtection = PasswordProtection(passwordAsCharArray)
-
-        // Sertifikatet har jceks-format
-        val jceksKeyStore = KeyStore.getInstance("jceks")
-        jceksKeyStore.load(certificateResponse.data.newInput(), passwordAsCharArray)
-
-        // Fiks IO signerer pakkene med sertifikatet, men støtter bare JKS.
-        val jksKeyStore = KeyStore.getInstance("JKS")
-        jksKeyStore.load(null, passwordAsCharArray)
-
-        // Vi må kopiere innholdet i jceks keyStoren til JKS keyStoren
-        for (alias in jceksKeyStore.aliases()) {
-            val entry = jceksKeyStore.getEntry(alias, passwordProtection)
-            jksKeyStore.setEntry(alias, entry, passwordProtection)
+        val password = Files.readAllBytes(Path.of("$virksomhetssertifikatPath/credentials.json")).let {
+            objectMapper.readValue<DigisosKeyStoreCredentials>(it)
         }
-        return VirksomhetssertifikatKonfigurasjon.builder().keyStore(jksKeyStore).keyStorePassword(password.password).keyAlias(password.alias).keyPassword(password.password).build()
-    }
 
-    private fun SecretManagerServiceClient.accessSecretVersionResponse(projectId: String, secretId: String, secretVersion: String): AccessSecretVersionResponse {
-        val secretVersionName = SecretVersionName.of(projectId, secretId, secretVersion)
+        val keyStore = KeyStore.getInstance("pkcs12")
+        keyStore.load(sertifikat.inputStream(), password.password.toCharArray())
 
-        val response = accessSecretVersion(secretVersionName)
-
-        val data = response.payload.data.toByteArray()
-        val checksum: Checksum = CRC32C()
-        checksum.update(data, 0, data.size)
-        if (response.payload.dataCrc32C != checksum.value) {
-            log.error("Data corruption detected.")
-            throw IOException("Data corruption detected.")
-        }
-        return response
+        return VirksomhetssertifikatKonfigurasjon.builder().keyStore(keyStore).keyStorePassword(password.password).keyAlias(password.alias).keyPassword(password.password).build()
     }
 }

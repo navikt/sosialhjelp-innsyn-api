25a7ee9f23881eb3b855dada75399f62903773f4
Martin Almvik <martin.almvik@nav.no>
2024-02-21 14:31:22 +0100

    Går tilbake til futures

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/vedlegg/KrypteringService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/vedlegg/KrypteringService.kt
@@ -1,27 +1,29 @@
 package no.nav.sosialhjelp.innsyn.vedlegg
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import no.ks.kryptering.CMSKrypteringImpl
 import no.nav.sosialhjelp.innsyn.utils.logger
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
+import java.io.IOException
 import java.io.InputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
 import java.security.Security
 import java.security.cert.X509Certificate
+import java.util.concurrent.ExecutorCompletionService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 interface KrypteringService {
-    suspend fun krypter(
-        fileInputStream: InputStream,
-        certificate: X509Certificate,
-    ): Pair<InputStream, Job>
+    fun krypter(
+        dokumentStream: InputStream,
+        krypteringFutureList: MutableList<Future<Void>>,
+        fiksX509Certificate: X509Certificate,
+    ): InputStream
 }
 
 private val kryptering = CMSKrypteringImpl()
+private val executor = ExecutorCompletionService<Void>(Executors.newCachedThreadPool())
 
 @Profile("!mock-alt")
 @Component
@@ -29,40 +31,54 @@ class KrypteringServiceImpl : KrypteringService {
     private val log by logger()
 
     // Timeout etter 30 sekunder
-    override suspend fun krypter(
-        fileInputStream: InputStream,
-        certificate: X509Certificate,
-    ): Pair<InputStream, Job> {
-        val pis = PipedInputStream()
-        val job =
-            fileInputStream.use { inputStream ->
-                val pos = PipedOutputStream(pis)
-
-                log.info("Starter kryptering")
-                withContext(Dispatchers.IO) {
-                    launch {
+    override fun krypter(
+        dokumentStream: InputStream,
+        krypteringFutureList: MutableList<Future<Void>>,
+        fiksX509Certificate: X509Certificate,
+    ): InputStream {
+        val pipedInputStream = PipedInputStream()
+        try {
+            val pipedOutputStream = PipedOutputStream(pipedInputStream)
+            val krypteringFuture =
+                executor.submit {
+                    try {
+                        kryptering.krypterData(
+                            pipedOutputStream,
+                            dokumentStream,
+                            fiksX509Certificate,
+                            Security.getProvider("BC"),
+                        )
+                    } catch (e: Exception) {
+                        log.error("Encryption failed, setting exception on encrypted InputStream", e)
+                        throw IllegalStateException("An error occurred during encryption", e)
+                    } finally {
                         try {
-                            pos.use {
-                                kryptering.krypterData(it, inputStream, certificate, Security.getProvider("BC"))
-                            }
-                        } catch (e: Exception) {
-                            log.error("Det skjedde en feil ved kryptering, exception blir lagt til kryptert InputStream", e)
-                            throw IllegalStateException("An error occurred during encryption", e)
+                            log.debug("Closing encryption OutputStream")
+                            pipedOutputStream.close()
+                            log.debug("Encryption OutputStream closed")
+                        } catch (e: IOException) {
+                            log.error("Failed closing encryption OutputStream", e)
                         }
-                        log.info("Ferdig med kryptering")
                     }
+                    null
                 }
-            }
-
-        return pis to job
+            krypteringFutureList.add(krypteringFuture)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        } finally {
+            log.debug("Closing dokumentStream InputStream")
+            dokumentStream.close()
+        }
+        return pipedInputStream
     }
 }
 
 @Profile("mock-alt")
 @Component
 class KrypteringServiceMock : KrypteringService {
-    override suspend fun krypter(
-        fileInputStream: InputStream,
-        certificate: X509Certificate,
-    ): Pair<InputStream, Job> = fileInputStream to Job().also { it.complete() }
+    override fun krypter(
+        dokumentStream: InputStream,
+        krypteringFutureList: MutableList<Future<Void>>,
+        fiksX509Certificate: X509Certificate,
+    ): InputStream = dokumentStream
 }

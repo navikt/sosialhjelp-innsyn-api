d92886e1debf009ed8416b9f4593550ed796d3ec
Martin Almvik <martin.almvik@nav.no>
2024-11-21 09:33:13 +0100

    Preprod-miljø i dev-gcp

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/redis/RedisConfig.kt
@@ -2,17 +2,46 @@ package no.nav.sosialhjelp.innsyn.redis
 
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import java.time.Duration
+
+private const val TIMEOUT_SECONDS: Long = 1
 
 @Profile("!mock-redis")
 @Configuration
 @EnableConfigurationProperties(RedisProperties::class)
-class RedisConfig {
+class RedisConfig(
+    @Value("\${redis_host}") private val host: String,
+    @Value("\${redis_port}") private val port: Int,
+    @Value("\${redis_password}") private val password: String,
+    @Value("\${redis_username}") private val username: String,
+) {
     @Bean
+    @Profile("preprod")
+    fun redisClient(): RedisClient {
+        val redisURI =
+            RedisURI
+                .builder()
+                .withHost(host)
+                .withPort(port)
+                .withAuthentication(username, password.toCharArray())
+                .withTimeout(Duration.ofSeconds(TIMEOUT_SECONDS))
+                .withSsl(true)
+                .build()
+        println("connecting to redis with host: $host")
+        println("connecting to redis with port: $port")
+        println("connecting to redis with username: $username")
+        println("connecting to redis with url: $redisURI")
+        return RedisClient.create(redisURI)
+    }
+
+    @Bean
+    @Profile("!preprod")
     fun redisClient(properties: RedisProperties): RedisClient {
         val redisUri =
             RedisURI.Builder.redis(properties.host, properties.port)

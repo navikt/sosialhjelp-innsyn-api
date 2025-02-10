6a11a2a39a57f2788d94241b21b940bbe15708b4
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-02-11 00:13:54 +0100

    Egentlig er kanskje SessionMetadata et bedre navn.

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/session/SessionController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/session/SessionController.kt
@@ -3,10 +3,10 @@ package no.nav.sosialhjelp.innsyn.session
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.slf4j.MDCContext
 import kotlinx.coroutines.withContext
+import no.nav.sosialhjelp.innsyn.app.protectionAnnotation.ProtectionSelvbetjeningHigh
 import no.nav.sosialhjelp.innsyn.app.subjecthandler.SubjectHandlerUtils.getUserIdFromToken
 import no.nav.sosialhjelp.innsyn.digisossak.hendelser.RequestAttributesContext
 import no.nav.sosialhjelp.innsyn.pdl.PdlService
-import no.nav.sosialhjelp.innsyn.app.protectionAnnotation.ProtectionSelvbetjeningHigh
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -18,10 +18,10 @@ class SessionController(
     private val pdlService: PdlService,
 ) {
     @GetMapping("/session")
-    fun getSessionInfo(): SessionInfo = runBlocking {
+    fun getSessionMetadata(): SessionMetadata = runBlocking {
         withContext(MDCContext() + RequestAttributesContext()) {
             with(getUserIdFromToken()) {
-                SessionInfo(
+                SessionMetadata(
                     fornavn = pdlService.getFornavnByPid(this),
                     harAdressebeskyttelse = pdlService.getAdressebeskyttelseByPid(this)
                 )
@@ -29,8 +29,11 @@ class SessionController(
         }
     }
 
-    data class SessionInfo(
+    data class SessionMetadata(
+        /** Brukers fornavn fra PDL */
         val fornavn: String,
+        /** Tjenesten er ikke tilgjengelig for brukere med adressebeskyttelse.
+         *  Dersom denne er true, må brukeren få en feilside. */
         val harAdressebeskyttelse: Boolean,
     )
 }

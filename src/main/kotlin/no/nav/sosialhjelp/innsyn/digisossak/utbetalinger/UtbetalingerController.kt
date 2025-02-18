721ac0938c0e65b215dd5d3601f633d2b15b7e3b
Martin Almvik <martin.almvik@nav.no>
2025-02-18 17:04:25 +0100

    Prøver å få til 'native' async

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/utbetalinger/UtbetalingerController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/utbetalinger/UtbetalingerController.kt
@@ -1,15 +1,7 @@
 package no.nav.sosialhjelp.innsyn.digisossak.utbetalinger
 
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.slf4j.MDCContext
-import kotlinx.coroutines.withContext
-import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.sosialhjelp.api.fiks.exceptions.FiksClientException
-import no.nav.sosialhjelp.innsyn.digisossak.hendelser.RequestAttributesContext
 import no.nav.sosialhjelp.innsyn.tilgang.TilgangskontrollService
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.ACR_IDPORTEN_LOA_HIGH
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.ACR_LEVEL4
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.SELVBETJENING
 import no.nav.sosialhjelp.innsyn.utils.logger
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.HttpStatus
@@ -20,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@ProtectedWithClaims(issuer = SELVBETJENING, claimMap = [ACR_LEVEL4, ACR_IDPORTEN_LOA_HIGH], combineWithOr = true)
 @RestController
 @RequestMapping("/api/v1/innsyn")
 class UtbetalingerController(
@@ -28,68 +19,59 @@ class UtbetalingerController(
     private val tilgangskontroll: TilgangskontrollService,
 ) {
     @GetMapping("/utbetalinger")
-    fun hentUtbetalinger(
+    suspend fun hentUtbetalinger(
         @RequestHeader(value = AUTHORIZATION) token: String,
         @RequestParam(defaultValue = "3") month: Int,
-    ): ResponseEntity<List<UtbetalingerResponse>> =
-        runBlocking {
-            withContext(MDCContext() + RequestAttributesContext()) {
-                tilgangskontroll.sjekkTilgang(token)
+    ): ResponseEntity<List<UtbetalingerResponse>> {
+        tilgangskontroll.sjekkTilgang(token)
 
-                try {
-                    ResponseEntity.ok().body(utbetalingerService.hentUtbetalteUtbetalinger(token, month))
-                } catch (e: FiksClientException) {
-                    if (e.status == HttpStatus.FORBIDDEN.value()) {
-                        log.error("FiksClientException i UtbetalingerController status: ${e.status} message: ${e.message}", e)
-                        ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
-                    } else {
-                        throw e
-                    }
-                }
+        return try {
+            ResponseEntity.ok().body(utbetalingerService.hentUtbetalteUtbetalinger(token, month))
+        } catch (e: FiksClientException) {
+            if (e.status == HttpStatus.FORBIDDEN.value()) {
+                log.error("FiksClientException i UtbetalingerController status: ${e.status} message: ${e.message}", e)
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+            } else {
+                throw e
             }
         }
+    }
 
     @GetMapping("/nye")
-    fun hentNyeUtbetalinger(
+    suspend fun hentNyeUtbetalinger(
         @RequestHeader(value = AUTHORIZATION) token: String,
-    ): ResponseEntity<List<NyeOgTidligereUtbetalingerResponse>> =
-        runBlocking {
-            withContext(MDCContext() + RequestAttributesContext()) {
-                tilgangskontroll.sjekkTilgang(token)
+    ): ResponseEntity<List<NyeOgTidligereUtbetalingerResponse>> {
+        tilgangskontroll.sjekkTilgang(token)
 
-                try {
-                    ResponseEntity.ok().body(utbetalingerService.hentNyeUtbetalinger(token))
-                } catch (e: FiksClientException) {
-                    if (e.status == HttpStatus.FORBIDDEN.value()) {
-                        log.error("FiksClientException i UtbetalingerController status: ${e.status} message: ${e.message}", e)
-                        ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
-                    } else {
-                        throw e
-                    }
-                }
+        return try {
+            ResponseEntity.ok().body(utbetalingerService.hentNyeUtbetalinger(token))
+        } catch (e: FiksClientException) {
+            if (e.status == HttpStatus.FORBIDDEN.value()) {
+                log.error("FiksClientException i UtbetalingerController status: ${e.status} message: ${e.message}", e)
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+            } else {
+                throw e
             }
         }
+    }
 
     @GetMapping("/tidligere")
-    fun hentTidligereUtbetalinger(
+    suspend fun hentTidligereUtbetalinger(
         @RequestHeader(value = AUTHORIZATION) token: String,
-    ): ResponseEntity<List<NyeOgTidligereUtbetalingerResponse>> =
-        runBlocking {
-            withContext(MDCContext() + RequestAttributesContext()) {
-                tilgangskontroll.sjekkTilgang(token)
+    ): ResponseEntity<List<NyeOgTidligereUtbetalingerResponse>> {
+        tilgangskontroll.sjekkTilgang(token)
 
-                try {
-                    ResponseEntity.ok().body(utbetalingerService.hentTidligereUtbetalinger(token))
-                } catch (e: FiksClientException) {
-                    if (e.status == HttpStatus.FORBIDDEN.value()) {
-                        log.error("FiksClientException i UtbetalingerController status: ${e.status} message: ${e.message}", e)
-                        ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
-                    } else {
-                        throw e
-                    }
-                }
+        return try {
+            ResponseEntity.ok().body(utbetalingerService.hentTidligereUtbetalinger(token))
+        } catch (e: FiksClientException) {
+            if (e.status == HttpStatus.FORBIDDEN.value()) {
+                log.error("FiksClientException i UtbetalingerController status: ${e.status} message: ${e.message}", e)
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+            } else {
+                throw e
             }
         }
+    }
 
     companion object {
         private val log by logger()

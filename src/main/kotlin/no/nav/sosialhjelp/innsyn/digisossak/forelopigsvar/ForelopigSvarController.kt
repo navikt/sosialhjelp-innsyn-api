db4484b9def538f3d0ca149890c99d8dea06b869
Martin Almvik <martin.almvik@nav.no>
2025-02-18 17:04:25 +0100

    Prøver å få til 'native' async

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/forelopigsvar/ForelopigSvarController.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/forelopigsvar/ForelopigSvarController.kt
@@ -1,23 +1,13 @@
 package no.nav.sosialhjelp.innsyn.digisossak.forelopigsvar
 
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.slf4j.MDCContext
-import kotlinx.coroutines.withContext
-import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.sosialhjelp.innsyn.digisossak.hendelser.RequestAttributesContext
 import no.nav.sosialhjelp.innsyn.tilgang.TilgangskontrollService
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.ACR_IDPORTEN_LOA_HIGH
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.ACR_LEVEL4
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.SELVBETJENING
 import org.springframework.http.HttpHeaders
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@ProtectedWithClaims(issuer = SELVBETJENING, claimMap = [ACR_LEVEL4, ACR_IDPORTEN_LOA_HIGH], combineWithOr = true)
 @RestController
 @RequestMapping("/api/v1/innsyn")
 class ForelopigSvarController(
@@ -25,17 +15,12 @@ class ForelopigSvarController(
     private val tilgangskontroll: TilgangskontrollService,
 ) {
     @GetMapping("/{fiksDigisosId}/forelopigSvar")
-    fun hentForelopigSvarStatus(
+    suspend fun hentForelopigSvarStatus(
         @PathVariable fiksDigisosId: String,
         @RequestHeader(value = HttpHeaders.AUTHORIZATION) token: String,
-    ): ResponseEntity<ForelopigSvarResponse> =
-        runBlocking {
-            withContext(MDCContext() + RequestAttributesContext()) {
-                tilgangskontroll.sjekkTilgang(token)
+    ): ForelopigSvarResponse {
+        tilgangskontroll.sjekkTilgang(token)
 
-                val forelopigSvarResponse: ForelopigSvarResponse = forelopigSvarService.hentForelopigSvar(fiksDigisosId, token)
-
-                ResponseEntity.ok().body(forelopigSvarResponse)
-            }
-        }
+        return forelopigSvarService.hentForelopigSvar(fiksDigisosId, token)
+    }
 }
--- src/test/kotlin/no/nav/sosialhjelp/innsyn/tilgang/TilgangskontrollServiceTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/tilgang/TilgangskontrollServiceTest.kt
@@ -3,55 +3,41 @@ package no.nav.sosialhjelp.innsyn.tilgang
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.coroutines.test.runTest
 import no.nav.sosialhjelp.api.fiks.DigisosSak
 import no.nav.sosialhjelp.innsyn.app.exceptions.TilgangskontrollException
-import no.nav.sosialhjelp.innsyn.app.subjecthandler.SubjectHandler
-import no.nav.sosialhjelp.innsyn.app.subjecthandler.SubjectHandlerUtils
 import no.nav.sosialhjelp.innsyn.tilgang.pdl.Adressebeskyttelse
 import no.nav.sosialhjelp.innsyn.tilgang.pdl.Gradering
 import no.nav.sosialhjelp.innsyn.tilgang.pdl.PdlClient
 import no.nav.sosialhjelp.innsyn.tilgang.pdl.PdlHentPerson
 import no.nav.sosialhjelp.innsyn.tilgang.pdl.PdlNavn
+import no.nav.sosialhjelp.innsyn.utils.runTestWithToken
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.core.env.Environment
-import kotlin.time.Duration.Companion.seconds
 
 internal class TilgangskontrollServiceTest {
     private val pdlClientMock: PdlClient = mockk()
-    private val environment: Environment = mockk()
-    private val service = TilgangskontrollService("clientId", environment, pdlClientMock)
+    private val service = TilgangskontrollService(pdlClientMock)
 
     private val ident = "123"
 
     private val clientResponse: PdlHentPerson = mockk()
 
-    private val mockSubjectHandler: SubjectHandler = mockk()
-
     private val digisosSak: DigisosSak = mockk()
 
     @BeforeEach
     fun init() {
-        SubjectHandlerUtils.setNewSubjectHandlerImpl(mockSubjectHandler)
-
-        every { mockSubjectHandler.getUserIdFromToken() } returns ident
-        every { mockSubjectHandler.getToken() } returns "token"
-        every { mockSubjectHandler.getClientId() } returns "clientId"
         every { digisosSak.sokerFnr } returns ident
-        every { environment.activeProfiles } returns arrayOf("test")
     }
 
     @AfterEach
     internal fun tearDown() {
-        SubjectHandlerUtils.resetSubjectHandlerImpl()
     }
 
     @Test
     internal fun `sjekkTilgang - skal ikke kaste feil hvis client returnerer null`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns null
 
             assertThat(kotlin.runCatching { service.sjekkTilgang(ident) }.isSuccess)
@@ -59,7 +45,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `sjekkTilgang - skal kaste feil hvis client returnerer PdlHentPerson_pdlPerson = null`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson } returns null
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
 
@@ -68,7 +54,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `sjekkTilgang - skal ikke kaste feil hvis adressebeskyttelse-liste er tom`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns emptyList()
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
 
@@ -77,7 +63,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `sjekkTilgang - skal kaste feil hvis bruker er kode6`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.STRENGT_FORTROLIG))
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
 
@@ -89,7 +75,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `sjekkTilgang - skal kaste feil hvis bruker er kode7`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.FORTROLIG))
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
 
@@ -101,7 +87,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `sjekkTilgang - skal kaste feil hvis bruker er kode6 utland`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.STRENGT_FORTROLIG_UTLAND))
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
 
@@ -115,7 +101,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `hentTilgang - skal ikke gi tilgang hvis client returnerer null`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns null
 
             assertThat(service.hentTilgang(ident, "token").harTilgang).isFalse
@@ -124,7 +110,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `hentTilgang - skal ikke gi tilgang hvis client returnerer PdlHentPerson_pdlPerson = null`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson } returns null
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
 
@@ -134,7 +120,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `hentTilgang - skal gi harTilgang true hvis ingen adressebeskyttelse`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns emptyList()
             every { clientResponse.hentPerson?.navn } returns null
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
@@ -144,7 +130,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `hentTilgang - skal gi harTilgang false hvis adressebeskyttelse`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.STRENGT_FORTROLIG))
             every { clientResponse.hentPerson?.navn } returns null
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
@@ -154,7 +140,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `hentTilgang - skal gi forste fornavn med stor forbokstav`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.STRENGT_FORTROLIG))
             every { clientResponse.hentPerson?.navn } returns listOf(PdlNavn("KREATIV"), PdlNavn("NATA"))
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
@@ -165,7 +151,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `hentTilgang - skal gi fornavn som tom string om det ikke finnes`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.STRENGT_FORTROLIG))
             every { clientResponse.hentPerson?.navn } returns null
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
@@ -176,7 +162,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `hentTilgang - skal gi fornavn som tom string om navneliste er tom`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             every { clientResponse.hentPerson?.adressebeskyttelse } returns listOf(Adressebeskyttelse(Gradering.FORTROLIG))
             every { clientResponse.hentPerson?.navn } returns emptyList()
             coEvery { pdlClientMock.hentPerson(any(), any()) } returns clientResponse
@@ -187,7 +173,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `verifyDigisosSakIsForCorrectUser - ok - skal ikke kaste exception`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             coEvery { pdlClientMock.hentIdenter(any(), any()) } returns listOf(ident)
 
             service.verifyDigisosSakIsForCorrectUser(digisosSak)
@@ -195,7 +181,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `verifyDigisosSakIsForCorrectUser - feil person - skal kaste exception`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             coEvery { pdlClientMock.hentIdenter(any(), any()) } returns listOf("fnr")
 
             runCatching { service.verifyDigisosSakIsForCorrectUser(digisosSak) }.let {
@@ -206,7 +192,7 @@ internal class TilgangskontrollServiceTest {
 
     @Test
     internal fun `verifyDigisosSakIsForCorrectUser - to fnr - ok`() =
-        runTest(timeout = 5.seconds) {
+        runTestWithToken {
             coEvery { pdlClientMock.hentIdenter(any(), any()) } returns listOf("fnr", ident)
 
             service.verifyDigisosSakIsForCorrectUser(digisosSak)

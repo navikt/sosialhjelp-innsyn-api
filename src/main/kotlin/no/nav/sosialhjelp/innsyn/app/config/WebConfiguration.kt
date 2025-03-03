a03df790c1b34f62f6a84f9d0cab1d7f70b143ab
Martin Almvik <martin.almvik@nav.no>
2025-03-03 15:08:57 +0100

    Opprydding etter security

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/config/WebConfiguration.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/config/WebConfiguration.kt
@@ -1,6 +1,5 @@
 package no.nav.sosialhjelp.innsyn.app.config
 
-import no.nav.sosialhjelp.innsyn.app.config.webfilter.AuthWebFilter
 import no.nav.sosialhjelp.innsyn.app.config.webfilter.TracingWebFilter
 import no.nav.sosialhjelp.innsyn.app.config.webfilter.mdc.MDCFilter
 import org.springframework.context.annotation.Bean
@@ -10,7 +9,6 @@ import org.springframework.web.server.WebFilter
 @Configuration
 class WebConfiguration(
     private val tracingWebFilter: TracingWebFilter,
-    private val authWebFilter: AuthWebFilter,
     private val mdcFilter: MDCFilter,
 ) {
     @Bean

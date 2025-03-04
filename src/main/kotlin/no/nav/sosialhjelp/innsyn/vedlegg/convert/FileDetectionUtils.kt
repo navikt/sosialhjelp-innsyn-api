97a0df8d4b03619bcb567fbc22089dcf90b1d3c7
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-03-04 11:38:49 +0100

    Endepunkt for konvertering av PDF

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/vedlegg/convert/FileDetectionUtils.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/vedlegg/convert/FileDetectionUtils.kt
@@ -15,14 +15,5 @@ object FileDetectionUtils {
 object MimeTypes {
     const val APPLICATION_PDF = "application/pdf"
     const val APPLICATION_JSON = "application/json"
-    const val IMAGE_PNG = "image/png"
-    const val IMAGE_JPEG = "image/jpeg"
     const val TEXT_X_MATLAB = "text/x-matlab"
-}
-
-enum class TikaFileType(val extension: String) {
-    JPEG(".jpg"),
-    PNG(".png"),
-    PDF(".pdf"),
-    UNKNOWN(""),
 }

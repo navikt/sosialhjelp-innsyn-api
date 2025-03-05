d6200c30ad29682e695d238067e7daf433cbcf1d
Martin Almvik <martin.almvik@nav.no>
2025-03-05 11:52:14 +0100

    Oppdatering etter rebase

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/pdl/PdlService.kt
@@ -1,29 +1,27 @@
 package no.nav.sosialhjelp.innsyn.pdl
 
-import no.nav.sosialhjelp.innsyn.app.subjecthandler.SubjectHandler
-import no.nav.sosialhjelp.innsyn.app.subjecthandler.SubjectHandlerUtils.getToken
+import no.nav.sosialhjelp.innsyn.app.token.TokenUtils
 import no.nav.sosialhjelp.innsyn.pdl.dto.PdlGradering
 import org.springframework.stereotype.Service
 
 @Service
 class PdlService(
     private val pdlClient: PdlClient,
-    private val subjectHandler: SubjectHandler,
 ) {
     suspend fun getAdressebeskyttelse(): Boolean =
         pdlClient
-            .getPerson(subjectHandler.getToken())
+            .getPerson(TokenUtils.getUserIdFromToken())
             .adressebeskyttelse
             .any { it.gradering in BEGRENSEDE_GRADERINGER }
 
     suspend fun getFornavn(): String =
         pdlClient
-            .getPerson(subjectHandler.getToken())
+            .getPerson(TokenUtils.getUserIdFromToken())
             .navn
             .first()
             .fornavn
 
-    suspend fun getIdentsByIdent(ident: String): List<String> = pdlClient.getIdentsByIdent(getToken())
+    suspend fun getIdentsByIdent(ident: String): List<String> = pdlClient.getIdentsByIdent(ident)
 
     companion object {
         private val BEGRENSEDE_GRADERINGER =

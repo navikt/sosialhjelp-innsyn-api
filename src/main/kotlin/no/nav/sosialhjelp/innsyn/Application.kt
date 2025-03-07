1be91974b3851d1af3ee9f11d09c31845574d777
Martin Almvik <martin.almvik@nav.no>
2025-03-07 20:20:27 +0100

    Ikke bruk @EnableWebFlux

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/Application.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/Application.kt
@@ -2,10 +2,8 @@ package no.nav.sosialhjelp.innsyn
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
-@EnableWebFlux
 class Application
 
 fun main(args: Array<String>) {

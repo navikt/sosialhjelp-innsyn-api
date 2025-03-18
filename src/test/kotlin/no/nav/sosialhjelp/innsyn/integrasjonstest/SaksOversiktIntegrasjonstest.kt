7a01e0ec3895df4077e138a4d09c5f1c46d860c5
joeldlin <jorgen.elden.lindgren@nav.no>
2025-03-18 14:11:55 +0100

    r - wip

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/integrasjonstest/SaksOversiktIntegrasjonstest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/integrasjonstest/SaksOversiktIntegrasjonstest.kt
@@ -19,7 +19,6 @@ import no.nav.sosialhjelp.innsyn.responses.ok_komplett_jsondigisossoker_response
 import no.nav.sosialhjelp.innsyn.saksoversikt.SaksListeResponse
 import no.nav.sosialhjelp.innsyn.testutils.IntegrasjonstestStubber
 import no.nav.sosialhjelp.innsyn.testutils.MockOauth2ServerUtils
-import no.nav.sosialhjelp.innsyn.utils.logger
 import no.nav.sosialhjelp.innsyn.utils.objectMapper
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test

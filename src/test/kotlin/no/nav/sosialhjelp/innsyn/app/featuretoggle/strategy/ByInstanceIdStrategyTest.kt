262a573d39482b070dd21bf0f18408dae6e41dd2
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2025-05-12 10:41:36 +0200

    Upgrade unleash (#908)
    * Upgrade unleash-client-java
    
    * Breaking changes i oppgradering av Unleash
    
    * Ktlint

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/strategy/ByInstanceIdStrategyTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/app/featuretoggle/strategy/ByInstanceIdStrategyTest.kt
@@ -1,21 +1,24 @@
 package no.nav.sosialhjelp.innsyn.app.featuretoggle.strategy
 
+import io.getunleash.UnleashContext
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ByInstanceIdStrategyTest {
+    private val ctx = UnleashContext("userId", "sessionId", "remoteAdress", emptyMap())
+
     @Test
     fun shouldReturnFalse_instanceIdNotInMap() {
         val strategy = ByInstanceIdStrategy("local")
         val parameters = mutableMapOf("instance.id" to "dev,mock")
-        assertFalse(strategy.isEnabled(parameters))
+        assertFalse(strategy.isEnabled(parameters, ctx))
     }
 
     @Test
     fun shoudReturnTrue_instanceIdInMap() {
         val strategy = ByInstanceIdStrategy("dev")
         val parameters = mutableMapOf("instance.id" to "dev,mock")
-        assertTrue(strategy.isEnabled(parameters))
+        assertTrue(strategy.isEnabled(parameters, ctx))
     }
 }

1b1c5533dee980215e27df7e5ac08a7c266bff09
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-07-04 08:42:13 +0200

    Testfiks

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/integrasjonstest/AbstractIntegrationTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/integrasjonstest/AbstractIntegrationTest.kt
@@ -5,36 +5,22 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.coEvery
 import no.nav.security.mock.oauth2.MockOAuth2Server
-import no.nav.sosialhjelp.innsyn.MockOAuth2ServerInitializer
-import no.nav.sosialhjelp.innsyn.testutils.MockOauth2ServerUtils
 import no.nav.sosialhjelp.innsyn.tilgang.pdl.PdlClientOld
 import no.nav.sosialhjelp.innsyn.tilgang.pdl.PdlHentPerson
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = ["mock-redis", "test", "local_unleash"])
-@ExtendWith(SpringExtension::class)
-@ContextConfiguration(initializers = [MockOAuth2ServerInitializer::class])
 abstract class AbstractIntegrationTest {
     @Autowired
     private lateinit var webClient: WebTestClient
-
-    @Autowired
-    private lateinit var mockLogin: MockOauth2ServerUtils
-
-    @Autowired
-    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
-    private lateinit var mockOAuth2Server: MockOAuth2Server
 
     @MockkBean
     private lateinit var pdlClientOld: PdlClientOld
@@ -67,6 +53,14 @@ abstract class AbstractIntegrationTest {
             .expectStatus()
             .isOk
 
+    companion object {
+        private val mockOAuth2Server = MockOAuth2Server()
+        private const val PORT = 12345
+
+        init {
+            mockOAuth2Server.start(PORT)
+        }
+    }
 }
 
 private fun hentPersonAnswer(): PdlHentPerson {

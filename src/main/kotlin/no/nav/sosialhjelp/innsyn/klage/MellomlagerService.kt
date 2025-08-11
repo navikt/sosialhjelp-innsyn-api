f3e52f99176dc9c7b21b1ce9eefd43758695deaf
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-08-11 10:28:34 +0200

    forts..... mellomlager

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/klage/MellomlagerService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/klage/MellomlagerService.kt
@@ -8,17 +8,73 @@ import no.nav.sosialhjelp.innsyn.vedlegg.virusscan.VirusScanner
 import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.stereotype.Service
 import java.util.UUID
+import org.springframework.context.annotation.Profile
+import org.springframework.http.codec.multipart.FilePart
 
 interface MellomlagerService {
     suspend fun processFileUpload(
         klageId: UUID,
-        metadata: OpplastetVedleggMetadata,
+        allFiles: List<FilePart>,
     ): List<OppgaveValidering>
 }
 
+@Profile("!local")
+@Service
+class MellomlagerServiceImpl(
+    private val mellomlagerClient: MellomlagerClient,
+    private val virusScanner: VirusScanner,
+): MellomlagerService {
+
+    private val uploadDocumentHelper = UploadDocumentHelper(virusScanner)
+
+    override suspend fun processFileUpload(
+        klageId: UUID,
+        allFiles: List<FilePart>,
+    ): List<OppgaveValidering> {
+
+        val metadataList = uploadDocumentHelper.extractAndAddFilesToMetadata(allFiles)
+
+        val validations = uploadDocumentHelper.validateMetadata(metadataList)
+        if (validations.flatMap { validation -> validation.filer.map { it.status } }.any { it.result != ValidationValues.OK }) {
+            return validations
+        }
+
+        val filerForOpplasting = uploadDocumentHelper.createFilerForOpplasting(metadataList)
+
+
+        mellomlagerClient.lastOppVedlegg(klageId, filerForOpplasting)
+
+        // TODO Do upload
+
+
+        return validations
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+@Profile("local")
 @Service
 class LocalMellomlagerService(
-    private val mellomlagerClient: FiksMellomlagerClient,
+    private val mellomlagerClient: MellomlagerClient,
     private val virusScanner: VirusScanner,
 ) : MellomlagerService {
     override suspend fun processFileUpload(

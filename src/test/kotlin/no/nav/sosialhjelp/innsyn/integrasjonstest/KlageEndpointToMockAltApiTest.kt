6ef635b97c2030a97edeebc3c9375132944f2fff
Amund Sørumshagen <amund.sorumshagen@nav.no>
2025-09-19 10:10:39 +0200

    Venter på isReady

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/integrasjonstest/KlageEndpointToMockAltApiTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/integrasjonstest/KlageEndpointToMockAltApiTest.kt
@@ -1,6 +1,5 @@
 package no.nav.sosialhjelp.innsyn.integrasjonstest
 
-import java.util.UUID
 import no.nav.sosialhjelp.innsyn.klage.KlageDto
 import no.nav.sosialhjelp.innsyn.klage.KlageInput
 import org.assertj.core.api.Assertions.assertThat
@@ -19,6 +18,7 @@ import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
+import java.util.UUID
 
 @AutoConfigureWebTestClient(timeout = "PT36000S")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)

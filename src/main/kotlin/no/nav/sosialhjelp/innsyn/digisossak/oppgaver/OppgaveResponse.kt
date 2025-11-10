cccc824352b0cf97669ff4b4f724fd162ba4420a
Martin Almvik <martin.almvik@nav.no>
2025-11-10 14:32:59 +0100

    Flytt nye endepunkter for oppgave til v2

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/oppgaver/OppgaveResponse.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/oppgaver/OppgaveResponse.kt
@@ -2,6 +2,7 @@ package no.nav.sosialhjelp.innsyn.digisossak.oppgaver
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import no.nav.sbl.soknadsosialhjelp.vedlegg.JsonVedlegg
+import no.nav.sosialhjelp.api.fiks.DokumentInfo
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -31,4 +32,11 @@ data class OppgaveElement(
     val hendelsetype: JsonVedlegg.HendelseType?,
     val hendelsereferanse: String?,
     val erFraInnsyn: Boolean,
+)
+
+data class OppgaveVedleggFil(
+    val url: String,
+    val filnavn: String,
+    val storrelse: Long,
+    val tidspunktLastetOpp: LocalDateTime,
 )

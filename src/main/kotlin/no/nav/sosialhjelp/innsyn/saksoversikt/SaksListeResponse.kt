37759bdde5d32304677f7ed9dd6ce9ad9b890809
joeldlin <jorgen.elden.lindgren@nav.no>
2025-11-20 13:54:08 +0100

    fjerner klokkeslett

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/saksoversikt/SaksListeResponse.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/saksoversikt/SaksListeResponse.kt
@@ -7,12 +7,12 @@ data class SaksListeResponse(
     // TODO: Trenger denne å være nullable?
     val fiksDigisosId: String?,
     val soknadTittel: String,
-    @param:JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @param:JsonFormat(pattern = "yyyy-MM-dd")
     val sistOppdatert: Date,
     // TODO: Dette er legacy fra svarut-integrasjon i soknad-api. Kan avvikles.
     val kilde: String,
     val url: String?,
     val kommunenummer: String?,
-    @param:JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @param:JsonFormat(pattern = "yyyy-MM-dd")
     val soknadOpprettet: Date?,
 )

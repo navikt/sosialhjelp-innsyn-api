93cdd7cbe1f09caa3a823067ed752dedb00b8f73
Ragnhild Neset <ragcecnes@gmail.com>
2025-11-20 09:48:19 +0100

    Fjerne eventuelle duplikate utbetalinger.
    Og logge.

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/utbetalinger2/UtbetalingerController2.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/utbetalinger2/UtbetalingerController2.kt
@@ -11,16 +11,36 @@ class UtbetalingerController2(
     private val tilgangskontroll: TilgangskontrollService,
     private val utbetalingerServiceNew: UtbetalingerService,
 ) {
+    private val logger = org.slf4j.LoggerFactory.getLogger(UtbetalingerController2::class.java)
+
     @GetMapping
     suspend fun hentUtbetalinger(): List<UtbetalingDto> {
         tilgangskontroll.sjekkTilgang()
 
-        return utbetalingerServiceNew.hentUtbetalinger().flatMap { (fiksDigisosId, utbetalinger) ->
-            utbetalinger.map {
-                it.toDto(
-                    fiksDigisosId,
-                )
-            }
-        }
+        val flatUtbetalinger =
+            utbetalingerServiceNew
+                .hentUtbetalinger()
+                .flatMap { (fiksDigisosId, utbetalinger) ->
+                    utbetalinger.map {
+                        it.toDto(fiksDigisosId)
+                    }
+                }.also { utbetalinger ->
+                    // Grunnet rapportert feil med at bruker ser duplikate utbetalinger, legger vi til egen logging på dette.
+                    // Kan fjernes når vi er sikre på at problemet er løst.
+                    val duplikater =
+                        utbetalinger
+                            .groupBy { it.referanse }
+                            .filter { it.value.size > 1 }
+                    if (duplikater.isNotEmpty()) {
+                        val duplikatLogg =
+                            duplikater
+                                .flatMap { (ref, list) ->
+                                    list.map { dto -> "fiksDigisosId=${dto.fiksDigisosId}, referanse=$ref" }
+                                }.joinToString(", ")
+                        logger.warn("Fant duplikate utbetalinger: $duplikatLogg")
+                    }
+                }.distinctBy { it.referanse } // Fjerner eventuelle duplikater basert på referanse
+
+        return flatUtbetalinger
     }
 }

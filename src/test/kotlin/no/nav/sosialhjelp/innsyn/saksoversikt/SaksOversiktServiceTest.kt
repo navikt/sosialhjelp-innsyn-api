df5e6361149b5fd48b2e6fab43f96643a19efe47
Martin Almvik <martin.almvik@nav.no>
2025-11-26 08:52:07 +0100

    Fiks tester

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/saksoversikt/SaksOversiktServiceTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/saksoversikt/SaksOversiktServiceTest.kt
@@ -1,15 +1,11 @@
 package no.nav.sosialhjelp.innsyn.saksoversikt
 
-import io.getunleash.Unleash
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import no.nav.sosialhjelp.api.fiks.DigisosSak
-import no.nav.sosialhjelp.innsyn.app.featuretoggle.FAGSYSTEM_MED_INNSYN_I_PAPIRSOKNADER
 import no.nav.sosialhjelp.innsyn.digisosapi.FiksClient
-import no.nav.sosialhjelp.innsyn.digisossak.oppgaver.OppgaveService
-import no.nav.sosialhjelp.innsyn.utils.IntegrationUtils.KILDE_INNSYN_API
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -17,10 +13,8 @@ import kotlin.time.Duration.Companion.seconds
 
 internal class SaksOversiktServiceTest {
     private val fiksClient: FiksClient = mockk()
-    private val unleashClient: Unleash = mockk()
-    private val oppgaveService: OppgaveService = mockk()
 
-    private val saksOversiktService = SaksOversiktService(fiksClient, unleashClient, oppgaveService)
+    private val saksOversiktService = SaksOversiktService(fiksClient)
 
     private val digisosSak1: DigisosSak = mockk()
     private val digisosSak2: DigisosSak = mockk()
@@ -46,8 +40,6 @@ internal class SaksOversiktServiceTest {
                 every { timestampSendt } returns 500L
             }
         every { digisosSak2.digisosSoker } returns mockk()
-
-        every { unleashClient.isEnabled(FAGSYSTEM_MED_INNSYN_I_PAPIRSOKNADER, false) } returns false
     }
 
     @Test
@@ -60,12 +52,8 @@ internal class SaksOversiktServiceTest {
             assertThat(alleSaker).hasSize(2)
             assertThat(alleSaker[0].fiksDigisosId).isEqualTo("123")
             assertThat(alleSaker[0].soknadTittel).isEqualTo("saker.default_tittel")
-            assertThat(alleSaker[0].kilde).isEqualTo(KILDE_INNSYN_API)
-            assertThat(alleSaker[0].url).isNull()
             assertThat(alleSaker[1].fiksDigisosId).isEqualTo("456")
             assertThat(alleSaker[1].soknadTittel).isEqualTo("saker.default_tittel")
-            assertThat(alleSaker[1].kilde).isEqualTo(KILDE_INNSYN_API)
-            assertThat(alleSaker[1].url).isNull()
         }
 
     @Test

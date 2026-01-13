008198dcc81247c09d86e758702401344139f3d5
Martin Almvik <martin.almvik@nav.no>
2026-01-13 13:40:24 +0100

    Fjerner logging av duplikate utbetalinger

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/utbetalinger2/UtbetalingerController2.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/utbetalinger2/UtbetalingerController2.kt
@@ -25,21 +25,6 @@ class UtbetalingerController2(
                     utbetalinger.map {
                         it.toDto(fiksDigisosId)
                     }
-                }.also { utbetalinger ->
-                    // Grunnet rapportert feil med at bruker ser duplikate utbetalinger, legger vi til egen logging på dette.
-                    // Kan fjernes når vi er sikre på at problemet er løst.
-                    val duplikater =
-                        utbetalinger
-                            .groupBy { it.referanse }
-                            .filter { it.value.size > 1 }
-                    if (duplikater.isNotEmpty()) {
-                        val duplikatLogg =
-                            duplikater
-                                .flatMap { (ref, list) ->
-                                    list.map { dto -> "fiksDigisosId=${dto.fiksDigisosId}, referanse=$ref" }
-                                }.joinToString(", ")
-                        logger.warn("Fant duplikate utbetalinger: $duplikatLogg")
-                    }
                 }.distinctBy { it.referanse } // Fjerner eventuelle duplikater basert på referanse
 
         return flatUtbetalinger

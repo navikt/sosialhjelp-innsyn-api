62bcfba9d4aacb2a1d452528ebfede067444fa3a
Martin <martin.almvik@nav.no>
2026-01-19 12:11:42 +0100

    Oppdater sistOppdatert til å også se på sistOppdatert på innsynsfil (#1033)

--- src/test/kotlin/no/nav/sosialhjelp/innsyn/saksoversikt/SaksOversiktServiceTest.kt
+++ src/test/kotlin/no/nav/sosialhjelp/innsyn/saksoversikt/SaksOversiktServiceTest.kt
@@ -6,9 +6,11 @@ import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import no.nav.sosialhjelp.api.fiks.DigisosSak
 import no.nav.sosialhjelp.innsyn.digisosapi.FiksService
+import no.nav.sosialhjelp.innsyn.utils.unixToLocalDateTime
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
 import kotlin.time.Duration.Companion.seconds
 
 internal class SaksOversiktServiceTest {
@@ -39,7 +41,10 @@ internal class SaksOversiktServiceTest {
                 every { navEksternRefId } returns "123"
                 every { timestampSendt } returns 500L
             }
-        every { digisosSak2.digisosSoker } returns mockk()
+        every { digisosSak2.digisosSoker } returns
+            mockk {
+                every { timestampSistOppdatert } returns System.currentTimeMillis()
+            }
     }
 
     @Test
@@ -50,10 +55,59 @@ internal class SaksOversiktServiceTest {
             val alleSaker = saksOversiktService.hentAlleSaker()
 
             assertThat(alleSaker).hasSize(2)
-            assertThat(alleSaker[0].fiksDigisosId).isEqualTo("123")
+            assertThat(alleSaker[0].fiksDigisosId).isEqualTo("456")
             assertThat(alleSaker[0].soknadTittel).isEqualTo("saker.default_tittel")
-            assertThat(alleSaker[1].fiksDigisosId).isEqualTo("456")
+            assertThat(alleSaker[1].fiksDigisosId).isEqualTo("123")
             assertThat(alleSaker[1].soknadTittel).isEqualTo("saker.default_tittel")
+        }
+
+    @Test
+    internal fun `Skal ta høyeste sistOppdatert`() =
+        runTest(timeout = 5.seconds) {
+            val currentTimeMillis = System.currentTimeMillis()
+            val digisosSak3: DigisosSak =
+                mockk {
+                    every { fiksDigisosId } returns "456"
+                    every { sistEndret } returns 1000L
+                    every { kommunenummer } returns "0301"
+                    every { originalSoknadNAV } returns
+                        mockk {
+                            every { navEksternRefId } returns "123"
+                            every { timestampSendt } returns 500L
+                        }
+                    every { digisosSoker } returns
+                        mockk {
+                            every { timestampSistOppdatert } returns currentTimeMillis
+                        }
+                }
+            val digisosSak4: DigisosSak =
+                mockk {
+                    every { fiksDigisosId } returns "123"
+                    every { sistEndret } returns currentTimeMillis
+                    every { kommunenummer } returns "0301"
+                    every { originalSoknadNAV } returns
+                        mockk {
+                            every { navEksternRefId } returns "456"
+                            every { timestampSendt } returns 500L
+                        }
+                    every { digisosSoker } returns
+                        mockk {
+                            every { timestampSistOppdatert } returns 1000
+                        }
+                }
+
+            coEvery { fiksService.getAllSoknader() } returns listOf(digisosSak3, digisosSak4)
+
+            val alleSaker = saksOversiktService.hentAlleSaker()
+
+            assertThat(alleSaker).hasSize(2)
+            val firstSak = alleSaker.find { it.fiksDigisosId == "456" }
+            assertNotNull(firstSak)
+            val nowDate = unixToLocalDateTime(currentTimeMillis)
+            assertThat(firstSak.sistOppdatert).isEqualTo(nowDate)
+            val secondSak = alleSaker.find { it.fiksDigisosId == "123" }
+            assertNotNull(secondSak)
+            assertThat(firstSak.sistOppdatert).isEqualTo(nowDate)
         }
 
     @Test

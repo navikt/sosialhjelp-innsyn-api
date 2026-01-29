7cd7341b1c8d6a3c6b08e60125f952af0ad31526
Amund Sørumshagen <amund.sorumshagen@nav.no>
2026-01-29 07:55:10 +0100

    Tilbakemeldinger på pr

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/klage/KlageMetricsService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/klage/KlageMetricsService.kt
@@ -3,11 +3,13 @@ package no.nav.sosialhjelp.innsyn.klage
 import io.micrometer.core.instrument.MeterRegistry
 
 class KlageMetricsService(
-    meterRegistry: MeterRegistry,
+    private val meterRegistry: MeterRegistry,
 ) {
     fun registerSent() {
+        meterRegistry.counter("klage.sent").increment()
     }
 
     fun registerSendError() {
+        meterRegistry.counter("klage.send.error").increment()
     }
 }

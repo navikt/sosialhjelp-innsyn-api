7eada719efc569da95b29743f973287f63dde2a4
Martin <martin.almvik@nav.no>
2026-03-25 09:58:44 +0100

    Legger til brev-controller for å hente alle relevante brev (#1129)
    * Legger til brev-controller for å hente alle relevante brev
    
    * PR-kommentarer
    
    * Test for BrevService

---
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/digisossak/brev/BrevService.kt
@@ -0,0 +1,53 @@
+package no.nav.sosialhjelp.innsyn.digisossak.brev
+
+import no.nav.sosialhjelp.innsyn.digisosapi.FiksService
+import no.nav.sosialhjelp.innsyn.event.EventService
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+class BrevService(
+    private val fiksService: FiksService,
+    private val eventService: EventService,
+) {
+    suspend fun getBrev(id: String): List<Brev> {
+        val digisosSak = fiksService.getSoknad(id)
+        val model = eventService.createModel(digisosSak)
+        val forelopigSvarBrev: Brev? =
+            with(model.forelopigSvar) {
+                if (harMottattForelopigSvar && link != null) {
+                    Brev(
+                        type = Brev.BrevType.FORELOPIG_SVAR,
+                        url = link,
+                        timestamp = timestamp,
+                    )
+                } else {
+                    null
+                }
+            }
+
+        val vedtaksbrev = model.saker.flatMap { it.vedtak }.map { Brev(Brev.BrevType.VEDTAK, it.vedtaksFilUrl, it.dato?.atStartOfDay()) }
+        val dokEtterspurtBrev =
+            model.oppgaver
+                .mapNotNull { it.forvaltningsbrev }
+                .distinctBy { it.url }
+                .map { Brev(Brev.BrevType.DOKUMENTASJON_ETTERSPURT, it.url, it.timestamp) }
+        return listOf(
+            vedtaksbrev,
+            dokEtterspurtBrev,
+            listOf(forelopigSvarBrev),
+        ).flatten().filterNotNull().sortedByDescending { it.timestamp }
+    }
+}
+
+data class Brev(
+    val type: BrevType,
+    val url: String,
+    val timestamp: LocalDateTime?,
+) {
+    enum class BrevType {
+        FORELOPIG_SVAR,
+        DOKUMENTASJON_ETTERSPURT,
+        VEDTAK,
+    }
+}

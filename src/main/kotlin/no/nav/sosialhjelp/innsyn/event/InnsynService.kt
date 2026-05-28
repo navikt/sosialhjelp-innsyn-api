daa299c8073b837a9c1609d9c97e86d75e423149
Amund Sørumshagen <3726993+amundsor@users.noreply.github.com>
2026-05-28 11:57:06 +0200

    Batche bulk-innhenting (#1158)
    * Det kan potensielt være mange søknader for en person, og requestens levetid ved å hente alle innsynsfiler er ganske høy til tider. Ved å batche opp dette vil det senke levetiden pr. request og data-.overføring pr. request vil være mindre.
    
    * Spotless
    
    * Det kan potensielt være mange søknader for en person, og requestens levetid ved å hente alle innsynsfiler er ganske høy til tider. Ved å batche opp dette vil det senke levetiden pr. request og data-.overføring pr. request vil være mindre.
    
    * Det kan potensielt være mange søknader for en person, og requestens levetid ved å hente alle innsynsfiler er ganske høy til tider. Ved å batche opp dette vil det senke levetiden pr. request og data-.overføring pr. request vil være mindre.
    
    * Bytter til .asFlow() så man alltid har n aktive slots som gjør arbeid
    
    * Spotless
    
    * Fjerner logging
    
    * Trenger ikke InnsynService lenger da test er flyttet til annen testklasse
    
    * Spotlessapply

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/event/InnsynService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/event/InnsynService.kt
@@ -1,5 +1,12 @@
 package no.nav.sosialhjelp.innsyn.event
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.withContext
 import no.nav.sbl.soknadsosialhjelp.digisos.soker.JsonDigisosSoker
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonSoknad
 import no.nav.sosialhjelp.api.fiks.DigisosSak
@@ -36,7 +43,18 @@ class InnsynService(
         }
     }
 
-    suspend fun hentJsonDigisosSokerBulk(saker: List<DigisosSak>): Map<String, JsonDigisosSoker> = fiksService.getAllInnsynsfiler(saker)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    suspend fun hentJsonDigisosSokerBulk(saker: List<DigisosSak>): Map<String, JsonDigisosSoker> =
+        withContext(Dispatchers.IO) {
+            saker
+                .chunked(CHUNK_SIZE)
+                .asFlow()
+                .flatMapMerge(concurrency = MAX_CONCURRENT_CHUNK_REQUESTS) { chunk ->
+                    flow { emit(fiksService.getAllInnsynsfiler(chunk)) }
+                }.fold(mutableMapOf<String, JsonDigisosSoker>()) { acc, chunkMap ->
+                    acc.apply { putAll(chunkMap) }
+                }
+        }
 
     suspend fun hentOriginalSoknad(digisosSak: DigisosSak): JsonSoknad? {
         val originalMetadataId = digisosSak.originalSoknadNAV?.metadata
@@ -54,5 +72,7 @@ class InnsynService(
 
     companion object {
         private val log by logger()
+        const val CHUNK_SIZE = 25
+        const val MAX_CONCURRENT_CHUNK_REQUESTS = 10
     }
 }

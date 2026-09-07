e9a8ee320d70bc6077f737b9a38ab33cbe83ba8b
Martin <martin.almvik@nav.no>
2026-09-07 13:11:27 +0200

    Fix digisosid i mdc (#1227)
    * Fix digisosid i mdc
    
    Hvis en url ikke var med i lista over endepunkt, så ville den bli feil i mdc-en
    
    * Bare sett MDC inni coroutinecontext
    
    * Fjern logging av fodselsdato til secure logs
    
    Brukes ikke

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/app/config/webfilter/mdc/MDCFilter.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/app/config/webfilter/mdc/MDCFilter.kt
@@ -3,11 +3,7 @@ package no.nav.sosialhjelp.innsyn.app.config.webfilter.mdc
 import kotlinx.coroutines.slf4j.MDCContext
 import kotlinx.coroutines.withContext
 import no.nav.sosialhjelp.innsyn.app.config.webfilter.mdc.MDCUtils.DIGISOS_ID
-import no.nav.sosialhjelp.innsyn.app.config.webfilter.mdc.MDCUtils.clearMDC
-import no.nav.sosialhjelp.innsyn.app.config.webfilter.mdc.MDCUtils.put
-import no.nav.sosialhjelp.innsyn.app.token.TokenUtils
 import org.springframework.http.HttpHeaders
-import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.stereotype.Component
 import org.springframework.web.server.CoWebFilter
 import org.springframework.web.server.CoWebFilterChain
@@ -25,44 +21,23 @@ class MDCFilter : CoWebFilter() {
             return chain.filter(exchange)
         }
 
-        addDigisosId(request)
-        put(MDCUtils.PATH, request.uri.path)
-        put(MDCUtils.HTTP_METHOD, request.method.name())
-
-        request.headers.getFirst(HttpHeaders.USER_AGENT)?.let { put(MDCUtils.USER_AGENT, it) }
-        request.headers.getFirst(HttpHeaders.REFERER)?.let { put(MDCUtils.REFERER, it) }
-
-        // Fødselsdato blir ekskludert fra vanlig logging. Inkluderes altså kun i secure logs. Se logback-spring.xml
-        TokenUtils.getUserIdFromTokenOrNull()?.take(6)?.let { put("fodselsdato", it) }
-
-        try {
-            withContext(MDCContext()) {
-                chain.filter(exchange)
+        val mdcValues =
+            buildMap {
+                DIGISOS_ID_PATH_REGEX.find(request.uri.path)?.groupValues?.get(1)?.let { put(DIGISOS_ID, it) }
+                put(MDCUtils.PATH, request.uri.path)
+                put(MDCUtils.HTTP_METHOD, request.method.name())
+                request.headers.getFirst(HttpHeaders.USER_AGENT)?.let { put(MDCUtils.USER_AGENT, it) }
+                request.headers.getFirst(HttpHeaders.REFERER)?.let { put(MDCUtils.REFERER, it) }
             }
-        } finally {
-            clearMDC()
-        }
-    }
 
-    private fun addDigisosId(request: ServerHttpRequest) {
-        val path = request.uri.path
-        if (path.matches(
-                Regex(
-                    "^$INNSYN_BASE_URL(.*)/(forelopigSvar|hendelser|kommune|oppgaver|oppgaver/(.*)" +
-                        "|saksStatus|soknadsStatus|vedlegg|vilkar|dokumentasjonkrav|dokumentasjonkrav/(.*)" +
-                        "|harLeverteDokumentasjonkrav|fagsystemHarDokumentasjonkrav)",
-                ),
-            )
-        ) {
-            val digisosId = path.substringAfter(INNSYN_BASE_URL).substringBefore("/")
-            put(DIGISOS_ID, digisosId)
-        } else if (path.matches(Regex("^$INNSYN_BASE_URL(.*)/detaljer"))) {
-            val digisosId = path.substringAfter("${INNSYN_BASE_URL}sak/").substringBefore("/")
-            put(DIGISOS_ID, digisosId)
+        withContext(MDCContext(mdcValues)) {
+            chain.filter(exchange)
         }
     }
 
     companion object {
-        private const val INNSYN_BASE_URL = "/sosialhjelp/innsyn-api/api/v1/innsyn/"
+        private const val UUID_PATTERN = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+        private val DIGISOS_ID_PATH_REGEX =
+            Regex("(?:/sosialhjelp/innsyn-api)?/api/v[12]/innsyn/(?:sak/)?($UUID_PATTERN)(?:/|$)")
     }
 }

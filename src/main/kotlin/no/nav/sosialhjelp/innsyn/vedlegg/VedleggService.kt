be989c7a544eaedd0b2e130d2a41a8f03ce06116
Amund <amund.sorumshagen@nav.no>
2026-09-07 09:22:46 +0200

    Dobbeltsjekke om ettersendelse.pdf er inkludert i FilerMetadata fra fiks.

--- src/main/kotlin/no/nav/sosialhjelp/innsyn/vedlegg/VedleggService.kt
+++ src/main/kotlin/no/nav/sosialhjelp/innsyn/vedlegg/VedleggService.kt
@@ -164,27 +164,25 @@ class VedleggService(
                 }
             }
     }
+
     // TODO Hvilken fil er det som ikke matcher?
     private fun ettersendelsePdf(
         jsonVedlegg: List<JsonVedlegg>,
-        metadataFilerFiks: List<DokumentInfo>
-    ): String {
-        return runCatching {
+        metadataFilerFiks: List<DokumentInfo>,
+    ): String =
+        runCatching {
+            val filnavn = jsonVedlegg.flatMap { it.filer }.map { it.filnavn.sanitize() }
 
-        val filnavn = jsonVedlegg.flatMap { it.filer }.map { it.filnavn.sanitize() }
-
-         metadataFilerFiks
-            .filter { !filnavn.contains(it.filnavn.sanitize()) }
-            .let {
-                when {
-                    it.isEmpty() -> "Tom liste"
-                    it.size == 1 && it.first().filnavn.contains( "ettersendelse") -> "ettersendelse"
-                    else -> "Flere filer som ikke matcher - ettersendelse? ${it.find { fil -> fil.filnavn.contains("ettersendelse") }}"
+            metadataFilerFiks
+                .filter { !filnavn.contains(it.filnavn.sanitize()) }
+                .let {
+                    when {
+                        it.isEmpty() -> "Tom liste"
+                        it.size == 1 && it.first().filnavn.contains("ettersendelse") -> "ettersendelse"
+                        else -> "Flere filer som ikke matcher - ettersendelse? ${it.find { fil -> fil.filnavn.contains("ettersendelse") }}"
+                    }
                 }
-            }
-        }
-            .getOrElse { "error" }
-    }
+        }.getOrElse { "error" }
 
     private fun List<DokumentInfo>.addByFilename(filer: List<JsonFiler>): List<DokumentInfo> {
         val sanitizedFilenames = filer.map { it.filnavn.sanitize() }
